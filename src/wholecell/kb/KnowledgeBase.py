9092c4417be557565a8c019e45fb39b20a357aa0
macklin <macklin@server>
2013-03-26 14:09:21 -0700

    Added loadMetabolites() method to KnowledgeBase class

--- src/wholecell/kb/KnowledgeBase.py
+++ src/wholecell/kb/KnowledgeBase.py
@@ -34,7 +34,45 @@ class KnowledgeBase(object):
 		self.loadReactions()
 
 	def loadMetabolites(self):
-		pass
+		wb = xl.load_workbook(filename = self.dataFileName, use_iterators = True)
+		ws = wb.get_sheet_by_name("Metabolites").iter_rows()
+
+		# Skip the first row
+		ws.next()
+
+		self.metabolites = []
+		for row in ws:
+			m = {
+				"id": row[0].internal_value,
+				"name": row[1].internal_value,
+				"formula": row[2].internal_value,
+				"smiles": row[3].internal_value,
+				"charge": row[4].internal_value,
+				"mw": row[5].internal_value,
+				"hydrophobic": row[6].internal_value == "Y",
+				"mediaConc": 0,
+				"biomassConc": 0,
+				"metabolismNewFlux": 0,
+				"metabolismRecyclingFlux": 0,
+				"maxExchangeRate": 0
+			}
+			if m["name"] == None:
+				m["name"] = ""
+			if m["smiles"] == None:
+				m["smiles"] = ""
+			if row[7].internal_value != None:
+				m["mediaConc"] = row[7].internal_value
+			if row[8].internal_value != None:
+				m["biomassConc"] = row[8].internal_value
+			if row[9].internal_value != None:
+				m["metabolismNewFlux"] = row[9].internal_value
+			if row[10].internal_value != None:
+				m["metabolismRecyclingFlux"] = row[10].internal_value
+			if row[11].internal_value != None:
+				m["maxExchangeRate"] = row[11].internal_value
+			self.metabolites.append(m)
+
+
 
 	def loadGenome(self):
 		pass
