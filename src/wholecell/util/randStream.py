7a98040738eb8a3e94a15a9c8cb5d6c8c57475b3
macklin <macklin@server>
2013-03-25 20:26:23 -0700

    Adding randStream.py and Test_randStream.py

---
+++ src/wholecell/util/randStream.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+
+"""
+RandStream
+Wrapper over numpy.random.RandomState
+
+Adds method:
+ - stochasticRound: rounding of floats to integers weighted by decimal part
+
+Note:
+ - any size arguments from Matlab need to be carefully checked for intended behavior
+   - "rand(10)" in Matlab returns a 10x10 matrix
+   - "numpy.random.rand(10)" returns an array with 10 elements
+ - modifications have been made for 0-indexing in Python
+
+@author: Derek Macklin
+@organization: Covert Lab, Department of Bioengineering, Stanford University
+@date: Created 3/22/2013
+"""
+
+import numpy
+import numpy.random
+
+class randStream(object):
+	""" Random Stream """
+
+	def __init__(self, seed = None):
+		self.randStream = numpy.random.RandomState()
+		self.seed = seed
+		self.__initialState__ = self.randStream.get_state()
+		self.defaultStream = None
+
+	def reset(self, seed = None):
+		self.randStream.set_state(self.__initialState__)
+		if seed:
+			self.seed = seed
+
+	# def setDefault(self):
+	# 	# Store the default (global) random state
+	# 	self.defaultStream = numpy.random.get_state()
+
+	# 	# Set the global random state to have this random state
+	# 	numpy.random.set_state(self.randStream.get_state())
+
+	# def resetDefault(self):
+	# 	numpy.random.set_state(self.defaultStream)
+
+	def rand(self, *args):
+		return self.randStream.rand(*args)
+
+	# TODO: Change name to "randint"?
+	def randi(self, imax, size = None):
+		return self.randStream.randint(0, imax, size)
+		# return self.randStream.randint(1, imax + 1, size)
+
+	def randn(self, *args):
+		return self.randStream.randn(*args)
+
+	def randperm(self, n):
+		array = numpy.arange(n)
+		# array = numpy.arange(1, n + 1)
+		self.randStream.shuffle(array)
+		return array
+
+	def random(self):
+		raise Exception, "No numpy equivalent. Call appropriate function in numpy.random instead."
+
+	# TODO: Change name to "poisson"?
+	def poissrnd(self, Lambda, size = None):
+		return self.randStream.poisson(Lambda, size)
+
+	# TODO: Change name to "multinomial"?
+	def mnrnd(self, n, pvals, size = None):
+		return self.randStream.multinomial(n, pvals, size)
+
+	# TODO: Change name to "binomial"?
+	def binornd(self, n, p, size = None):
+		return self.randStream.binomial(n, p, size)
+
+	def randsample(self, n, k, replacement = False, w = None):
+		k = numpy.array(k, dtype = numpy.int)
+		if k.size != 1:
+			raise Exception, "Expect k to be a scalar"
+
+		if w != None:
+			# The w argument can be weights, but choice() needs strict probabilities
+			w_array = numpy.array(w, dtype = numpy.float)
+			if numpy.any(w_array < 0):
+				raise ValueError, "probabilities are not non-negative"
+			if numpy.any(w_array == numpy.Inf) or numpy.any(w_array == numpy.NaN):
+				raise ValueError, "expect finite weights"
+			if numpy.abs(numpy.sum(w_array)) < 1e-9:
+				raise ValueError, "weights sum to zero - cannot scale"
+			w_array /= numpy.sum(w_array)
+		else:
+			w_array = None
+
+		return self.randStream.choice(numpy.arange(n), k, replacement, w_array)
+		# return self.randStream.choice(numpy.arange(1, n + 1), k, replacement, w)
+
+	def randCounts(self, counts, N):
+		counts = numpy.array(counts)
+		if counts.shape == ():
+			counts = counts.reshape(1)
+		if numpy.any(counts < 0) or counts.dtype != numpy.dtype(numpy.int):
+			raise Exception, "counts must contain positive integers."
+		if N < 0:
+			raise Exception, "N must be positive."
+
+		cumsumCounts = numpy.cumsum(counts)
+		positiveSelect = True
+
+		if N > cumsumCounts[-1]:
+			raise Exception, "N must be at most the total available counts."
+
+		if N == cumsumCounts[-1]:
+			return counts
+		elif N > cumsumCounts[-1] / 2:
+			positiveSelect = False
+			N = cumsumCounts[-1] - N
+
+		selectedCounts = numpy.zeros(numpy.shape(counts))
+
+		for i in xrange(N):
+			idx = numpy.ravel(numpy.where(self.randi(cumsumCounts[-1]) + 1 <= cumsumCounts))[0]
+			selectedCounts[idx] += 1
+			cumsumCounts[idx:] -= 1
+
+		if not positiveSelect:
+			selectedCounts = counts - selectedCounts
+
+		return selectedCounts
+
+	def stochasticRound(self, value):
+		# value = numpy.copy(valueToRound)
+		value = numpy.array(value)
+		valueShape = value.shape
+		valueRavel = numpy.ravel(value)
+		roundUp = self.rand(valueRavel.size) < (valueRavel % 1)
+		valueRavel[roundUp] = numpy.ceil(valueRavel[roundUp])
+		valueRavel[~roundUp] = numpy.floor(valueRavel[~roundUp])
+		if valueShape != ():
+			return numpy.unravel_index(valueRavel, valueShape)
+		else:
+			return valueRavel
+
+	def randomlySelectRows(self, mat, prob):
+		nRndRows = self.stochasticRound(prob * numpy.shape(mat)[0])
+		return self.randomlySelectNRows(mat, nRndRows)
+
+	def randomlySelectNRows(self, mat, nRndRows = numpy.Inf):
+		# mat = numpy.copy(matToChooseFrom)
+		rndIdxs = numpy.sort(self.randsample(numpy.shape(mat)[0], numpy.min([numpy.shape(mat)[0], nRndRows]), False))
+		mat = mat[rndIdxs, :]
+		return mat, rndIdxs
+
+	@property
+	def type(self):
+		return self.randStream.get_state()[0]
+
+	@property
+	def seed(self):
+		pass
+
+	@seed.setter
+	def seed(self, value):
+		self.randStream.seed(value)
+
+	@property
+	def state(self):
+		return self.randStream.get_state()
+
+	@state.setter
+	def state(self, value):
+		self.randStream.set_state(value)
+
+	def __eq__(self, other):
+		stateSelf = self.state
+		stateOther = other.state
+
+		return (stateSelf[0] == stateOther[0]) and \
+			numpy.array_equal(stateSelf[1], stateOther[1]) and \
+			numpy.array_equal(stateSelf[2], stateOther[2]) and \
+			numpy.array_equal(stateSelf[3], stateOther[3])
---
+++ src_test/wholecell/util/Test_randStream.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+
+"""
+Test randStream.py
+
+@author: Derek Macklin
+@organization: Covert Lab, Department of Bioengineering, Stanford University
+@date: Created 3/22/2013
+"""
+
+import unittest
+import warnings
+
+import numpy
+import wholecell.util.randStream
+
+class Test_randStream(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		cls.randStream = wholecell.util.randStream.randStream(seed = 1)
+
+	@classmethod
+	def tearDownClass(cls):
+		pass
+
+	def setUp(self):
+		self.randStream.reset(1)
+
+	def tearDown(self):
+		pass
+
+	def test_randsample(self):
+		r = self.randStream
+
+		with self.assertRaises(AttributeError) as context:
+			r.randw(numpy.array([[0, 0], [0, 0]]))
+		self.assertEqual(context.exception.message, "'randStream' object has no attribute 'randw'")
+
+		# NOTE: In Matlab, no exception is thrown here (the returned array is merely empty)
+		with self.assertRaises(ValueError) as context:
+			self.assertEqual(numpy.zeros([0, 1]), r.randsample(2, 1, True, [0, 0]))
+		self.assertEqual(context.exception.message, "weights sum to zero - cannot scale")
+
+		with self.assertRaises(ValueError) as context:
+			r.randsample(2, 1, True, [-1, 0])
+		self.assertEqual(context.exception.message, "probabilities are not non-negative")
+
+		with self.assertRaises(ValueError) as context:
+			r.randsample(2, 1, True, [0, numpy.Inf])
+		self.assertEqual(context.exception.message, "expect finite weights")
+
+		with warnings.catch_warnings(record = True) as w:
+			with self.assertRaises(ValueError) as context:
+				r.randsample(2, 1, True, [0, numpy.NaN])
+			self.assertEqual(context.exception.message, "probabilities do not sum to 1")
+
+		with self.assertRaises(ValueError) as context:
+			r.randsample(2, -1, True, [0, 1])
+		self.assertEqual(context.exception.message, "negative dimensions are not allowed")
+
+		# NOTE: In Matlab, there is a test for integer-valued k
+		# However, numpy will just floor a float-valued k and use it
+
+		with self.assertRaises(OverflowError) as context:
+			r.randsample(2, numpy.Inf, True, [0, 1])
+		self.assertEqual(context.exception.message, "cannot convert float infinity to integer")
+
+		with self.assertRaises(ValueError) as context:
+			r.randsample(2, numpy.NaN, True, [0, 1])
+		self.assertEqual(context.exception.message, "cannot convert float NaN to integer")
+
+		with self.assertRaises(Exception) as context:
+			r.randsample(2, [1, 1], True, [0, 1])
+		self.assertEqual(context.exception.message, "Expect k to be a scalar")
+
+		# Sampling
+		self.assertEqual(1, r.randsample(7, 1, True, [0, 1, 1, 1, 1, 0, 2]).size)
+		self.assertEqual(0, r.randsample(7, 0, True, [0, 1, 1, 1, 1, 0, 2]).size)
+		self.assertEqual(10, r.randsample(7, 10, True, [0, 1, 1, 1, 1, 0, 2]).size)
+
+		with self.assertRaises(ValueError) as context:
+			r.randsample(7, 10, False, [0, 1, 1, 1, 1, 0, 2])
+		self.assertEqual(context.exception.message, "Cannot take a larger sample than population when 'replace=False'")
+
+		self.assertTrue(numpy.array_equal(numpy.array([6, 2, 1, 3, 4]), r.randsample(7, 5, False, [0, 1, 1, 1, 1, 0, 2])))
+		self.assertTrue(numpy.array_equal(numpy.array([1, 2, 6, 4, 3]), r.randsample(7, 5, False, numpy.transpose([0, 1, 1, 1, 1, 0, 2]))))
+
+		# Fairness
+		counts = numpy.zeros([2, 1])
+		for i in xrange(100):
+			idx = r.randsample(2, 1, True, [1, 1])
+			counts[idx] += 1
+		self.assertTrue(numpy.max(counts) - numpy.min(counts) < 0.12 * numpy.max(counts))
+
+	def test_randomlySelectRows(self):
+		r = self.randStream
+
+		mat = numpy.reshape(numpy.arange(10), [5, 2])
+		self.assertTrue(numpy.array_equal(mat, r.randomlySelectRows(mat, 1)[0]))
+
+		mat = numpy.reshape(numpy.arange(4), [2, 2])
+		counts = numpy.zeros([2, 1])
+		for i in xrange(1000):
+			selMat, selIdx = r.randomlySelectRows(mat, 0.5)
+			self.assertTrue(numpy.array_equal(selMat, mat[selIdx, :]), "Randomly selected rows should correspond to randomly selected indices")
+			self.assertTrue(numpy.array_equal(selIdx, numpy.unique(selIdx)), "Random selection should not be done with replacement")
+			counts[selIdx] += 1
+		self.assertTrue(numpy.max(counts) - numpy.min(counts) < 0.11 * numpy.max(counts), "Random selection should be unbiased")
+
+	def test_randomlySelectNRows(self):
+		r = self.randStream
+
+		mat = numpy.reshape(numpy.arange(10), [5, 2])
+		self.assertTrue(numpy.array_equal(mat, r.randomlySelectRows(mat, 1)[0]))
+		self.assertEqual(5, r.randomlySelectNRows(mat, 5)[0].shape[0])
+
+		mat = numpy.reshape(numpy.arange(4), [2, 2])
+		counts = numpy.zeros(2)
+		for i in xrange(1000):
+			selMat, selIdx = r.randomlySelectNRows(mat, 1)
+			self.assertTrue(numpy.array_equal(selMat, mat[selIdx, :]), "Randomly selected rows should correspond to randomly selected indices")
+			self.assertTrue(numpy.array_equal(selIdx, numpy.unique(selIdx)), "Random selection should not be done with replacement")
+			counts[selIdx] += 1
+		self.assertTrue(numpy.max(counts) - numpy.min(counts) < 0.11 * numpy.max(counts), "Random selection should be unbiased")
+
+	def test_randCounts(self):
+		r = self.randStream
+
+		with self.assertRaises(Exception) as context:
+			r.randCounts(2, -1)
+		self.assertEqual(context.exception.message, "N must be positive.")
+
+		self.assertEqual(0, r.randCounts(2, 0))
+		self.assertEqual(1, r.randCounts(2, 1))
+		self.assertEqual(2, r.randCounts(2, 2))
+
+		self.assertTrue(numpy.array_equal([2, 2, 3], r.randCounts([2, 2, 3], 7)))
+		with self.assertRaises(Exception) as context:
+			r.randCounts([2, 2, 3], 8)
+		self.assertEqual(context.exception.message, "N must be at most the total available counts.")	
+
+		self.assertEqual(2, numpy.sum(r.randCounts([2, 2, 3], 2)))
+
+		self.assertTrue(numpy.all([2, 2, 3] >= r.randCounts([2, 2, 3], 6)))
+
+		counts = numpy.zeros(2)
+		for i in xrange(1000):
+
+			tmp = r.randCounts([2, 2], 1)
+			self.assertEqual(1, numpy.sum(tmp))
+			counts += tmp
+
+		self.assertTrue(numpy.max(counts) - numpy.min(counts) < 0.10 * numpy.max(counts), "Random selection should be unbiased")
+
+	def test_getters(self):
+		r = self.randStream
+		self.assertEqual('MT19937', r.type)
+		self.assertEqual(5, len(r.state))
+
+	def test_setters(self):
+		r = self.randStream
+		state = r.state
+		r.randi(5)
+		self.assertFalse(numpy.array_equal(state[1], r.state[1]))
+		r.state = state
+		self.assertTrue(numpy.array_equal(state[1], r.state[1]))
+		r.seed = 1234
+		self.assertFalse(numpy.array_equal(state[1], r.state[1]))
