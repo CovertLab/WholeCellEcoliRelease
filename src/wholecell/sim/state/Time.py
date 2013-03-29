942ef7c06ca34613419650904b078707fa71a5bc
macklin <macklin@server>
2013-03-29 16:15:13 -0700

    Adding Time class in Time.py. Untested.

---
+++ src/wholecell/sim/state/Time.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+Time
+
+Time state variable. Represents the current time lapsed since the start of the simulation in seconds.
+
+@author: Derek Macklin
+@organization: Covert Lab, Department of Bioengineering, Stanford University
+@date: Created 3/29/2013
+"""
+
+import numpy
+
+import wholecell.sim.state
+
+class Time(wholecell.sim.state.State):
+	""" Time """
+
+	meta = {
+		"id": "Time",
+		"name": "Time",
+		"dynamics": ["value"],
+		"units": {"value": "s"}
+	}
+
+	def __init__(self, *args, **kwargs):
+		self.value = None
+		super(Time, self).__init__(*args, **kwargs)
+
+	def allocate(self):
+		super(Time, self).allocate()
+
+		self.value = numpy.zeros(1)
+
+	def calcInitialConditions(self):
+		self.value = 0
