e2b3a69125a5094792798911495dfdf5bf579be3
macklin <macklin@server>
2013-03-29 16:50:36 -0700

    Adding Constants.py

---
+++ src/wholecell/util/Constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+Constants
+
+Utility class with values of important biophysical constants.
+
+@author: Derek Macklin
+@organization: Covert Lab, Department of Bioengineering, Stanford University
+@date: Created 3/29/2013
+"""
+
+class Constants:
+	nAvogadro = 6.022e23  # Avogadro's number
