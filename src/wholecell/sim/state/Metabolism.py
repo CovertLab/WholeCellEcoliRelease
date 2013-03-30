6377b7a4c7abfcd57430046a2a4020b2e36cf2ec
macklin <macklin@server>
2013-03-29 18:18:23 -0700

    Adding Metabolism class in Metabolism.py. Untested.

---
+++ src/wholecell/sim/state/Metabolism.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+"""
+Metabolism
+
+Metabolism state variable. Represents the instantaneous growth rate (fg/h) and metabolic reaction fluxes (reactions/s)
+
+@author: Derek Macklin
+@organization: Covert Lab, Department of Bioengineering, Stanford University
+@date: Created 3/29/2013
+"""
+
+import numpy
+
+import wholecell.sim.state.State
+
+class Metabolism(wholecell.sim.state.State.State):
+	""" Metabolism """
+
+	meta = {
+		"id": "Metabolism",
+		"name": "Metabolism",
+		"dynamics": ["growth", "fluxes"],
+		"units": {
+			"growth": "fg/h",
+			"fluxes": "reactions/s"
+			}
+	}
+
+	# Constructor
+	def __init__(self, *args, **kwargs):
+		# State, process references
+		self.moleculeCounts = None
+		self.metabolism = None
+
+		self.growth = None
+		self.fluxes = None
+
+		super(Metabolism, self).__init__(*args, **kwargs)
+
+	# Construct state-process graph
+	def initialize(self, sim, kb):
+		super(Metabolism, self).initialize(sim, kb)
+
+		self.moleculeCounts = sim.getState("MoleculeCounts")
+		self.metabolism = sim.getProcess("Metabolism")
+
+		self.reactionIds = [reaction["id"] for reaction in kb.reactions]
+		self.reactionNames = [reaction["name"] for reaction in kb.reactions]
+
+	# Allocate memory
+	def allocate(self):
+		super(Metabolism, self).allocate()
+
+		self.growth = numpy.zeros(1)
+		self.fluxes = numpy.zeros(len(self.reactionIds))
+
+	# Calculate initial conditions
+	def calcInitialConditions(self):
+		mc = self.moleculeCounts
+		met = self.metabolism
+
+		bounds = met.calcFluxBounds(
+			mc.counts(met.metabolite.mapping), mc.counts(met.enzyme.mapping)
+			)
+		self.growth, self.fluxes = met.calcGrowthRate(bounds)
