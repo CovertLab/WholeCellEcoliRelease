4d9374e4e3d3a5c37dd602e75858e571d70114ac
macklin <macklin@server>
2013-04-04 10:31:12 -0700

    Adding ProteinMaturation class in ProteinMaturation.py. Untested.

---
+++ src/wholecell/sim/process/ProteinMaturation.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+"""
+ProteinMaturation
+
+Protein maturation sub-model. Encodes molecular simulation of protein maturation: processing, localization
+
+@author: Derek Macklin
+@organization: Covert Lab, Department of Bioengineering, Stanford University
+@date: Created 4/4/2013
+"""
+
+import numpy
+
+import wholecell.sim.process.Process
+
+class ProteinMaturation(wholecell.sim.process.Process.Process):
+	""" ProteinMaturation """
+
+	meta = {
+		"id": "ProteinMaturation",
+		"name": "ProteinMaturation",
+	}
+
+	# Constructor
+	def __init__(self):
+		# References to states
+		self.nascentProteinMonomer = None
+		self.matureProteinMonomer = None
+		
+		super(ProteinMaturation, self).__init__()
+
+	# Construct object graph
+	def initialize(self, sim, kb):
+		super(ProteinMaturation, self).initialize(sim, kb)
+
+		monomers = [x for x in kb.proteins if x["monomer"] == True]
+
+		self.nascentProteinMonomer = sim.getState("MoleculeCounts").addPartition(self,
+			[x["id"] + ":nascent[c]" for x in monomers],
+			self.calcReqNascentProteinMonomer)
+
+		self.matureProteinMonomer = sim.getState("MoleculeCounts").addPartition(self,
+			[x["id"] + ":mature[" + x["compartment"] + "]" for x in monomers],
+			self.calcReqMatureProteinMonomer)
+
+	# Calculate needed proteins
+	def calcReqNascentProteinMonomer(self):
+		return numpy.ones(self.nascentProteinMonomer.fullCounts.shape)
+
+	# Calculate needed proteins
+	def calcReqMatureProteinMonomer(self):
+		return numpy.zeros(self.matureProteinMonomer.fullCounts.shape)
+
+	# Calculate temporal evolution
+	def evolveState(self):
+		self.matureProteinMonomer.counts += self.nascentProteinMonomer.counts
+		self.nascentProteinMonomer.counts[:] = 0
