a71dd4fed7c6da616915be2f6590b5aa506ed040
macklin <macklin@server>
2013-04-08 15:51:36 -0700

    Fixing typos in Shell.py.

--- src/wholecell/sim/logger/Shell.py
+++ src/wholecell/sim/logger/Shell.py
@@ -26,8 +26,8 @@ class Shell(wholecell.sim.logger.Logger.Logger):
 		# Array of columns
 		self.columns = [
 			{"header": "Time (s)", "state": "Time", "property": "value", "length": 8, "format": "d", "sum": False},
-			{"header": "Mass (fg)", "state": "Mass", "property": "cell", "length": 9, "format": "0.2f", "sum": True},
-			{"header": "Growth (fg/h)", "state": "Metabolism", "property": "growth", "length": 13, "format": "0.2f", "sum": False}
+			{"header": "Mass (fg)", "state": "Mass", "property": "cell", "length": 9, "format": ".2f", "sum": True},
+			{"header": "Growth (fg/h)", "state": "Metabolism", "property": "growth", "length": 13, "format": ".2f", "sum": False}
 		]
 
 		# Collect Metadata
@@ -60,7 +60,7 @@ class Shell(wholecell.sim.logger.Logger.Logger):
 			c = self.columns[iColumn]
 			if iColumn > 0:
 				sys.stdout.write("  ")
-			val = getattr(sim.states[c.stateIdx], c["property"])
+			val = getattr(sim.states[c["stateIdx"]], c["property"])
 			if c["sum"]:
 				val = numpy.sum(val)
 			sys.stdout.write(("%" + str(self.columns[iColumn]["length"]) + c["format"]) % val)
