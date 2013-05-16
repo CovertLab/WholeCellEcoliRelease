2e87b4afa1b09a4f362b840b92cb164a14d47110
nruggero <nruggero@server>
2013-05-16 11:02:32 -0700

    Added location compartment dict and started parsing metabolites

--- data/src_parsing/parse_metabolites.py
+++ data/src_parsing/parse_metabolites.py
@@ -9,19 +9,44 @@ import numpy as np
 class parse_metabolites:
 	def __init__(self):
 		self.metDict = {}
+		self.compartmentDict = {}
 
-
-
+		self.defineCompartments()
+		self.loadOrthData()
 
 		self.writeMetaboliteCSV()
 
+
+	def defineCompartments(self):
+		self.compartmentDict['Cytosol'] == 'CCO-CYTOSOL'
+		self.compartmentDict['Periplasm'] == 'CCO-PERI-BAC'
+		self.compartmentDict['Extra-organism'] == 'CCO-EXTRACELLULAR'
+
+	def loadOrthData(self):
+		with open(os.path.join(os.environ['PARWHOLECELLPY'], 'data', 'raw', 'Orth_metabolites.csv'),'rb') as csvfile:
+			csvreader = csv.reader(csvfile, delimiter='\t', quotechar='"')
+
+			firstRow = True
+			for row in csvreader:
+				if firstRow:
+					firstRow = False
+				else:
+					metId = row[0][:-2]
+					if self.metDict.has_key(metName):
+						# Append this badboy
+						pass
+					else:
+						met = metabolite()
+						met.metId = row[0][:-2]
+						met.name = row[1]
+						met.empricalFormula = row[2]
+						self.compartments
 
 
 	def writeMetaboliteCSV(self):
 		with open(os.path.join(os.environ['PARWHOLECELLPY'], 'data', 'parsed', 'metabolites.csv'),'wb') as csvfile:
 			csvwriter = csv.writer(csvfile, delimiter='\t', quotechar='"')
 
-			
 			csvwriter.writerow(['ID', 'frameId', 'Empirical formula', 'Type', 'SMILES', 'Charge', 'Weight', 'Hydophobic', 'Media concentration', 'Maximum exchange rate (mmol/gDCW/h)','Comments'])
 
 
