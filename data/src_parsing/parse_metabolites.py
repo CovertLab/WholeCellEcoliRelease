5e26dfe0a0b035320ed89c4d2ee31744492ee532
nruggero <nruggero@server>
2013-05-16 11:44:45 -0700

    Added conversion from Orth compartment names to Ecocyc frameIds. Parsed comparment information. Write to csv.

--- data/src_parsing/parse_metabolites.py
+++ data/src_parsing/parse_metabolites.py
@@ -5,6 +5,7 @@ import os
 import ipdb
 import sets
 import numpy as np
+import json
 
 class parse_metabolites:
 	def __init__(self):
@@ -18,9 +19,9 @@ class parse_metabolites:
 
 
 	def defineCompartments(self):
-		self.compartmentDict['Cytosol'] == 'CCO-CYTOSOL'
-		self.compartmentDict['Periplasm'] == 'CCO-PERI-BAC'
-		self.compartmentDict['Extra-organism'] == 'CCO-EXTRACELLULAR'
+		self.compartmentDict['Cytosol'] = 'CCO-CYTOSOL'
+		self.compartmentDict['Periplasm'] = 'CCO-PERI-BAC'
+		self.compartmentDict['Extra-organism'] = 'CCO-EXTRACELLULAR'
 
 	def loadOrthData(self):
 		with open(os.path.join(os.environ['PARWHOLECELLPY'], 'data', 'raw', 'Orth_metabolites.csv'),'rb') as csvfile:
@@ -32,7 +33,7 @@ class parse_metabolites:
 					firstRow = False
 				else:
 					metId = row[0][:-2]
-					if self.metDict.has_key(metName):
+					if self.metDict.has_key(metId):
 						# Append this badboy
 						pass
 					else:
@@ -40,15 +41,22 @@ class parse_metabolites:
 						met.metId = row[0][:-2]
 						met.name = row[1]
 						met.empricalFormula = row[2]
-						met.compartments[self.compartmentDict[row[5]]] = {'charge' int(row[4]): , 'charged form' : row[3]}
+						met.compartments[self.compartmentDict[row[5]]] = {'charge' : int(row[4]), 'charged form' : row[3]}
+
+					self.metDict[met.metId] = met
 
 
 	def writeMetaboliteCSV(self):
 		with open(os.path.join(os.environ['PARWHOLECELLPY'], 'data', 'parsed', 'metabolites.csv'),'wb') as csvfile:
 			csvwriter = csv.writer(csvfile, delimiter='\t', quotechar='"')
 
-			csvwriter.writerow(['ID', 'frameId', 'Empirical formula', 'Type', 'SMILES', 'Charge', 'Weight', 'Hydophobic', 'Media concentration', 'Maximum exchange rate (mmol/gDCW/h)','Comments'])
-
+			csvwriter.writerow(['ID', 'frameId', 'Empirical formula', 'Compartment data', 'Weight', 'SMILES', 'Hydrophobic', 'Media concentration', 'Maximum exchange rate (mmol/gDCW/h)','Comments'])
+			
+			keys = self.metDict.keys()
+			keys.sort()
+			for key in keys:
+				m = self.metDict[key]
+				csvwriter.writerow([m.metId, m.frameId, m.empiricalFormula, json.dumps(m.compartments), m.weight, m.SMILES, m.hydrophobic, m.mediaConc, m.biomassConc, m.exchangeRate])
 
 
 
