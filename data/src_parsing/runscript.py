f550bd4cca6be35ac525dd2a0fb2f0cdbad6e1e0
nruggero <nruggero@server>
2013-05-21 10:18:34 -0700

    Added intermediate parsing functions to runscript.

--- data/src_parsing/runscript.py
+++ data/src_parsing/runscript.py
@@ -6,11 +6,17 @@ import re
 
 
 def main():
+	parseIntermediateFiles()
+
+	parseGenes()
+
+# Intermediate file functions
+def parseIntermediateFiles():
 	# Load and save gene synonym dictionary
 	parseGeneSynonymDictionary()
-
-
-
+	parseProteionMonomerLocations()
+	parseGeneProductUnmodifiedForm()
+	parseRnaTypes()
 
 def parseGeneSynonymDictionary():
 	synDict = {}
@@ -43,9 +49,89 @@ def parseGeneSynonymDictionary():
 						synDict[syn.lower()] = name.lower()
 						synDictFrameId[syn.lower()] = frameId
 
-	with open(os.path.join(os.environ['PARWHOLECELLPY'], 'data', 'intermediate', 'gene_synonyms.json'),'wb') as jsonfile:
+	with open(os.path.join(os.environ['PARWHOLECELLPY'], 'data', 'intermediate', 'gene_name_synonyms.json'),'wb') as jsonfile:
 		jsonfile.write(json.dumps(synDict))
+
+	with open(os.path.join(os.environ['PARWHOLECELLPY'], 'data', 'intermediate', 'gene_frameId_synonyms.json'),'wb') as jsonfile:
 		jsonfile.write(json.dumps(synDictFrameId))
+
+def parseProteionMonomerLocations():
+	proteinLocationDict = {}
+
+	with open(os.path.join(os.environ['PARWHOLECELLPY'], 'data', 'raw', 'Ecocyc_protein_location.csv'),'rb') as csvfile:
+		csvreader = csv.reader(csvfile, delimiter='\t')
+
+		for row in csvreader:
+			protFrameId = row[1]
+			locationInformation = splitBigBracket(row[2])
+			locationList= []
+			for loc in locationInformation:
+				if loc != '':
+					param = splitSmallBracket(loc)
+					if param['description'] != []:
+						locationList.append(param['description'])
+
+					proteinLocationDict[protFrameId] = locationList
+
+	with open(os.path.join(os.environ['PARWHOLECELLPY'], 'data', 'intermediate', 'proteinLocations.json'),'wb') as jsonfile:
+		jsonfile.write(json.dumps(proteinLocationDict))
+
+def parseGeneProductUnmodifiedForm():
+	unmodifiedForm = {}
+	with open(os.path.join(os.environ['PARWHOLECELLPY'], 'data', 'raw', 'Ecocyc_rna_mod_tree.csv'),'rb') as csvfile:
+		csvreader = csv.reader(csvfile, delimiter='\t')
+
+		for row in csvreader:
+			if row[3] != '':
+				if row[4] != '':
+					unmodifiedForm[row[1]] = True
+				else:
+					unmodifiedForm[row[1]] = False
+
+	with open(os.path.join(os.environ['PARWHOLECELLPY'], 'data', 'raw', 'Ecocyc_protein_mod_tree.csv'),'rb') as csvfile:
+		csvreader = csv.reader(csvfile, delimiter='\t')
+
+		for row in csvreader:
+			if row[2] != '':
+				unmodifiedForm[row[1]] = True
+			else:
+				unmodifiedForm[row[1]] = False
+
+	with open(os.path.join(os.environ['PARWHOLECELLPY'], 'data', 'intermediate', 'gene_product_unmodifiedForm.json'),'wb') as jsonfile:
+		jsonfile.write(json.dumps(unmodifiedForm))
+
+def parseRnaTypes():
+	with open(os.path.join(os.environ['PARWHOLECELLPY'], 'data', 'intermediate', 'gene_product_unmodifiedForm.json'),'rb') as jsonfile:
+		unmodifiedForm = json.loads(jsonfile.read())
+
+	rnaType = {}
+	with open(os.path.join(os.environ['PARWHOLECELLPY'], 'data', 'raw', 'Ecocyc_rna_mod_tree.csv'),'rb') as csvfile:
+		csvreader = csv.reader(csvfile, delimiter='\t')
+
+		for row in csvreader:
+			if row[3] != '':
+				if row[1].count('RRNA') > 0:
+					rnaType[row[1]] = 'rRNA'
+				elif row[1].count('tRNA') > 0 and not unmodifiedForm[row[1]]:
+					rnaType[row[1]] = 'tRNA'
+				elif not unmodifiedForm[row[1]]:
+					rnaType[row[1]] = 'miscRNA'
+
+	with open(os.path.join(os.environ['PARWHOLECELLPY'], 'data', 'intermediate', 'rnaTypes.json'),'wb') as jsonfile:
+		jsonfile.write(json.dumps(rnaType))
+
+# Utility functions
+def splitBigBracket(s):
+	s = s[2:-2]
+	s = s.replace('"','')
+	s = s.split(') (')
+	return s
+
+def splitSmallBracket(s):
+	s = s.split(', ')
+	frameId = s[0]
+	description = s[1]
+	return {'frameId' : frameId, 'description' : description}
 
 if __name__ == "__main__":
     main()
