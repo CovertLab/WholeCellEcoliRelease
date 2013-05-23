9695497ca49bba159d20a9d3ee86b7a3e1417753
nruggero <nruggero@server>
2013-05-23 13:35:03 -0700

    Added sequence parser

--- data/src_parsing/generateSequencesForGRAVY.py
+++ data/src_parsing/generateSequencesForGRAVY.py
@@ -4,8 +4,29 @@ import os
 import csv
 import ipdb
 
-main():
-	pass
+def main():
+	sequence = loadSequence()
 
+	geneDict
+	with open(os.path.join(os.environ['PARWHOLECELLPY'], 'data', 'raw', 'Ecocyc_genes.csv'),'rb') as csvfile:
+		csvreader = csv.reader(csvfile, delimiter='\t')
+		csvreader.next()
+		for row in csvreader:
+			pass
+
+
+def loadSequence():
+	sequence = []
+	with open(os.path.join(os.environ['PARWHOLECELLPY'], 'data', 'raw', 'sequence.txt')) as txtfile:
+		firstLine = True
+		for row in txtfile:
+			if firstLine:
+				firstLine = False
+			else:
+				sequence.append(row.strip())
+	return "".join(sequence)
+
+
+	
 if __name__ == "__main__":
     main()
