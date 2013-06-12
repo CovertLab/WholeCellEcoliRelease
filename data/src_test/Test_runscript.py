eeff15a816fdb76bb477ae275bc249a28ca30101
nruggero <nruggero@server>
2013-06-12 11:06:32 -0700

    Renamed src_parsing directories to just src

---
+++ data/src_test/Test_runscript.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import nose.plugins.attrib as noseAttrib
+import nose.tools as noseTools
+import unittest
+
+import data.src.runscript as r
+
+class Test_Simulation(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		pass
+
+	@classmethod
+	def tearDownClass(cls):
+		pass
+
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	def test_construction(self):
+		pass
