925104f7cfda0c99f6e0f518ac8aa78cb7756072
nruggero <nruggero@server>
2013-06-26 11:58:22 -0700

    Fixing index

--- data/src/runscript.py
+++ data/src/runscript.py
@@ -1973,11 +1973,10 @@ class reactionParser:
 			csvreader.next()
 			for row in csvreader:
 				proteinLocations[row[0]] = json.loads(row[2])
-				modifiedForm = json.loads(row[4])
+				modifiedForm = json.loads(row[5])
 				if len(modifiedForm):
 					for m in modifiedForm:
 						proteinLocations[m] = json.loads(row[2])
-
 		return proteinLocations
 
 	def loadMonomerToComplex(self):
