6e72142bc50c3b6ca178964d9f8d13923b0bf974
nruggero <nruggero@server>
2013-08-23 11:40:19 -0700

    Commented other functions. Fixed protein validation

--- src/wholecell/kb/KnowledgeBaseValidator.py
+++ src/wholecell/kb/KnowledgeBaseValidator.py
@@ -32,46 +32,42 @@ class KnowledgeBaseValidator(object):
 		# Other
 
 
-		# Other
-		self.validateFrameId()
+	# def validateFrameId(self):
+	# 	# Validates that all frameid's are unique
+	# 	# TODO: Redo this so that it uses the id's actually in the KB
+	# 	frameIds = []
+	# 	fileNames = []
+	# 	self.getFrameIds('genes.csv', frameIds, fileNames)
+	# 	self.getFrameIds('locations.csv', frameIds, fileNames)
+	# 	self.getFrameIds('metabolites.csv', frameIds, fileNames)
+	# 	self.getFrameIds('promoters.csv', frameIds, fileNames)
+	# 	self.getFrameIds('proteinComplexes_modified.csv', frameIds, fileNames)
+	# 	self.getFrameIds('proteinComplexes.csv', frameIds, fileNames)
+	# 	self.getFrameIds('proteinMonomers_modified.csv', frameIds, fileNames)
+	# 	self.getFrameIds('proteinMonomers.csv', frameIds, fileNames)
+	# 	self.getFrameIds('reactions.csv', frameIds, fileNames)
+	# 	self.getFrameIds('rna_modified.csv', frameIds, fileNames)
+	# 	self.getFrameIds('rna.csv', frameIds, fileNames)
+	# 	self.getFrameIds('terminators.csv', frameIds, fileNames)
+	# 	self.getFrameIds('transcriptionUnits.csv', frameIds, fileNames)
 
+	# def getFrameIds(self, fileName, frameIds, fileNames):
+	# 	fileName = self.kb.dataFileDir + os.sep + fileName
+	# 	if not os.path.isfile(fileName):
+	# 		raise Exception, "%s is missing" % fileName
 
-	def validateFrameId(self):
-		# Validates that all frameid's are unique
-		# TODO: Redo this so that it uses the id's actually in the KB
-		frameIds = []
-		fileNames = []
-		self.getFrameIds('genes.csv', frameIds, fileNames)
-		self.getFrameIds('locations.csv', frameIds, fileNames)
-		self.getFrameIds('metabolites.csv', frameIds, fileNames)
-		self.getFrameIds('promoters.csv', frameIds, fileNames)
-		self.getFrameIds('proteinComplexes_modified.csv', frameIds, fileNames)
-		self.getFrameIds('proteinComplexes.csv', frameIds, fileNames)
-		self.getFrameIds('proteinMonomers_modified.csv', frameIds, fileNames)
-		self.getFrameIds('proteinMonomers.csv', frameIds, fileNames)
-		self.getFrameIds('reactions.csv', frameIds, fileNames)
-		self.getFrameIds('rna_modified.csv', frameIds, fileNames)
-		self.getFrameIds('rna.csv', frameIds, fileNames)
-		self.getFrameIds('terminators.csv', frameIds, fileNames)
-		self.getFrameIds('transcriptionUnits.csv', frameIds, fileNames)
-
-	def getFrameIds(self, fileName, frameIds, fileNames):
-		fileName = self.kb.dataFileDir + os.sep + fileName
-		if not os.path.isfile(fileName):
-			raise Exception, "%s is missing" % fileName
-
-		with open(fileName, "r") as csvfile:
-			dr = csv.DictReader(csvfile, delimiter = "\t")
-			for row in dr:
-				if row['Frame ID'] in frameIds:
-					# TODO: Change back into an exception once we hear back from Ecocyc about the one error here
-					#raise Exception, '%s already in use as Frame ID!' % row['Frame ID']
-					print '%s already in use as Frame ID!' % row['Frame ID']
-					print 'frameid that offends is in filename %s ' % fileName
-					print 'frameid in conflict is in filename %s ' % fileNames[frameIds.index(row['Frame ID'])]
-				else:
-					frameIds.append(row['Frame ID'])
-					fileNames.append(fileName)
+	# 	with open(fileName, "r") as csvfile:
+	# 		dr = csv.DictReader(csvfile, delimiter = "\t")
+	# 		for row in dr:
+	# 			if row['Frame ID'] in frameIds:
+	# 				# TODO: Change back into an exception once we hear back from Ecocyc about the one error here
+	# 				#raise Exception, '%s already in use as Frame ID!' % row['Frame ID']
+	# 				print '%s already in use as Frame ID!' % row['Frame ID']
+	# 				print 'frameid that offends is in filename %s ' % fileName
+	# 				print 'frameid in conflict is in filename %s ' % fileNames[frameIds.index(row['Frame ID'])]
+	# 			else:
+	# 				frameIds.append(row['Frame ID'])
+	# 				fileNames.append(fileName)
 
 	def validateProteins(self):
 		# Validate datatypes
@@ -80,7 +76,7 @@ class KnowledgeBaseValidator(object):
 						 'composition': [list],
 						 'formationProcess': [str],
 						 'geneId': [str],
-						 'id': [str],
+						 'id': [str, unicode],
 						 'location': [str],
 						 'modifiedForm': [bool],
 						 'modifiedForms': [list],
@@ -89,41 +85,49 @@ class KnowledgeBaseValidator(object):
 						 'name': [str],
 						 'ntCount': [numpy.ndarray],
 						 'rnaId': [str],
-						 'seq': [str],
+						 'seq': [str, unicode],
 						 'unmodifiedForm': [None, str]}
 
 		s = ''
 		for protein in self.kb.proteins:
 			for fieldName in fieldDataType.iterkeys():
-				isOk = False
-				for allowedType in fieldDataType[fieldName]:
-					if isinstance(protein[fieldName], allowedType):
-						isOk = True
-				if not isOk:
-					s += '%s has field %s that is invalid!\n' % (protein['id'], fieldName)
+				hasField = False
+				if protein.has_key(fieldName):
+					hasField = True
+				else:
+					s += '%s is missing field "%s"!\n' % (protein['id'], fieldName)
+
+				if hasField:
+					isOk = False
+					for allowedType in fieldDataType[fieldName]:
+						if inspect.isclass(allowedType):
+							if isinstance(protein[fieldName], allowedType):
+								isOk = True
+						elif protein[fieldName] == allowedType:
+							isOk = True
+					if not isOk:
+						s += '%s has field "%s" that is invalid with value "%s"! Has type %s requires type %s.\n' % (protein['id'], fieldName, str(protein[fieldName]), str(type(protein[fieldName])), str(fieldDataType[fieldName]))
 
 		if len(s):
 			raise Exception, s
 
+	# def validateMetabolicNetwork(self):
+	# 	# Validate all metabolites are used in the metabolic network
+	# 	metabolites = set([m['id'] for m in self.metabolites])
+	# 	metabolitesUsed = set(list(itertools.chain(*[[met['molecule'] for met in rxn['stoichiometry']] for rxn in self.reactions])))
 
-	
-	def validateMetabolicNetwork(self):
-		# Validate all metabolites are used in the metabolic network
-		metabolites = set([m['id'] for m in self.metabolites])
-		metabolitesUsed = set(list(itertools.chain(*[[met['molecule'] for met in rxn['stoichiometry']] for rxn in self.reactions])))
+	# 	if len(metaboltiesUsed.difference(metabolites)):
+	# 		raise Exception, 'Reactions use metabolites not in KnowledgeBase\n'
 
-		if len(metaboltiesUsed.difference(metabolites)):
-			raise Exception, 'Reactions use metabolites not in KnowledgeBase\n'
+	# 	# TODO: Check inverse difference. SHould be zero if you count all the subunits of complexes and stuff.
+	# 	problemMet = list(metabolites.difference(metabolitesUsed))
 
-		# TODO: Check inverse difference. SHould be zero if you count all the subunits of complexes and stuff.
-		problemMet = list(metabolites.difference(metabolitesUsed))
-
-		# Validate all reactions have valid metabolites
+	# 	# Validate all reactions have valid metabolites
 
 
-		# Validate all reactions occur in allowed compartments
+	# 	# Validate all reactions occur in allowed compartments
 
-	def validateMetabolites(self):
-		pass
+	# def validateMetabolites(self):
+	# 	pass
 
 
