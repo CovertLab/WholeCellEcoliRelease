7be942f79f7d9e3b904234aa3367d63408a31882
nruggero <nruggero@server>
2013-08-28 10:52:32 -0700

    Removed monomer and unmodified form

--- src/wholecell/kb/KnowledgeBase.py
+++ src/wholecell/kb/KnowledgeBase.py
@@ -173,7 +173,6 @@ class KnowledgeBase(object):
 					"id": g["rnaId"],
 					"name": "",
 					"expression": float(row["expression"]),
-					"modifiedForm": False,
 					"modifiedForms": [],
 					"unmodifiedForm": None,
 					"composition": [],
