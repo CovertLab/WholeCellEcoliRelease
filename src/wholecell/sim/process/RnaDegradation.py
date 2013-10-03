7895d79d6a36f710e8fc6b90a8ea290a0af22dca
macklin <macklin@server>
2013-10-03 10:01:27 -0700

    Commenting out print statement in RnaDegradation.py.

--- src/wholecell/sim/process/RnaDegradation.py
+++ src/wholecell/sim/process/RnaDegradation.py
@@ -92,4 +92,4 @@ class RnaDegradation(wholecell.sim.process.Process.Process):
 		self.metabolite.counts += numpy.dot(self.rnaDegSMat, self.rna.counts)
 		self.rna.counts[:] = 0
 
-		print "NTP recycling: %s" % str(self.metabolite.counts[self.metabolite.idx["ntps"]])
+#		print "NTP recycling: %s" % str(self.metabolite.counts[self.metabolite.idx["ntps"]])
