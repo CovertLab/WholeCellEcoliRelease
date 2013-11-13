2c58b5c70d19157f2bacbf72d34ea79b2d0f062d
nruggero <nruggero@server>
2013-11-13 10:54:57 -0800

    Commented out script code

--- src/wholecell/sim/state/uniques.py
+++ src/wholecell/sim/state/uniques.py
@@ -162,15 +162,15 @@ class MoleculeUniqueMeta(type):
 
 
 ###
-class enz4Unique(object):
-	registrationId = "enz4"
-	__metaclass__ = MoleculeUniqueMeta
+# class enz4Unique(object):
+# 	registrationId = "enz4"
+# 	__metaclass__ = MoleculeUniqueMeta
 
-#	def __init__(self, uniqueIdx):
-#		self._uniqueIdx = uniqueIdx
+# #	def __init__(self, uniqueIdx):
+# #		self._uniqueIdx = uniqueIdx
 
-	def attr1(self):
-		return self._container._uniqueDict[self._molRowIdx][self._molColIdx]["attr4_1"][self._uniqueIdx]
+# 	def attr1(self):
+# 		return self._container._uniqueDict[self._molRowIdx][self._molColIdx]["attr4_1"][self._uniqueIdx]
 
 # class rnas(object):
 # 	registrationType = "rnas"
@@ -299,47 +299,47 @@ class RNAP(object):
 	def rnaLocsIs(self, i, val): self._rnaLocs[i] = val
 	def rnaLocsIncr(self, i, val): self._rnaLocs[i] += val
 
-numpy.random.seed(1)
-chrLen = 5e6
-dna = "".join(["ACGU"[x] for x in numpy.random.choice(numpy.arange(4), size = chrLen)])
-rnaps = [RNAP() for x in xrange(500)]
-rnas = []
+# numpy.random.seed(1)
+# chrLen = 5e6
+# dna = "".join(["ACGU"[x] for x in numpy.random.choice(numpy.arange(4), size = chrLen)])
+# rnaps = [RNAP() for x in xrange(500)]
+# rnas = []
 
-before = time.time()
-for t in xrange(10):
-	freeRnaps = [x for x in rnaps if x.state() == "F"]
-	for rnap in freeRnaps:
-		if numpy.random.uniform() > 0.5: continue
-		pos = numpy.random.random_integers(0, chrLen)
-		[rnap.chromLocsIs(x, (x + pos) % chrLen) for x in xrange(rnap.chromLocsLen())]
-		rnap.stateIs("B")
-		rnap.rnaIs(RNA(str(len(rnas) + 1)))
-		rnap.rnaLocsIs(0, 0)
-		rnas.append(rnap.rna())
+# before = time.time()
+# for t in xrange(10):
+# 	freeRnaps = [x for x in rnaps if x.state() == "F"]
+# 	for rnap in freeRnaps:
+# 		if numpy.random.uniform() > 0.5: continue
+# 		pos = numpy.random.random_integers(0, chrLen)
+# 		[rnap.chromLocsIs(x, (x + pos) % chrLen) for x in xrange(rnap.chromLocsLen())]
+# 		rnap.stateIs("B")
+# 		rnap.rnaIs(RNA(str(len(rnas) + 1)))
+# 		rnap.rnaLocsIs(0, 0)
+# 		rnas.append(rnap.rna())
 
-	ntCounts = numpy.random.poisson(1e4, 4)
-	ntLookup = {"A": 0, "C": 1, "G": 2, "U": 3}
-	boundRnaps = [x for x in rnaps if x.state() == "B"]
-	rnapsDone = numpy.zeros(len(boundRnaps))
-	rnapsStuck = numpy.zeros(len(boundRnaps))
-	# while not numpy.all(rnapsDone) and not numpy.all(rnapsStuck):
-	for rnapIdx, rnap in enumerate(boundRnaps):
-		if rnap.state() == "F": continue
-		for relPos in xrange(rnap.velocity):
-			meanPos = int(numpy.mean((rnap.chromLocs(0), rnap.chromLocs(-1))))
-			rna = rnap.rna()
-			for x in rna.tracksIdxs["sequence"]: rna.tracks[x][rnap.rnaLocs(0)] = dna[meanPos]
-			# [rnap.chromLocsIncr(x, 1) for x in xrange(rnap.chromLocsLen())]
-			rnap.chromLocsIncr(1)
-			rnap.rnaLocsIncr(0, 1)
-			if numpy.random.uniform() < 0.001:
-				rnap.rnaIs(None)
-				[rnap.chromLocsIs(x, -1) for x in xrange(rnap.chromLocsLen())]
-				rnap.stateIs("F")
-				break
+# 	ntCounts = numpy.random.poisson(1e4, 4)
+# 	ntLookup = {"A": 0, "C": 1, "G": 2, "U": 3}
+# 	boundRnaps = [x for x in rnaps if x.state() == "B"]
+# 	rnapsDone = numpy.zeros(len(boundRnaps))
+# 	rnapsStuck = numpy.zeros(len(boundRnaps))
+# 	# while not numpy.all(rnapsDone) and not numpy.all(rnapsStuck):
+# 	for rnapIdx, rnap in enumerate(boundRnaps):
+# 		if rnap.state() == "F": continue
+# 		for relPos in xrange(rnap.velocity):
+# 			meanPos = int(numpy.mean((rnap.chromLocs(0), rnap.chromLocs(-1))))
+# 			rna = rnap.rna()
+# 			for x in rna.tracksIdxs["sequence"]: rna.tracks[x][rnap.rnaLocs(0)] = dna[meanPos]
+# 			# [rnap.chromLocsIncr(x, 1) for x in xrange(rnap.chromLocsLen())]
+# 			rnap.chromLocsIncr(1)
+# 			rnap.rnaLocsIncr(0, 1)
+# 			if numpy.random.uniform() < 0.001:
+# 				rnap.rnaIs(None)
+# 				[rnap.chromLocsIs(x, -1) for x in xrange(rnap.chromLocsLen())]
+# 				rnap.stateIs("F")
+# 				break
 
-after = time.time()
-print "Elapsed: %0.3f seconds" % (after - before)
+# after = time.time()
+# print "Elapsed: %0.3f seconds" % (after - before)
 # class TrackGroup(object):
 
 # 	def __init__(self):
@@ -383,10 +383,10 @@ print "Elapsed: %0.3f seconds" % (after - before)
 # 			raise TypeError, "Invalid argument type."
 
 
-kb = type("", (), {})()
-kb.molecules = [{"id": "enz1", "mass": 1.0, "uniqueAttrs": None}, {"id": "enz2", "mass": 2.0, "uniqueAttrs": None}, {"id": "enz3", "mass": 3.0, "uniqueAttrs": ["attr1", "attr2", "attr3"]}, {"id": "enz4", "mass": 4.0, "uniqueAttrs": ["attr4_1", "attr4_2"]}, {"id": "enz5", "mass": 5.0, "uniqueAttrs": None}]
-kb.compartments = [{"id": "c"}, {"id": "e"}, {"id": "m"}]
+# kb = type("", (), {})()
+# kb.molecules = [{"id": "enz1", "mass": 1.0, "uniqueAttrs": None}, {"id": "enz2", "mass": 2.0, "uniqueAttrs": None}, {"id": "enz3", "mass": 3.0, "uniqueAttrs": ["attr1", "attr2", "attr3"]}, {"id": "enz4", "mass": 4.0, "uniqueAttrs": ["attr4_1", "attr4_2"]}, {"id": "enz5", "mass": 5.0, "uniqueAttrs": None}]
+# kb.compartments = [{"id": "c"}, {"id": "e"}, {"id": "m"}]
 
-mc = MoleculesContainer()
-mc.initialize(kb)
-mol = mc.molecule("enz3", "c")
+# mc = MoleculesContainer()
+# mc.initialize(kb)
+# mol = mc.molecule("enz3", "c")
