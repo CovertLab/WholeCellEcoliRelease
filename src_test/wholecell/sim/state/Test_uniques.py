678a72d5ec62d6ebedd6ad3a78718f982e2d0707
nruggero <nruggero@server>
2013-11-13 16:29:05 -0800

    Added tests for basic increment/decrement functions bulk counts

--- src_test/wholecell/sim/state/Test_uniques.py
+++ src_test/wholecell/sim/state/Test_uniques.py
@@ -28,20 +28,47 @@ class Test_uniques(unittest.TestCase):
 		pass
 
 	def setUp(self):
-		print 'hello'
 		self.kb = type("", (), {})()
-		self.kb.molecules = [{"id": "enz1", "mass": 1.0, "uniqueAttrs": None}, {"id": "enz2", "mass": 2.0, "uniqueAttrs": None}, {"id": "enz3", "mass": 3.0, "uniqueAttrs": ["attr1", "attr2", "attr3"]}, {"id": "enz4", "mass": 4.0, "uniqueAttrs": ["attr4_1", "attr4_2"]}, {"id": "enz5", "mass": 5.0, "uniqueAttrs": None}]
+		self.kb.molecules = [{"id": "enz1", "mass": 1.0,"uniqueAttrs": None},
+							{"id": "enz2", "mass": 2.0, "uniqueAttrs": None},
+							{"id": "enz3", "mass": 3.0, "uniqueAttrs": ["attr1", "attr2", "attr3"]},
+							{"id": "enz4", "mass": 4.0, "uniqueAttrs": ["attr4_1", "attr4_2"]},
+							{"id": "enz5", "mass": 5.0, "uniqueAttrs": None}]
 		self.kb.compartments = [{"id": "c"}, {"id": "e"}, {"id": "m"}]
 
 		self.mc = wholecell.sim.state.uniques.MoleculesContainer()
 		self.mc.initialize(self.kb)
-		mol = self.mc.molecule("enz3", "c")
 
 	def tearDown(self):
 		pass
 
 	@noseAttrib.attr('uniqueTest')
-	def test_program(self):
-		
-		import ipdb
-		ipdb.set_trace()
+	def test_countsBulk(self):
+		mol = self.mc.molecule("enz1", "c")
+		self.assertEqual(mol.countsBulk(), -1.0)
+
+	@noseAttrib.attr('uniqueTest')
+	def test_countsBulkIs(self):
+		mol = self.mc.molecule("enz1", "c")
+		mol.countsBulkIs(5)
+		self.assertEqual(mol.countsBulk(), 5.0)
+		mol.countsBulkIs(2)
+		self.assertEqual(mol.countsBulk(), 2.0)
+
+	@noseAttrib.attr('uniqueTest')
+	def test_countsBulkInc(self):
+		mol = self.mc.molecule("enz1", "c")
+		mol.countsBulkIs(0)
+		mol.countsBulkInc(1)
+		self.assertEqual(mol.countsBulk(), 1.0)
+		mol.countsBulkInc(2)
+		self.assertEqual(mol.countsBulk(), 3.0)
+
+	@noseAttrib.attr('uniqueTest')
+	def test_countsBulkDec(self):
+		mol = self.mc.molecule("enz1", "c")
+		mol.countsBulkIs(3)
+		mol.countsBulkDec(1)
+		self.assertEqual(mol.countsBulk(), 2.0)
+		mol.countsBulkDec(2)
+		self.assertEqual(mol.countsBulk(), 0.0)
