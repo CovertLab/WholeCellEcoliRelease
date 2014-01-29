c4c12b886e09afe732ab368a1917144900a90593
John Mason <jmason42@stanford.edu>
2014-01-29 12:22:17 -0800

    Removed default behavior from generateTestFixtures.py; breaks instance method pickling

--- generateTestFixtures.py
+++ generateTestFixtures.py
@@ -15,10 +15,9 @@ Example:
 
 import os
 import os.path
-import cPickle
-import pickle
 import copy_reg
 import types
+import cPickle
 
 # We need the following two methods so that we can pickle instancemethods
 # Also need the copy_reg.pickle() line in generateTestFixtures()
@@ -26,20 +25,22 @@ import types
 # (Thanks Steven Bethard!)
 
 def _pickle_method(method):
-    func_name = method.im_func.__name__
-    obj = method.im_self
-    cls = method.im_class
-    return _unpickle_method, (func_name, obj, cls)
+	func_name = method.im_func.__name__
+	obj = method.im_self
+	cls = method.im_class
+
+	return _unpickle_method, (func_name, obj, cls)
 
 def _unpickle_method(func_name, obj, cls):
-    for cls in cls.mro():
-        try:
-            func = cls.__dict__[func_name]
-        except KeyError:
-            pass
-        else:
-            break
-    return func.__get__(obj, cls)
+	for cls in cls.mro():
+		try:
+			func = cls.__dict__[func_name]
+		except KeyError:
+			pass
+		else:
+			break
+	return func.__get__(obj, cls)
+
 
 def generateTestFixtures():
 	import wholecell.kb.KnowledgeBase
@@ -55,14 +56,11 @@ def generateTestFixtures():
 
 	# Construct KB
 	kb = wholecell.kb.KnowledgeBase.KnowledgeBase(dataFileDir = "data/parsed/", seqFileName = "data/raw/sequence.txt")
-	cPickle.dump(kb, open(os.path.join(outDir, "KnowledgeBase.cPickle"), "w"), protocol = cPickle.HIGHEST_PROTOCOL)
+	cPickle.dump(kb, open(os.path.join(outDir, "KnowledgeBase.cPickle"), "wb"), protocol = cPickle.HIGHEST_PROTOCOL)
 
 	# Construct simulation
 	sim = wholecell.sim.Simulation.Simulation(kb)
 	sim.setOptions({"seed": 1})
 	wholecell.util.Fitter.Fitter.FitSimulation(sim, kb)
 	sim.calcInitialConditions()
-	cPickle.dump(sim, open(os.path.join(outDir, "Simulation.cPickle"), "w"), protocol = cPickle.HIGHEST_PROTOCOL)
-
-if __name__ == '__main__':
-	generateTestFixtures()
+	cPickle.dump(sim, open(os.path.join(outDir, "Simulation.cPickle"), "wb"), protocol = cPickle.HIGHEST_PROTOCOL)
