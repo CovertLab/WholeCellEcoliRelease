84cf8a93e7bfd3d29e4a5b802debac797a23f4c2
nruggero <nruggero@server>
2014-02-12 10:24:15 -0800

    Removed pickle methods they are not needed anymore

--- src_test/wholecell/sim/process/Test_Transcription_RnaDegradation.py
+++ src_test/wholecell/sim/process/Test_Transcription_RnaDegradation.py
@@ -27,29 +27,6 @@ comm = MPI.COMM_WORLD
 import copy_reg
 import types
 
-# We need the following two methods so that we can pickle instancemethods
-# Also need the copy_reg.pickle() line in generateTestFixtures()
-# Borrowed from: http://mail.python.org/pipermail/python-list/2006-October/367078.html
-# (Thanks Steven Bethard!)
-
-def _pickle_method(method):
-    func_name = method.im_func.__name__
-    obj = method.im_self
-    cls = method.im_class
-    return _unpickle_method, (func_name, obj, cls)
-
-def _unpickle_method(func_name, obj, cls):
-    for cls in cls.mro():
-        try:
-            func = cls.__dict__[func_name]
-        except KeyError:
-            pass
-        else:
-            break
-    return func.__get__(obj, cls)
-
-copy_reg.pickle(types.MethodType, _pickle_method, _unpickle_method)
-
 class Test_Transcription_RnaDegradation(unittest.TestCase):
 
 	@classmethod
