db83a39320d48b47da577bdcf87b58cbc7e573ce
John Mason <jmason42@stanford.edu>
2014-02-18 13:17:16 -0800

    Modified runSimulation to use the new simulation running, and to accept a json file as an argument.

--- runSimulation.py
+++ runSimulation.py
@@ -16,70 +16,28 @@ Example:
 @date: Created 4/10/2013
 """
 
-import os
-import cPickle
-
-import wholecell.loggers.disk
-import wholecell.loggers.shell
 import wholecell.sim.simulation
-import wholecell.reconstruction.knowledgebase
-import wholecell.utils.fitter
+import sys
 
-import weakref
+def main():
+	# TODO: argument parsing
 
-KB_PATH = os.path.join('data', 'fixtures', 'KnowledgeBase.cPickle')
+	nArgs = len(sys.argv)
 
-def runSimulation(reconstructKB = False, fitSimulation = True,
-		useShellLogger = True, useDiskLogger = False, outDir = None,
-		simOpts = None):
-
-	# Instantiate knowledge base
-	if reconstructKB or not os.path.exists(KB_PATH):
-		kb = wholecell.reconstruction.knowledgebase.KnowledgeBase(
-			dataFileDir = "data/parsed", seqFileName = "data/raw/sequence.txt"
+	if nArgs == 1:
+		# Use default parameters
+		wholecell.sim.simulation.Simulation(
+			seed = 10,
+			lengthSec = 100,
+			autoRun = True
 			)
 
-		cPickle.dump(kb, open(KB_PATH, "wb"),
-			protocol = cPickle.HIGHEST_PROTOCOL)
-
-	else:
-		kb = cPickle.load(open(KB_PATH, "rb"))
-
-	kbWeakRef = weakref.ref(kb)
-
-	# Set up simulation
-	sim = wholecell.sim.simulation.Simulation()
-
-	sim.initialize(kb)
-
-	if simOpts:
-		sim.setOptions(simOpts)
-
-	if fitSimulation:
-		wholecell.utils.fitter.Fitter.FitSimulation(sim, kb)
-
-	del kb
-
-	assert kbWeakRef() is None, 'Failed to release knowledge base from memory.'
-
-	# Instantiate loggers
-	if useShellLogger:
-		sim.loggerAdd(wholecell.loggers.shell.Shell())
-
-	if useDiskLogger:
-		sim.loggerAdd(
-			wholecell.loggers.disk.Disk(outDir = outDir)
+	elif nArgs == 2:
+		# Attempt to parse from a json file
+		wholecell.sim.simulation.Simulation.initFromFile(
+			sys.argv[1],
+			autoRun = True
 			)
-
-	# Run simulation
-	sim.run()
 
 if __name__ == '__main__':
-	runSimulation(
-		simOpts = {
-			'seed':10,
-			'lengthSec':100
-			},
-		fitSimulation = True,
-		useDiskLogger = False,
-		)
+	main()
