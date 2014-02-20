1f8edfa58b869873abc544b6d47af764bc88f2da
John Mason <jmason42@stanford.edu>
2014-02-20 15:07:53 -0800

    One more test, an OrderedDict implementation, and a saving implementation for each class variant

--- wholecell/states/chromosome.py
+++ wholecell/states/chromosome.py
@@ -20,6 +20,8 @@ import numpy
 import time
 import random
 
+from collections import OrderedDict
+
 N_BASES = 5000000
 # N_CHROMOSOMES = 10
 
@@ -113,11 +115,18 @@ class Chromosome(object):
 
 	_range = None # Function that returns a range of values given a start and stop
 
+	def _freeSites(self):
+		raise NotImplementedError()
+
+
+	def fractionOccupied(self):
+		raise NotImplementedError()
+
 
 class ChromosomeArray(Chromosome):
 	def __init__(self):
 		super(ChromosomeArray, self).__init__()
-		self.chrArray = numpy.empty(N_BASES, int)
+		self.chrArray = numpy.empty(N_BASES, numpy.int64)
 		self.chrArray[:] = self.empty
 
 
@@ -131,6 +140,18 @@ class ChromosomeArray(Chromosome):
 		self.chrArray[rng] = value
 
 
+	def _freeSites(self):
+		return self.chrArray == self.empty
+
+
+	def fractionOccupied(self):
+		return 1 - 1.*self._freeSites().sum()/N_BASES
+
+
+	def toUnsignedArray(self):
+		return self.chrArray + 1
+
+
 	_range = numpy.arange
 
 
@@ -138,6 +159,7 @@ class ChromosomeDict(Chromosome):
 	def __init__(self):
 		super(ChromosomeDict, self).__init__()
 		self.chrDict = {i:self.empty for i in self._range(N_BASES)}
+		# self.chrDict = OrderedDict([(i, self.empty) for i in self._range(N_BASES)])
 
 
 	def boundMolecules(self, start, stop):
@@ -151,16 +173,49 @@ class ChromosomeDict(Chromosome):
 			self.chrDict[i] = value
 
 
+	def _freeSites(self):
+		return {i for i, value in self.chrDict.viewitems() if value == self.empty}
+
+
+	def fractionOccupied(self):
+		# return 1 - 1.*len(self._freeSites())/N_BASES
+		return sum(mol.width for mol in self.molecules)/N_BASES
+
+
+	def toUnsignedArray(self):
+		array = numpy.empty(N_BASES)
+
+		(keys, values) = zip(*self.chrDict.items())
+
+		array[numpy.array(keys)] = values
+
+		array += 1
+
+		return array
+
+
 	_range = xrange
+
+
+class ChromosomeOrderedDict(ChromosomeDict):
+	def __init__(self):
+		# super(ChromosomeOrderedDict, self).__init__()
+		self.molecules = [] # this is why you shouldn't create separate __init__ methods for subclasses
+		self.chrDict = OrderedDict([(i, self.empty) for i in self._range(N_BASES)])
+
+
+	def toUnsignedArray(self):
+		return numpy.array(self.chrDict.values()) + 1
 		
 
-def testClass(chromosomeClass, iters = N_ITERS):
+def testRunningClass(chromosomeClass, iters = N_ITERS):
 	timeInit = 0
 	timeSetup = 0
 	timeAccess = 0
 	timeRemove = 0
 	timeAdd = 0
 	timeMove = 0
+	timeCalcOccupied = 0
 	
 	for i in xrange(iters):
 		t = time.time()
@@ -203,33 +258,39 @@ def testClass(chromosomeClass, iters = N_ITERS):
 				chromosome.boundMoleculeIs(molecule, newPos)
 
 		timeMove += time.time() - t
+
+		t = time.time()
+		fractionOccupied = chromosome.fractionOccupied()
+		timeCalcOccupied += time.time() - t
 	
-	return timeInit, timeSetup, timeAccess, timeRemove, timeAdd, timeMove
+	return timeInit, timeSetup, timeAccess, timeRemove, timeAdd, timeMove, timeCalcOccupied
 
 
-def testClasses(classes = (ChromosomeArray, ChromosomeDict)):
+def testRunningClasses(classes = (ChromosomeArray, ChromosomeDict)):
 	for cls in classes:
-		timeInit, timeSetup, timeAccess, timeRemove, timeAdd, timeMove = testClass(cls)
+		timeInit, timeSetup, timeAccess, timeRemove, timeAdd, timeMove, timeCalcOccupied = testRunningClass(cls)
 
 		print ''
 		print cls.__name__
-		print '{:0.3f}s to initialize\n{:0.3f}s to bind {} molecules\n{:0.3f}s to check {} locations\n{:0.3f}s to remove {} molecules\n{:0.3f}s to re-bind {} molecules\n{:0.3f}s to randomly move {} molecules'.format(
+		print '{:0.3f}s to initialize\n{:0.3f}s to bind {} molecules\n{:0.3f}s to check {} locations\n{:0.3f}s to remove {} molecules\n{:0.3f}s to re-bind {} molecules\n{:0.3f}s to randomly move {} molecules\n{:0.3f}s to calculate fraction occupied'.format(
 			timeInit,
 			timeSetup, N_RNAP,
 			timeAccess, N_CHECK,
 			timeRemove, N_REMOVE,
 			timeAdd, N_REMOVE,
-			timeMove, N_REMOVE
+			timeMove, N_REMOVE,
+			timeCalcOccupied
 			)
 
-def testSaving():
+N_STEPS = 10
+
+def testSaving(chromosomeClass):
 	import tables
 
-	chromosome = ChromosomeArray()
+	chromosome = chromosomeClass()
 	chromosome.setup()
 
-	nSteps = 500
-
+	initTime = time.time()
 	with tables.open_file('test.hdf', mode = 'w', title = 'test') as h5file:
 		columns = {
 			'boundMolecules':tables.UInt32Col(N_BASES)
@@ -241,25 +302,74 @@ def testSaving():
 			columns,
 			title = 'test',
 			filters = tables.Filters(complevel = 9, complib = 'zlib'),
-			expectedrows = nSteps
+			expectedrows = N_STEPS
 			)
 
 		molecules = random.sample(chromosome.molecules, N_REMOVE)
 
-		for t in xrange(nSteps):
+		for t in xrange(N_STEPS):
 			chromosome.boundMoleculeRemove(molecules[t])
 
 			table = h5file.get_node('/', 'test')
 			entry = table.row
 
-			entry['boundMolecules'] = chromosome.chrArray+1
+			entry['boundMolecules'] = chromosome.toUnsignedArray()
 
 			entry.append()
 			table.flush()
 
-			if t % 100 == 0:
-				print t
+			# if t % 100 == 0:
+			# 	print t
 
+	return (time.time() - initTime)
+
+
+def testSavingClasses(classes = (ChromosomeArray, ChromosomeDict)):
+	for cls in classes:
+		timeSave = testSaving(cls)
+
+		print ''
+		print cls.__name__
+		print '{:0.3f}s to save {} simulation steps'.format(timeSave, N_STEPS)
 
 if __name__ == '__main__':
-	testClasses()
+	testRunningClasses()
+	testSavingClasses()
+
+'''
+ChromosomeArray
+0.128s to initialize
+0.284s to bind 1000 molecules
+0.693s to check 10000 locations
+0.015s to remove 500 molecules
+0.130s to re-bind 500 molecules
+0.144s to randomly move 500 molecules
+0.111s to calculate fraction occupied
+
+ChromosomeDict
+5.363s to initialize
+0.223s to bind 1000 molecules
+0.501s to check 10000 locations
+0.005s to remove 500 molecules
+0.099s to re-bind 500 molecules
+0.104s to randomly move 500 molecules
+0.001s to calculate fraction occupied
+
+ChromosomeOrderedDict
+84.309s to initialize
+0.267s to bind 1000 molecules
+0.866s to check 10000 locations
+0.005s to remove 500 molecules
+0.120s to re-bind 500 molecules
+0.122s to randomly move 500 molecules
+0.001s to calculate fraction occupied
+
+ChromosomeArray
+1.397s to save 10 simulation steps
+
+ChromosomeDict
+58.065s to save 10 simulation steps
+
+ChromosomeOrderedDict
+15.488s to save 10 simulation steps
+'''
