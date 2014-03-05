54c5ffc10959265c0d1eded9ef1d0f805edf3497
John Mason <jmason42@stanford.edu>
2014-03-05 13:03:33 -0800

    Added partitioning to UniqueMolecules State

--- wholecell/states/unique_molecules.py
+++ wholecell/states/unique_molecules.py
@@ -13,13 +13,15 @@ import numpy as np
 import tables
 
 import wholecell.states.state
+import wholecell.states.partition
 import wholecell.utils.unique_objects_container
 
 
 MOLECULE_ATTRIBUTES = {
 	'RNA polymerase':{
 		'boundToChromosome':'bool',
-		'chromosomeLocation':'uint32'
+		'chromosomeLocation':'uint32',
+		'_partitionedProcess':'uint32' # TODO: assign to every as default instead of in this fake KB
 		}
 	}
 
@@ -42,6 +44,7 @@ class UniqueMolecules(wholecell.states.state.State):
 			}
 
 		self.time = None
+		self.partitionClass = UniqueMoleculesPartition
 
 		self._container = None
 
@@ -58,18 +61,23 @@ class UniqueMolecules(wholecell.states.state.State):
 		self._container = wholecell.utils.unique_objects_container.UniqueObjectsContainer(
 			MOLECULE_ATTRIBUTES)
 
+		for iProcess, partition in enumerate(self.partitions.viewvalues()):
+			partition._processIndexIs(iProcess + 1)
+
 	
 	def calcInitialConditions(self):
 		# TODO: create a generalized calcInitialConditions routine as method of
 		# the Simulation class, or as a separate function like fitSimulation
 
-		# Add some molecules for testing save/load
-		self._container.moleculesNew(
-			'RNA polymerase',
-			20,
-			boundToChromosome = True, # just some example parameters
-			chromosomeLocation = 50
-			)
+		# # Add some molecules for testing save/load
+		# self._container.moleculesNew(
+		# 	'RNA polymerase',
+		# 	20,
+		# 	boundToChromosome = True, # just some example parameters
+		# 	chromosomeLocation = 50
+		# 	)
+
+		pass
 
 
 	def partition(self):
@@ -82,7 +90,45 @@ class UniqueMolecules(wholecell.states.state.State):
 		# Update queries prior to gathering requests
 		self._container.updateQueries()
 
-		# TODO: actually partition
+		# Gather requests
+		arrays = []
+		counts = []
+		processReference = []
+
+		for iProcess, partition in enumerate(self.partitions.viewvalues()):
+			partitionArrays, partitionCounts = partition.request()
+
+			nRequests = len(partitionArrays)
+
+			arrays.extend(partitionArrays)
+			counts.extend(partitionCounts)
+			processReference.extend([iProcess]*nRequests)
+
+		nTotalRequests = len(processReference)
+		nProcesses = len(self.partitions)
+
+		# Format requests into appropriate parameters
+		objectRequestsArray = np.vstack(arrays).transpose().copy() # must copy to fix memory order
+		requestNumberVector = np.array(counts)
+		requestProcessArray = (np.tile(np.arange(nProcesses), (nTotalRequests, 1)).T
+			== np.array(processReference)).transpose()
+
+		# import ipdb; ipdb.set_trace()
+
+		partitionedMolecules = wholecell.utils.unique_objects_container._partition(
+			objectRequestsArray, requestNumberVector, requestProcessArray, self.randStream)
+
+		for iProcess, partition in enumerate(self.partitions.viewvalues()):
+			molecules = self._container._moleculesByGlobalIndex(
+				np.where(partitionedMolecules[:, iProcess])[0]
+				)
+
+			for molecule in molecules:
+				molecule.attrIs('_partitionedProcess', iProcess+1)
+
+
+	def queryNew(self, moleculeName, **operations):
+		return self._container.queryNew(moleculeName, **operations)
 
 
 	def pytablesCreate(self, h5file, expectedRows):
@@ -99,8 +145,60 @@ class UniqueMolecules(wholecell.states.state.State):
 		# self._container.pytablesLoad(h5file, timePoint)
 		pass
 	
-	# TODO: partitioning
 
-# TODO: partitions
-# molecules created in a partition should be noted so they can be given a new, 
-# permanent reference in the state
+
+class UniqueMoleculesPartition(wholecell.states.partition.Partition):
+	def __init__(self, *args, **kwargs):
+		self._requestsEmpty()
+
+		self._processIndex = None
+
+		super(UniqueMoleculesPartition, self).__init__(*args, **kwargs)
+
+
+	def _processIndexIs(self, value):
+		self._processIndex = value
+
+
+	def _requestsEmpty(self):
+		self.requestArrays = []
+		self.requestCounts = []
+
+
+	def request(self):
+		self._requestsEmpty()
+		self._process.requestUniqueMolecules()
+
+		return self.requestArrays, self.requestCounts
+
+
+	def requestByMolecules(self, nMolecules, molecules):
+		if nMolecules > 0:
+			self.requestCounts.append(nMolecules)
+
+			container = self._state._container
+
+			size = container._arrays[container._globalRefIndex].size
+
+			array = np.zeros(size, np.bool)
+
+			indexes = np.array([molecule.attr('_globalIndex') for molecule in molecules])
+
+			array[indexes] = True
+
+			self.requestArrays.append(array)
+
+
+	def evaluateQuery(self, moleculeName, **operations):
+		operations['_partitionedProcess'] = ('==', self._processIndex)
+
+		return self._state._container.evaluateQuery(moleculeName, **operations)
+
+
+	def moleculesDel(self, molecules):
+		self._state._container.moleculesDel(molecules)
+
+
+	def moleculeDel(self, molecule):
+		self._state._container.moleculeDel(molecule)
+
