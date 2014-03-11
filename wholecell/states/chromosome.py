23977c4b8327a2caeba613fc3e57df1bc2b9f570
John Mason <jmason42@stanford.edu>
2014-03-11 14:30:31 -0700

    Handling for replication forks

--- wholecell/states/chromosome.py
+++ wholecell/states/chromosome.py
@@ -8,7 +8,7 @@ import wholecell.states.state
 import wholecell.utils.unique_objects_container
 
 N_BASES = 5000000 # TODO: from kb
-N_STRANDS = 2 # TODO: from kb/fit?
+STRAND_MULTIPLICITY = 3
 MOLECULE_WIDTH = 50 # TODO: from kb
 
 MOLECULE_ATTRIBUTES = {
@@ -22,19 +22,25 @@ class SequenceBoundMolecules(object):
 	# Special values for the state of the chromosome
 	_inactive = 0
 	_empty = 1
+	_fork = 2 # the (inactive) location of a fork
+	_end = 3 # the (inactive) location of the end of a strand, most likely where the fork is
 
-	_specialValues = np.array([_inactive, _empty])
+	_specialValues = np.array([_inactive, _empty, _fork, _end])
 	_offset = _specialValues.size
 
 	_defaultMoleculesContainerAttributes = {
 		'_sequenceBoundLocation':'uint32', # location of the molecule on the chromosome
 		}
 
+	_rootName = 'R' # The name of the root strand
 	
 	def __init__(self):
 		self._length = N_BASES
+
+		self._strandMultiplicity = STRAND_MULTIPLICITY
+		self._nStrands = 1 + 2*(self._strandMultiplicity - 1)**2
 		
-		self._array = np.zeros((N_BASES, N_STRANDS), dtype = np.int32) # TODO: choose best dtype based on array size
+		self._array = np.zeros((N_BASES, self._nStrands ), dtype = np.int32) # TODO: choose best dtype based on array size
 
 		self._array[:, 0] = self._empty # Primary strand is always active
 
@@ -45,6 +51,38 @@ class SequenceBoundMolecules(object):
 
 		self._moleculesContainer =  wholecell.utils.unique_objects_container.UniqueObjectsContainer(
 			moleculeAttributes)
+
+		self._buildStrandConnectivity()
+
+
+	def _buildStrandConnectivity(self):
+		strandNames = []
+		strandNames.append(self._rootName)
+
+		rootNames = strandNames
+		for i in xrange(self._strandMultiplicity - 1):
+			newNames = []
+
+			for rootName in rootNames:
+				newNames.append(rootName + 'A')
+				newNames.append(rootName + 'B')
+
+			strandNames.extend(newNames)
+			rootNames = newNames
+
+		self._strandNameToIndex = {name:ind for ind, name in enumerate(strandNames)}
+		self._strandChildrenIndexes = [
+			(self._strandNameToIndex[name + 'A'], self._strandNameToIndex[name + 'B'])
+			if self._strandNameToIndex.has_key(name + 'A') else None
+			for name in strandNames
+			]
+
+		self._strandParentIndex = [None]*len(strandNames)
+
+		for parentIndex, childrenIndexes in enumerate(self._strandChildrenIndexes):
+			if childrenIndexes is not None:
+				self._strandParentIndex[childrenIndexes[0]] = parentIndex
+				self._strandParentIndex[childrenIndexes[1]] = parentIndex
 
 
 	def moleculeNew(self, objectName, location, **attributes):
@@ -88,28 +126,30 @@ class SequenceBoundMolecules(object):
 
 	def forkNew(self, strandName, start, stop):
 		# TODO: assert fork does not exist
-		# TODO: move things about randomly over region split
-		# TODO: forbid binding of fork except in special cases
+		# TODO: move things about randomly over region split or assert that it is empty
 
-		strandRoot = self._strandNameToIndex(strandName)
-		strandChildA = self._strandNameToIndex(strandName + 'A') # should be the same as root
-		strandChildB = self._strandNameToIndex(strandName + 'B')
+		strandParent = self._strandNameToIndex[strandName]
+		try:
+			strandChildA, strandChildB = self._strandChildrenIndexes[strandParent]
+
+		except TypeError:
+			raise Exception('No space allocated for strand {} to fork into'.format(strandName))
+
+
+		self._array[start:stop, strandParent] = self._inactive
+		self._array[start+1, strandParent] = self._fork
+		self._array[stop-1, strandParent] = self._fork
+
+		self._array[start:stop, strandChildA] = self._empty
+		self._array[start-1, strandChildA] = self._end
+		self._array[stop+1, strandChildA] = self._end
 
 		self._array[start:stop, strandChildB] = self._empty
+		self._array[start-1, strandChildB] = self._end
+		self._array[stop+1, strandChildB] = self._end
 
 
-	def _strandNameToIndex(self, strandName):
-		# Strands are named from left-to-right
-		# A is the root strand
-		# AA and AB are the first child strands
-
-		strandValues = [{'A':0, 'B':1}[s] for s in strandName]
-
-		index = np.dot(strandValues, np.arange(len(strandName)))
-
-		return index
-
-
+	
 
 	# TODO: figure out how molecule width info is going to be handled
 	# TODO: circularly-permuted indexing
@@ -150,8 +190,11 @@ class Chromosome(wholecell.states.state.State):
 		origin = N_BASES // 2
 
 		forkStart = origin - N_BASES // 4
-		forkStop = origin - N_BASES // 4
+		forkStop = origin + N_BASES // 4
 
-		self._container.forkNew('A', forkStart, forkStop)
+		self._container.forkNew('R', forkStart, forkStop)
+
+		
+		import ipdb; ipdb.set_trace()
 
 
