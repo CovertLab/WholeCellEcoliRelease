fd8de79d234f8b9f04b12d17d085f821f9072d45
John Mason <jmason42@stanford.edu>
2014-03-11 16:57:12 -0700

    Playing with possible implementations

--- wholecell/processes/toy_replication.py
+++ wholecell/processes/toy_replication.py
@@ -25,7 +25,10 @@ class ToyReplication(wholecell.processes.process.Process):
 		self.meta = {
 			"id": "ToyReplication",
 			"name": "ToyReplication",
-		}
+			}
+
+		self.dnaPolymeraseFootprint = 50
+		self.elongationRate = 100
 
 		super(ToyReplication, self).__init__()
 
@@ -34,13 +37,44 @@ class ToyReplication(wholecell.processes.process.Process):
 	def initialize(self, sim, kb):
 		super(ToyReplication, self).initialize(sim, kb)
 
-		self.chromosomeView = self.chromosomeView()
+		# self.chromosomeView = self.chromosomeView()
+
+		self.replicationForks = self.replicationForkView(
+			extentAlongParent = self.dnaPolymeraseFootprint + self.elongationRate,
+			extentAlongChildren = self.elongationRate
+			)
 
 
 	def calculateRequest(self):
-		pass
+		self.replicationForks.requestAll()
 
 
 	# Calculate temporal evolution
 	def evolveState(self):
-		pass
+		# can't loop over regions, may overlap
+		# need a way to handle replication termination
+		# probably won't be able to avoid working with numbers for locations on the chromosome
+		for region in self.replicationForks.regions():
+			(dnaPolymerase,) = region.molecules(moleculeName = 'DNA polymerase') # singleton unpacking
+			(forkLocation,) = region.forkLocations() # singleton unpacking
+
+			moveDistance = region.extentAlongParent()
+
+			newDnaPolyLocation = region.moleculeLocation(dnaPolymerase) + moveDistance - self.dnaPolymeraseFootprint
+
+			region.moleculeMove(dnaPolymerase, newDnaPolyLocation)
+
+			region.forkMove(forkLocation, forkLocation + moveDistance)
+
+
+		dnaPolymerases = self.replicationForks.molecules(moleculeName = 'DNA polymerase')
+
+		forkLocations = self.replicationForks.forkLocations()
+
+		# how to find forks? associate with polymerases?
+		# - find fork, then find nearby molecules
+		# how to move forks?
+
+		# communicating distance that can be moved
+		# origin for molecule footprints
+
