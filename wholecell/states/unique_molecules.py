cbda05b82e6920aa39e88e1265336082e37b5a1e
John Mason <jmason42@stanford.edu>
2014-03-11 12:19:13 -0700

    Removed some attributes/methods I think are defunct

--- wholecell/states/unique_molecules.py
+++ wholecell/states/unique_molecules.py
@@ -61,8 +61,6 @@ class UniqueMolecules(wholecell.states.state.State):
 
 		self._container = wholecell.utils.unique_objects_container.UniqueObjectsContainer(
 			MOLECULE_ATTRIBUTES)
-		
-		self._queries = []
 
 	
 	def calcInitialConditions(self):
@@ -123,10 +121,6 @@ class UniqueMolecules(wholecell.states.state.State):
 					'_partitionedProcess',
 					view._processIndex + 1 # "0", being the default, is reserved for unpartitioned molecules
 					)
-
-
-	def queryNew(self, moleculeName, **operations):
-		return self._container.queryNew(moleculeName, **operations)
 
 
 	def pytablesCreate(self, h5file, expectedRows):
