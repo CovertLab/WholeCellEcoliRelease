8286b31814086ff8515bf224936efce4292c0ea3
nruggero <nruggero@server>
2014-03-13 11:38:55 -0700

    Fixed KB fixture loading.

--- wholecell/tests/reconstruction/test_knowledgebase.py
+++ wholecell/tests/reconstruction/test_knowledgebase.py
@@ -13,7 +13,9 @@ import warnings
 
 import cPickle
 import os
-import wholecell.reconstruction.knowledgebase
+
+import wholecell.utils.config
+TEST_FIXTURE_DIR = wholecell.utils.config.TEST_FIXTURE_DIR
 
 import nose.plugins.attrib as noseAttrib
 
@@ -28,7 +30,8 @@ class Test_KnowledgeBase(unittest.TestCase):
 		pass
 
 	def setUp(self):
-		self.kb = cPickle.load(open(os.path.join("fixtures", "KnowledgeBase.cPickle"), "r"))
+		import wholecell.utils.knowledgebase_fixture_manager
+		self.kb = wholecell.utils.knowledgebase_fixture_manager.loadKnowledgeBase(os.path.join(TEST_FIXTURE_DIR, 'KnowledgeBase.cPickle'))
 
 		# To load from the "raw" data, uncomment the following:
 		# self.kb = wholecell.knowledgebase.knowledgebase.KnowledgeBase(dataFileName = "data/KnowledgeBase.xlsx",
@@ -36,7 +39,3 @@ class Test_KnowledgeBase(unittest.TestCase):
 
 	def tearDown(self):
 		pass
-
-	@noseAttrib.attr('smalltest')
-	def test_construction(self):
-		wholecell.reconstruction.knowledgebase.KnowledgeBase()
