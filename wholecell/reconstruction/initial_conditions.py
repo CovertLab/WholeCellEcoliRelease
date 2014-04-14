177a741fbf47ad2779e4629e18459f8ec472a2d8
macklin <macklin@server>
2014-04-14 11:13:52 -0700

    Adding initializeBulkMonomers() and initializeBulkAAs() in wholecell/reconstruction/initial_conditions.py.

--- wholecell/reconstruction/initial_conditions.py
+++ wholecell/reconstruction/initial_conditions.py
@@ -23,23 +23,10 @@ def initializeBulk(bulkContainer, kb, randStream):
 	initialDryMass = kb.avgCellDryMassInit.to('g').magnitude
 	mwH2O = kb.bulkMolecules["mass"][kb.bulkMolecules["moleculeId"] == "H2O[c]"].magnitude
 
-	fracInitFreeAAs = kb.fracInitFreeAAs.to('dimensionless').magnitude
 	biomassFlux = kb.coreBiomass['biomassFlux'].to('mol/(DCW_g*hr)').magnitude
 	biomassMetabolites = kb.coreBiomass['metaboliteId']
 
 	avgCellWaterMassInit = kb.avgCellWaterMassInit.to('water_g').magnitude
-
-	## Monomers are not complexes and not modified
-	monomers = kb.bulkMolecules[kb.bulkMolecules['isProteinMonomer'] & ~kb.bulkMolecules['isModifiedForm']]
-	monomerLength = np.sum(kb.proteinMonomerAACounts, axis = 1)
-
-	monomerExpression = np.zeros(len(kb._proteinMonomerData), dtype = float)
-	monomerExpression = [
-		kb._rnaData['expression'][np.where(rnaId == kb._rnaData['id'])[0][0]]
-		for rnaId in kb._proteinMonomerData['rnaId']
-		] # TODO: I'm almost certain this data is already parsed in the KB, get it from there
-
-	monomerExpression /= np.sum(monomerExpression)
 
 	# Set bulk molecules
 
@@ -48,9 +35,6 @@ def initializeBulk(bulkContainer, kb, randStream):
 	feistCoreView = bulkContainer.countsView(biomassMetabolites)
 	h2oView = bulkContainer.countView('H2O[c]')
 	ntpsView = bulkContainer.countsView(ntpIds)
-	
-	aasView = bulkContainer.countsView(aaIds)
-	monomersView = bulkContainer.countsView(monomers['moleculeId'])
 
 	## Set metabolite counts from Feist biomass
 	feistCoreView.countsIs(
@@ -69,22 +53,9 @@ def initializeBulk(bulkContainer, kb, randStream):
 	initializeBulkNTPs(kb, bulkContainer, randStream)
 
 	## Set protein counts from expression levels
-	aasToPolym = np.round(
-		(1 - fracInitFreeAAs) * np.sum(aasView.counts())
-		)
+	initializeBulkMonomers(kb, bulkContainer, randStream)
+	initializeBulkAAs(kb, bulkContainer, randStream)
 
-	monCnts = randStream.mnrnd(
-		np.round(aasToPolym / (np.dot(monomerExpression, monomerLength))),
-		monomerExpression
-		)
-
-	aasView.countsIs(
-		np.round(
-			fracInitFreeAAs * aasView.counts()
-			)
-		)
-
-	monomersView.countsIs(monCnts)
 
 def initializeBulkRNA(kb, bulkContainer, randStream):
 	rnaIds = kb.bulkMolecules['moleculeId'][kb.bulkMolecules['isRna']]
@@ -116,5 +87,44 @@ def initializeBulkNTPs(kb, bulkContainer, randStream):
 	ntpsView.countsIs(
 		np.round(
 			fracInitFreeNTPs * ntpsView.counts()
+			)
+		)
+
+def initializeBulkMonomers(kb, bulkContainer, randStream):
+	## Monomers are not complexes and not modified
+	monomers = kb.bulkMolecules[kb.bulkMolecules['isProteinMonomer'] & ~kb.bulkMolecules['isModifiedForm']]
+
+	aasView = bulkContainer.countsView(aaIds)
+	monomersView = bulkContainer.countsView(monomers['moleculeId'])
+
+	fracInitFreeAAs = kb.fracInitFreeAAs.to('dimensionless').magnitude
+	monomerExpression = np.zeros(len(kb._proteinMonomerData), dtype = float)
+	monomerExpression = [
+		kb._rnaData['expression'][np.where(rnaId == kb._rnaData['id'])[0][0]]
+		for rnaId in kb._proteinMonomerData['rnaId']
+		] # TODO: I'm almost certain this data is already parsed in the KB, get it from there
+
+	monomerExpression /= np.sum(monomerExpression)
+	monomerLength = np.sum(kb.proteinMonomerAACounts, axis = 1)
+
+	aasToPolym = np.round(
+		(1 - fracInitFreeAAs) * np.sum(aasView.counts())
+		)
+
+	monCnts = randStream.mnrnd(
+		np.round(aasToPolym / (np.dot(monomerExpression, monomerLength))),
+		monomerExpression
+		)
+
+	monomersView.countsIs(monCnts)
+
+def initializeBulkAAs(kb, bulkContainer, randStream):
+	aasView = bulkContainer.countsView(aaIds)
+
+	fracInitFreeAAs = kb.fracInitFreeAAs.to('dimensionless').magnitude
+
+	aasView.countsIs(
+		np.round(
+			fracInitFreeAAs * aasView.counts()
 			)
 		)
