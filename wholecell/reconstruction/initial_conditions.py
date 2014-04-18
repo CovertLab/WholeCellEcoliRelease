Fixing units for biomass in wholecell/reconstruction/initial_conditions.py.

wholecell/reconstruction/initial_conditions.py
@@ -52,7 +52,7 @@ def initializeBulkBiomass(kb, bulkContainer, randStream):
 
 	nAvogadro = kb.nAvogadro.to('1 / mole').magnitude
 	initialDryMass = kb.avgCellDryMassInit.to('g').magnitude + randStream.normal(0.0, 1e-15)
-	biomassFlux = kb.coreBiomass['biomassFlux'].to('mol/(DCW_g*hr)').magnitude
+	biomassFlux = kb.coreBiomass['biomassFlux'].to('mol/(DCW_g)').magnitude
 
 	feistCoreView.countsIs(
 		np.round(