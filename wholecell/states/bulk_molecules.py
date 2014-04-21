85444fe1733fd853041bd04e188b968baa8e4fd5
macklin <macklin@server>
2014-04-21 11:25:37 -0700

    Adding 'rrna' typeIdx to wholecell/states/bulk_molecules.py.

--- wholecell/states/bulk_molecules.py
+++ wholecell/states/bulk_molecules.py
@@ -72,6 +72,7 @@ class BulkMolecules(wholecell.states.state.State):
 
 		self._typeIdxs = {'metabolites'	:	kb.bulkMolecules['isMetabolite'],
 							'rnas'		:	kb.bulkMolecules['isRna'],
+							'rrnas'		:	np.array([True if x in kb.rnaData["id"][kb.rnaData["isRRna"]] else False for x in kb.bulkMolecules["moleculeId"]]),
 							'proteins'	:	kb.bulkMolecules['isProteinMonomer'],
 							'water'		:	kb.bulkMolecules['isWater']}
 
