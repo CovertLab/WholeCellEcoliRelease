62f195cf923101fedef2e117a861f8d0638bc047
John Mason <jmason42@stanford.edu>
2014-05-05 13:40:12 -0700

    ADD: extended initialize, still no process dynamics

--- wholecell/processes/metabolism_fba.py
+++ wholecell/processes/metabolism_fba.py
@@ -14,6 +14,8 @@ import numpy as np
 
 import wholecell.processes.process
 
+UNCONSTRAINED_FLUX_VALUE = 1000.0
+
 class MetabolismFba(wholecell.processes.process.Process):
 	""" MetabolismFba """
 
@@ -31,11 +33,40 @@ class MetabolismFba(wholecell.processes.process.Process):
 	# Construct object graph
 	def initialize(self, sim, kb):
 		super(MetabolismFba, self).initialize(sim, kb)
-		print 'init'
+
+		# TODO: see if biomass reaction is in some way already in the table
+		
+		wildtypeIds = kb.wildtypeBiomass['metaboliteId']
+		wildtypeBiomassReaction = kb.wildtypeBiomass['biomassFlux'].magnitude
+
+		# Must add one entry for the biomass reaction
+
+		stoichMatrix = kb.metabolismStoichMatrix
+
+		self.stoichMatrix = np.hstack([
+			stoichMatrix,
+			np.zeros((stoichMatrix.shape[0], 1))
+			])
+
+		self.lowerBounds = np.zeros(self.stoichMatrix.shape[1])
+		self.lowerBounds[kb.metabolismReversibleReactions] = -UNCONSTRAINED_FLUX_VALUE
+
+		self.upperBounds = UNCONSTRAINED_FLUX_VALUE * np.ones(self.stoichMatrix.shape[1])
+
+		indexes = [kb.metabolismMoleculeNames.index(moleculeName) for moleculeName in wildtypeIds]
+
+		self.stoichMatrix[indexes, -1] = wildtypeBiomassReaction
+
+		self.objective = np.zeros(self.stoichMatrix.shape[1])
+		self.objective[-1] = -1 # TODO: check signs
+
+		self.molecules = self.bulkMoleculesView(kb.metabolismMoleculeNames)
 
 
 	def calculateRequest(self):
-		print 'request'
+		import ipdb; ipdb.set_trace()
+
+		# TODO: how to generate request?
 
 
 	# Calculate temporal evolution
