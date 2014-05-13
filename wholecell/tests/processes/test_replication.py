7ac08d94bbb2eca4cc142a0870dddfdff66678a0
nruggero <nruggero@server>
2014-05-12 17:00:37 -0700

    Updated tests for replication

--- wholecell/tests/processes/test_replication.py
+++ wholecell/tests/processes/test_replication.py
@@ -7,7 +7,7 @@ Test replication.py
 @organization: Covert Lab, Department of Chemical Engineering, Stanford University
 @date: Created 4/6/2014
 """
-import wholecell.utils.polymerize as p
+import wholecell.processes.replication
 
 import numpy as np
 
@@ -15,18 +15,60 @@ import nose.plugins.attrib as noseAttrib
 import nose.tools as noseTools
 import unittest
 
-class Test_polymerize(unittest.TestCase):
+class Test_replication(unittest.TestCase):
 	
-	@noseAttrib.attr('polymerizeTest')
+	@classmethod
+	def setUpClass(cls):
+		pass
+
+	@classmethod
+	def tearDownClass(cls):
+		pass
+
+	def setUp(self):
+		self.sequence = 'hello_world_i_am_genome'
+		self.elongationRate = 5
+		self.genomeLength = len(self.sequence)
+
+	def tearDown(self):
+		pass
+
+	@noseAttrib.attr('replicationTest')
 	@noseAttrib.attr('smalltest')
-	def test_polymerize_functionCall(self):
-		sequences = np.matrix([['A','T','A','A'],['G','T','A','C'],['C','C','T','T']])
-		baseAmounts = np.array([9,9,9,9])
-		bases = np.array(['A','C','G','T'])
-		basePadValue = ' '
-		energy = 0
-		energyCostPerBase = 0
+	def test_calculateSequence(self):
+		# Positive direction
+		chromosomeLocation = 1
+		directionIsPositive = True
 		
-		# Good calls test
-		p.polymerize(sequences, baseAmounts, bases, basePadValue, energy, energyCostPerBase)
-	
+		seq = wholecell.processes.replication.calculateSequence(
+			chromosomeLocation, directionIsPositive, self.elongationRate, self.sequence, self.genomeLength
+			)
+		self.assertEqual(seq, 'hello')
+
+		# Negative direction
+		chromosomeLocation = len(self.sequence)
+		directionIsPositive = False
+		
+		seq = wholecell.processes.replication.calculateSequence(
+			chromosomeLocation, directionIsPositive, self.elongationRate, self.sequence, self.genomeLength
+			)
+		self.assertEqual(seq, 'enome'[::-1])
+		
+		# Loop condition positive direction
+		chromosomeLocation = len(self.sequence) - 2
+		directionIsPositive = True
+
+		seq = wholecell.processes.replication.calculateSequence(
+			chromosomeLocation, directionIsPositive, self.elongationRate, self.sequence, self.genomeLength
+			)
+		self.assertEqual(seq, 'me' + 'hel')
+		return
+		# Loop condition negative direction
+		chromosomeLocation = 2
+		directionIsPositive = False
+
+		seq = wholecell.processes.replication.calculateSequence(
+			chromosomeLocation, directionIsPositive, self.elongationRate, self.sequence, self.genomeLength
+			)
+
+		self.assertEqual(seq, 'AAC'[::-1] + 'CA'[::-1])
