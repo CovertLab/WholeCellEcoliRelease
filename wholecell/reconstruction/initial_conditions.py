65c7f082af9148d0cf5917e4d7a5a630cf7b415b
John Mason <jmason42@stanford.edu>
2014-05-28 12:41:09 -0700

    Initialization code for unique ribosomes

--- wholecell/reconstruction/initial_conditions.py
+++ wholecell/reconstruction/initial_conditions.py
@@ -1,16 +1,25 @@
 
+from __future__ import division
+
+from itertools import izip
+
 import numpy as np
+
 from wholecell.containers.bulk_objects_container import BulkObjectsContainer
 from wholecell.reconstruction.fitter import countsFromMassAndExpression
 from wholecell.reconstruction.fitter import normalize
-
 
 def calcInitialConditions(sim, kb):
 	randStream = sim.randStream
 
 	bulk = sim.states['BulkMolecules']
+	unique = sim.states["UniqueMolecules"]
 
+	# Set up states
 	initializeBulk(bulk.container, kb, randStream)
+
+	# Modify states for specific processes
+	initializeTranslation(bulk.container, unique.container, kb, randStream)
 
 
 def initializeBulk(bulkContainer, kb, randStream):
@@ -239,3 +248,165 @@ def initializeBulkWater(kb, bulkContainer, randStream):
 	h2oView.countIs(
 		(avgCellWaterMassInit) / mwH2O * nAvogadro
 		)
+
+def initializeTranslation(bulkContainer, uniqueContainer, kb, randStream):
+	# Calculate the number of possible ribosomes
+
+	subunits = bulkContainer.countsView(["RRLA-RRNA[c]", "RRSA-RRNA[c]", "RRFA-RRNA[c]"])
+
+	subunitStoich = np.array([1, 1, 1])
+
+	activeRibosomeMax = (subunits.counts() * subunitStoich).min()
+
+	# Calculate the number of ribosomes that should be active
+
+	elngRate = kb.ribosomeElongationRate.to('amino_acid / s').magnitude
+
+	monomerIds = kb.monomerData["id"]
+	monomers = bulkContainer.countsView(monomerIds)
+	monomerCounts = monomers.counts()
+	monomerLengths = kb.monomerData["length"].to("count").magnitude
+
+	monomerLengthAverage = np.dot(monomerCounts, monomerLengths) / monomerCounts.sum()
+
+	fractionActive = 1 - elngRate/monomerLengthAverage
+
+	activeRibosomeCount = np.int64(activeRibosomeMax * fractionActive)
+
+	# Choose proteins to polymerize
+
+	mrnaIds = kb.rnaData["id"][kb.rnaIndexToMonomerMapping]
+	mrnas = bulkContainer.countsView(mrnaIds)
+	mrnaCounts = mrnas.counts()
+	mrnaFractions = mrnaCounts / mrnaCounts.sum()
+
+	monomerCountsPolymerizing = randStream.mnrnd(activeRibosomeCount, mrnaFractions)
+
+	# Compute the current protein mass
+
+	monomerMasses = (kb.monomerData["mw"].to("fg / mole").magnitude /
+		kb.nAvogadro.to("1 / mole").magnitude)
+	monomerMassTotal = np.dot(monomerCounts, monomerMasses)
+
+	# Reduce the number of ribosome subunits
+
+	subunits.countsDec(activeRibosomeCount * subunitStoich)
+
+	# Reduce the counts of monomers based on those polymerizing
+
+	monomers.countsDec(
+		np.fmin(monomerCounts, monomerCountsPolymerizing)
+		)
+
+	# Compute the difference in monomer mass
+
+	monomerCountsNew = monomers.counts()
+	monomerMassTotalNew = np.dot(monomerCountsNew, monomerMasses)
+	monomerMassDifference = monomerMassTotal - monomerMassTotalNew
+
+	# Create the lists of protein indexes
+
+	proteinIndexes = np.empty(activeRibosomeCount, np.int64)
+
+	startIndex = 0
+	for proteinIndex, counts in enumerate(monomerCountsPolymerizing):
+		proteinIndexes[startIndex:startIndex+counts] = proteinIndex
+
+		startIndex += counts
+
+	# Choose random protein lengths
+
+	maxPeptideLengths = monomerLengths[proteinIndexes]
+
+	peptideLengths = (randStream.rand(activeRibosomeCount) * maxPeptideLengths).astype(np.int64)
+
+	# Compute peptide masses
+	# TODO: refactor this logic (adapted from UniquePolypeptideElongation)
+
+	letterSequences = kb.monomerData["sequence"]
+
+	maxLen = np.int64(monomerLengths.max() + elngRate)
+
+	monomerSequences = np.empty((letterSequences.shape[0], maxLen), np.int64)
+	monomerSequences.fill(-1)
+
+	aaIDs_singleLetter = kb.aaIDs_singleLetter[:]
+	del aaIDs_singleLetter[kb.aaIDs.index("SEC-L[c]")]
+
+	aaMapping = {aa:i for i, aa in enumerate(aaIDs_singleLetter)}
+
+	aaMapping["U"] = aaMapping["C"]
+
+	for i, sequence in enumerate(letterSequences):
+		for j, letter in enumerate(sequence):
+			monomerSequences[i, j] = aaMapping[letter]
+
+	h2oWeight = (
+		kb.bulkMolecules[
+			kb.bulkMolecules["moleculeId"] == "H2O[c]"
+			]["mass"].to("fg / mole").magnitude /
+		kb.nAvogadro.to("1 / mole").magnitude
+		)
+
+	aaWeights = np.array([
+		kb.bulkMolecules[
+			kb.bulkMolecules["moleculeId"] == x
+			]["mass"].to("fg / mole").magnitude /
+		kb.nAvogadro.to("1 / mole").magnitude
+		for x in kb.aaIDs
+		if len(kb.bulkMolecules[kb.bulkMolecules["moleculeId"] == x]["mass"])
+		])
+
+	aaWeightsIncorporated = aaWeights - h2oWeight
+
+	# TODO: check whether there should be an additional water mass
+	peptideMasses = np.array([
+		aaWeightsIncorporated[monomerSequences[proteinIndex, :length]].sum()
+		for proteinIndex, length in izip(proteinIndexes, peptideLengths)
+		])
+
+	# Create the unique molecules representations of the ribosomes
+
+	activeRibosomes = uniqueContainer.objectsNew(
+		"activeRibosome",
+		activeRibosomeCount
+		)
+
+	activeRibosomes.attrIs(
+		proteinIndex = proteinIndexes,
+		peptideLength = peptideLengths,
+		massDiffProtein = peptideMasses
+		)
+
+	# Compute the amount of monomer mass that needs to be restored
+	monomerMassNeeded = monomerMassDifference - peptideMasses.sum()
+
+	# Using their relative fractions, add new monomers until the mass is restored
+	monomerFractions = monomerCounts / monomerCounts.sum()
+	monomerFractionsCumulative = monomerFractions.cumsum()
+
+	monomerCountsIncremented = np.zeros_like(monomerCounts)
+
+	while True:
+		monomerIndex = np.where( # sample once from a multinomial distribution
+			monomerFractionsCumulative > randStream.rand()
+			)[0][0]
+
+		monomerMass = monomerMasses[monomerIndex]
+
+		if monomerMass < monomerMassNeeded:
+			monomerMassNeeded -= monomerMass
+			monomerCountsIncremented[monomerIndex] += 1
+
+		elif monomerMass / 2 < monomerMassNeeded:
+			monomerMassNeeded -= monomerMass
+			monomerCountsIncremented[monomerIndex] += 1
+
+			break
+
+		else:
+			break
+
+	monomers.countsInc(monomerCountsIncremented)
+
+
