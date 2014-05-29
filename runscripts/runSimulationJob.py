954b108391303a8482c494dd48e8413a095999d4
macklin <macklin@server>
2014-05-29 16:02:34 -0700

    Checking that the user doesn't accidentally specify a WC_ environmental variable that doesn't get used in runscripts/runSimulationJob.py.

--- runscripts/runSimulationJob.py
+++ runscripts/runSimulationJob.py
@@ -23,41 +23,59 @@ def main(submissionTime):
 	if os.environ.has_key("ARRAY_ID"):
 		seed = int(os.environ["ARRAY_ID"]) - 1
 
+	# We use this to check if any undefined WC_* environmental variables
+	# were accidentally specified by the user
+	wcEnvVars = [x for x in os.environ if x.startswith("WC_")]
+
 	states = wholecell.sim.sim_definition.SIM_KWARG_DEFAULTS["states"]
 	if os.environ.has_key("WC_STATES") and len(os.environ["WC_STATES"]):
 		states = json.loads(os.environ["WC_STATES"])
+		wcEnvVars.remove("WC_STATES")
 
 	processes = wholecell.sim.sim_definition.SIM_KWARG_DEFAULTS["processes"]
 	if os.environ.has_key("WC_PROCESSES") and len(os.environ["WC_PROCESSES"]):
 		processes = json.loads(os.environ["WC_PROCESSES"])
+		wcEnvVars.remove("WC_PROCESSES")
 
 	listeners = wholecell.sim.sim_definition.SIM_KWARG_DEFAULTS["listeners"]
 	if os.environ.has_key("WC_LISTENERS") and len(os.environ["WC_LISTENERS"]):
 		listeners = json.loads(os.environ["WC_LISTENERS"])
+		wcEnvVars.remove("WC_LISTENERS")
 
 	hooks = wholecell.sim.sim_definition.SIM_KWARG_DEFAULTS["hooks"]
 	if os.environ.has_key("WC_HOOKS") and len(os.environ["WC_HOOKS"]):
 		hooks = json.loads(os.environ["WC_HOOKS"])
+		wcEnvVars.remove("WC_HOOKS")
 
 	lengthSec = wholecell.sim.sim_definition.SIM_KWARG_DEFAULTS["lengthSec"]
 	if os.environ.has_key("WC_LENGTHSEC") and len(os.environ["WC_LENGTHSEC"]):
 		lengthSec = int(os.environ["WC_LENGTHSEC"])
+		wcEnvVars.remove("WC_LENGTHSEC")
 
 	timeStepSec = wholecell.sim.sim_definition.SIM_KWARG_DEFAULTS["timeStepSec"]
 	if os.environ.has_key("WC_TIMESTEPSEC") and len(os.environ["WC_TIMESTEPSEC"]):
 		timeStepSec = float(timeStepSec)
+		wcEnvVars.remove("WC_TIMESTEPSEC")
 
 	logToShell = wholecell.sim.sim_definition.SIM_KWARG_DEFAULTS["logToShell"]
 	if os.environ.has_key("WC_LOGTOSHELL") and len(os.environ["WC_LOGTOSHELL"]):
 		logToShell = json.loads(os.environ["WC_LOGTOSHELL"])
+		wcEnvVars.remove("WC_LOGTOSHELL")
 
 	logToDiskEvery = wholecell.sim.sim_definition.SIM_KWARG_DEFAULTS["logToDiskEvery"]
 	if os.environ.has_key("WC_LOGTODISKEVERY") and len(os.environ["WC_LOGTODISKEVERY"]):
 		logToDiskEvery = int(os.environ["WC_LOGTODISKEVERY"])
+		wcEnvVars.remove("WC_LOGTODISKEVERY")
 
 	rebuildKB = wholecell.sim.sim_definition.SIM_KWARG_DEFAULTS["rebuildKB"]
 	if os.environ.has_key("WC_REBUILDKB") and len(os.environ["WC_REBUILDKB"]):
 		rebuildKB = json.loads(os.environ["WC_REBUILDKB"])
+		wcEnvVars.remove("WC_REBUILDKB")
+
+	assert (len(wcEnvVars) == 0), (
+		"The following WC_* environmental variables were specified but " +
+		"have no defined function: %s" % wcEnvVars
+		)
 
 	outputDir = os.path.join("out", "simOut", "%s" % submissionTime, "%06d" % seed)
 
