4628d286845d6d2b9b8fbc2af5483ef2e4ca532d
John Mason <jmason42@stanford.edu>
2014-06-09 12:58:01 -0700

    Simplified gene copy number analysis script, since it was taking a while to run

--- wholecell/analysis/single/geneCopyNumber.py
+++ wholecell/analysis/single/geneCopyNumber.py
@@ -28,7 +28,7 @@ def main(simOutDir, plotOutDir, plotOutFileName):
 
 	table = h.root.GeneCopyNumber
 
-	geneCopyNumber = table.read(0, None, 1, 'gene_copy_number')
+	# geneCopyNumber = table.read(0, None, 1, 'gene_copy_number')
 	totalCopyNumber = table.read(0, None, 1, 'total_copy_number')
 
 	h.close()
@@ -40,20 +40,20 @@ def main(simOutDir, plotOutDir, plotOutFileName):
 
 	plt.figure(figsize = (8.5, 11))
 
-	plt.subplot(2,1,1)
+	# plt.subplot(2,1,1)
 	plt.plot(time / 60., totalCopyNumber, linewidth = 2)
 	plt.xlabel("Time (min)")
 	plt.ylabel("Count")
 	plt.title("Total gene copy number")
 
 
-	plt.subplot(2,1,2)
-	plt.plot(time / 60., geneCopyNumber, linewidth = 2)
-	plt.xlabel("Time (min)")
-	plt.ylabel("Count")
-	plt.title("Individual gene copy number")
+	# plt.subplot(2,1,2)
+	# plt.plot(time / 60., geneCopyNumber, linewidth = 2)
+	# plt.xlabel("Time (min)")
+	# plt.ylabel("Count")
+	# plt.title("Individual gene copy number")
 
-	plt.subplots_adjust(hspace = 0.5, wspace = 0.5)
+	# plt.subplots_adjust(hspace = 0.5, wspace = 0.5)
 
 	plt.savefig(os.path.join(plotOutDir, plotOutFileName))
 
