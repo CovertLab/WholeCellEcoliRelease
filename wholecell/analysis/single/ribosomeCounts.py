4d4042567c6ecbcaac3781e38bab19249be00f8e
macklin <macklin@server>
2014-06-11 10:59:19 -0700

    Adding 'kbFile' argument to wholecell/analysis/single/ribosomeCounts.py.

--- wholecell/analysis/single/ribosomeCounts.py
+++ wholecell/analysis/single/ribosomeCounts.py
@@ -18,9 +18,11 @@ import matplotlib
 matplotlib.use("Agg")
 from matplotlib import pyplot as plt
 
+import wholecell.utils.constants
+
 # from wholecell.containers.unique_molecules_data import UniqueMoleculesData
 
-def main(simOutDir, plotOutDir, plotOutFileName):
+def main(simOutDir, plotOutDir, plotOutFileName, kbFile):
 
 	if not os.path.isdir(simOutDir):
 		raise Exception, "simOutDir does not currently exist as a directory"
@@ -63,11 +65,17 @@ def main(simOutDir, plotOutDir, plotOutFileName):
 	h.close()
 
 if __name__ == "__main__":
+	defaultKBFile = os.path.join(
+			wholecell.utils.constants.SERIALIZED_KB_DIR,
+			wholecell.utils.constants.SERIALIZED_KB_FIT_FILENAME
+			)
+
 	parser = argparse.ArgumentParser()
 	parser.add_argument("simOutDir", help = "Directory containing simulation output", type = str)
 	parser.add_argument("plotOutDir", help = "Directory containing plot output (will get created if necessary)", type = str)
 	parser.add_argument("plotOutFileName", help = "File name to produce", type = str)
+	parser.add_argument("--kbFile", help = "KB file name", type = str, default = defaultKBFile)
 
 	args = parser.parse_args().__dict__
 
-	main(args["simOutDir"], args["plotOutDir"], args["plotOutFileName"])
+	main(args["simOutDir"], args["plotOutDir"], args["plotOutFileName"], args["kbFile"])
