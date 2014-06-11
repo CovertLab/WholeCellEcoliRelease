d50215779abcda8f96763b420e5bd3542f97743b
macklin <macklin@server>
2014-06-11 15:24:58 -0700

    Adding getSimOptsFromEnvVars() to wholecell/sim/sim_definition.py.

--- wholecell/sim/sim_definition.py
+++ wholecell/sim/sim_definition.py
@@ -11,6 +11,8 @@ import collections
 import os
 import wholecell.utils.constants
 
+import json
+
 # References to sub-simulation abstractions
 
 # States
@@ -258,3 +260,51 @@ class SimDefinition(object):
 			}
 
 	# TODO: save, load
+
+def getSimOptsFromEnvVars(optionsToNotGetFromEnvVars = None):
+	optionsToNotGetFromEnvVars = optionsToNotGetFromEnvVars or []
+
+	# We use this to check if any undefined WC_* environmental variables
+	# were accidentally specified by the user
+	wcEnvVars = [x for x in os.environ if x.startswith("WC_")]
+
+	optionsAndEnvVars = dict(
+		seed = ("WC_SEED", int),
+		states = ("WC_STATES", json.loads),
+		processes = ("WC_PROCESSES", json.loads),
+		listeners = ("WC_LISTENERS", json.loads),
+		hooks = ("WC_HOOKS", json.loads),
+		lengthSec = ("WC_LENGTHSEC", int),
+		timeStepSec = ("WC_TIMESTEPSEC", float),
+		logToShell = ("WC_LOGTOSHELL", json.loads),
+		shellColumnHeaders = ("WC_SHELLCOLUMNSHEADERS", json.loads),
+		logToDisk = ("WC_LOGTODISK", json.loads),
+		outputDir = ("WC_OUTPUTDIR", json.loads),
+		overwriteExistingFiles = ("WC_OVERWRITEEXISTINGFILES", json.loads),
+		logToDiskEvery = ("WC_LOGTODISKEVERY", int),
+		kbLocation = ("WC_KBLOCATION", json.loads)
+		)
+
+	# These are options that the calling routine might set itself
+	# While it could just overwrite them silently, removing them here
+	# will at least alert the user
+	for opt in optionsToNotGetFromEnvVars:
+		del optionsAndEnvVars[opt]
+
+	simOpts = {}
+
+	# Get simulation options from environmental variables
+	for option, (envVar, handler) in optionsAndEnvVars.iteritems():
+		if os.environ.has_key(envVar) and len(os.environ[envVar]):
+			simOpts[option] = handler(os.environ[envVar])
+			wcEnvVars.remove(envVar)
+		else:
+			simOpts[option] = wholecell.sim.sim_definition.SIM_KWARG_DEFAULTS[option]
+
+	# Check for extraneous environmental variables (probably typos by the user)
+	assert (len(wcEnvVars) == 0), (
+		"The following WC_* environmental variables were specified but " +
+		"have no defined function: %s" % wcEnvVars
+		)
+
+	return simOpts
