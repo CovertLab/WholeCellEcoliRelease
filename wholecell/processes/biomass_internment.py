6dc3115aa61bae44aeb0c0623f97ce9c8077abb7
John Mason <jmason42@stanford.edu>
2014-06-19 21:57:17 -0700

    Added a process for interning biomass constituents that are not processed into macromolecules

---
+++ wholecell/processes/biomass_internment.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""
+BiomassInternment
+
+A process that only performs requests, interning the molecules over time steps.
+Useful for processes that are sensitive to the metabolic state of the cell.
+
+@author: John Mason
+@organization: Covert Lab, Department of Bioengineering, Stanford University
+@date: Created 5/12/2014
+"""
+
+from __future__ import division
+
+import numpy as np
+
+import wholecell.processes.process
+from wholecell.utils.constants import REQUEST_PRIORITY_INTERN
+
+class BiomassInternment(wholecell.processes.process.Process):
+	""" BiomassInternment """
+
+	_name = "BiomassInternment"
+
+	# Constructor
+	def __init__(self):
+		super(BiomassInternment, self).__init__()
+
+	# Construct object graph
+	def initialize(self, sim, kb):
+		super(BiomassInternment, self).initialize(sim, kb)
+
+		moleculeIds = (
+			list(kb.cellGlycogenFractionData["metaboliteId"]) +
+			list(kb.cellMureinFractionData["metaboliteId"]) +
+			list(kb.cellLPSFractionData["metaboliteId"]) +
+			list(kb.cellLipidFractionData["metaboliteId"]) +
+			list(kb.cellInorganicIonFractionData["metaboliteId"]) +
+			list(kb.cellSolublePoolFractionData["metaboliteId"])
+			)
+
+		self.molecules = self.bulkMoleculesView(moleculeIds)
+
+		self.bulkMoleculesRequestPriorityIs(REQUEST_PRIORITY_INTERN)
+
+
+	def calculateRequest(self):
+		self.molecules.requestAll()
+
+
+	def evolveState(self):
+		pass
