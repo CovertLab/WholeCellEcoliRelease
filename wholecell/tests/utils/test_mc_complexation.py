Fixed bug in reaction overlap calculation

wholecell/tests/utils/test_mc_complexation.py
@@ -73,13 +73,13 @@ class Test_mc_complexation(unittest.TestCase):
 								 [ 0,  0,  0, -1],
 								 [ 0,  0,  0,  1]], dtype=np.int64)
 		seed = 0
-		# moleculeCounts = np.array([0, 6, 8, 0, 0, 0], dtype=np.int64)
+		moleculeCounts = np.array([0, 6, 8, 0, 0, 0], dtype=np.int64)
 
-		# prebuiltMatrices = mccBuildMatrices(stoichMatrix)
+		prebuiltMatrices = mccBuildMatrices(stoichMatrix)
 
-		# updatedMoleculeCounts = mccFormComplexesWithPrebuiltMatrices(
-		# 	moleculeCounts,
-		# 	seed,
-		# 	stoichMatrix,
-		# 	*prebuiltMatrices
-		# 	)
+		updatedMoleculeCounts = mccFormComplexesWithPrebuiltMatrices(
+			moleculeCounts,
+			seed,
+			stoichMatrix,
+			*prebuiltMatrices
+			)