Fixed bug in mass diff calculation

wholecell/states/unique_molecules.py
@@ -173,7 +173,8 @@ class UniqueMolecules(wholecell.states.state.State):
 
 			massDiffs = molecules.attrsAsStructArray(*submassDiffNames).view((np.float64, len(submassDiffNames)))
 
-			masses[processIndexes, :] += massDiffs
+			for processIndex in np.arange(self._nProcesses + 1):
+				masses[processIndex, :] += massDiffs[processIndex == processIndexes, :].sum(0)
 
 		return masses
 