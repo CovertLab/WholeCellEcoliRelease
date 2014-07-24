Fixed sign in objective.  Not sure why it was more or less working with the wrong sign...

wholecell/utils/modular_fba.py
@@ -674,7 +674,7 @@ class FluxBalanceAnalysis(object):
 			self._f = cvxopt.matrix(-objectiveFunction)
 
 		else:
-			self._f = cvxopt.matrix(-objectiveFunction)
+			self._f = cvxopt.matrix(objectiveFunction)
 
 		self._lowerBound = np.empty(self._nEdges, np.float64)
 		self._lowerBound.fill(self._lowerBoundDefault)