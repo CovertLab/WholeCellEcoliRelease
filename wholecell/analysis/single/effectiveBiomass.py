f1930fcab78b9e205af815044150b182843fd92a
John Mason <jmason42@stanford.edu>
2014-07-31 17:12:50 -0700

    Analysis script for plotting the 'effective biomass'

--- wholecell/analysis/single/effectiveBiomass.py
+++ wholecell/analysis/single/effectiveBiomass.py
@@ -14,10 +14,13 @@ import cPickle
 import tables
 import numpy as np
 import matplotlib
-# matplotlib.use("Agg")
+matplotlib.use("Agg")
 from matplotlib import pyplot as plt
+import scipy.cluster.hierarchy as sch
 
 import wholecell.utils.constants
+
+FLUX_UNITS = "M/s"
 
 def main(simOutDir, plotOutDir, plotOutFileName, kbFile):
 
@@ -27,50 +30,48 @@ def main(simOutDir, plotOutDir, plotOutFileName, kbFile):
 	if not os.path.exists(plotOutDir):
 		os.mkdir(plotOutDir)
 
-	with tables.open_file(os.path.join(simOutDir, "EffectiveBiomassObjective.hdf")) as h5file:
-		timeStep = h5file.root.EffectiveBiomassObjective.col("time")
-		effectiveBiomass = h5file.root.EffectiveBiomassObjective.col("effectiveBiomassObjective")
-		nominalBiomass = h5file.root.EffectiveBiomassObjective.attrs.wildtypeBiomass
-		metaboliteIds = h5file.root.EffectiveBiomassObjective.attrs.metaboliteIds
+	with tables.open_file(os.path.join(simOutDir, "FBAResults.hdf")) as h5file:
+		timeStep = h5file.root.FBAResults.col("timeStep")
+		outputFluxes = h5file.root.FBAResults.col("outputFluxes")
 
-	ntpIds = ["ATP[c]", "CTP[c]", "GTP[c]", "UTP[c]"]
-	ntpIdxs = np.array([np.where(ntpId == metaboliteIds)[0][0] for ntpId in ntpIds])
+		names = h5file.root.names
+		outputMoleculeIDs = np.array(names.outputMoleculeIDs.read())
 
-	aaIds = cPickle.load(open(kbFile)).aaIDs
-	aaIdxs = np.array([np.where(aaId == metaboliteIds)[0][0] for aaId in aaIds])
+	fig = plt.figure(figsize = (30, 15))
 
-	ntpEffective = effectiveBiomass[:, ntpIdxs].sum(1)
+	normalized = (outputFluxes / np.max(np.abs(outputFluxes[20:, :]), 0)).transpose()
 
-	aaEffective = effectiveBiomass[:, aaIdxs].sum(1)
+	linkage = sch.linkage(normalized, method = "centroid")
+	dendro = sch.dendrogram(linkage, orientation="right", no_plot=True)
+	index = dendro["leaves"]
 
-	# normalizedEffective = np.nan_to_num(effectiveBiomass / effectiveBiomass.sum(1)[:, np.newaxis])
-	# normalizedNominal = np.nan_to_num(nominalBiomass / nominalBiomass.sum())
+	ax = fig.gca()
 
-	# relativeEffective =  np.nan_to_num(effectiveBiomass / nominalBiomass)
-	# relNormEffective =  np.nan_to_num(normalizedEffective / normalizedNominal)
+	ax.matshow(
+		np.fmax(np.fmin((normalized[index, :]+1)/2, +1), 0), # colormap expects values from 0 to 1
+		cmap = plt.get_cmap("coolwarm")
+		)
 
-	# plt.figure(figsize = (8.5, 11))
+	ax.set_yticks(np.arange(len(index)))
+	ax.set_yticklabels(outputMoleculeIDs[np.array(index)], size = 5)
 
-	import ipdb; ipdb.set_trace()
+	plt.title("Relative FBA production rates (red = production, blue = consumption)")
 
-	# plt.savefig(os.path.join(plotOutDir, plotOutFileName))
+	plt.savefig(os.path.join(plotOutDir, plotOutFileName))
 
 
 if __name__ == "__main__":
-	# defaultKBFile = os.path.join(
-	# 		wholecell.utils.constants.SERIALIZED_KB_DIR,
-	# 		wholecell.utils.constants.SERIALIZED_KB_FIT_FILENAME
-	# 		)
+	defaultKBFile = os.path.join(
+			wholecell.utils.constants.SERIALIZED_KB_DIR,
+			wholecell.utils.constants.SERIALIZED_KB_FIT_FILENAME
+			)
 
-	# parser = argparse.ArgumentParser()
-	# parser.add_argument("simOutDir", help = "Directory containing simulation output", type = str)
-	# parser.add_argument("plotOutDir", help = "Directory containing plot output (will get created if necessary)", type = str)
-	# parser.add_argument("plotOutFileName", help = "File name to produce", type = str)
-	# parser.add_argument("--kbFile", help = "KB file name", type = str, default = defaultKBFile)
+	parser = argparse.ArgumentParser()
+	parser.add_argument("simOutDir", help = "Directory containing simulation output", type = str)
+	parser.add_argument("plotOutDir", help = "Directory containing plot output (will get created if necessary)", type = str)
+	parser.add_argument("plotOutFileName", help = "File name to produce", type = str)
+	parser.add_argument("--kbFile", help = "KB file name", type = str, default = defaultKBFile)
 
-	# args = parser.parse_args().__dict__
+	args = parser.parse_args().__dict__
 
-	# main(args["simOutDir"], args["plotOutDir"], args["plotOutFileName"], args["kbFile"])
-
-	# TODO: write this to provide meaningful output
-	pass
+	main(args["simOutDir"], args["plotOutDir"], args["plotOutFileName"], args["kbFile"])
