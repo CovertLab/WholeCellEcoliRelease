5826b5d6ad5b4780551e9e28b575e2a8156ee4e1
John Mason <jmason42@stanford.edu>
2014-08-13 12:04:03 -0700

    Prototype for model-specific simulations

---
+++ models/ecoli/sim/simulation.py
@@ -0,0 +1,115 @@
+
+from wholecell.sim.simulation import simulationFactory
+
+# States
+from wholecell.states.bulk_molecules import BulkMolecules
+from wholecell.states.unique_molecules import UniqueMolecules
+from wholecell.states.bulk_chromosome import BulkChromosome
+
+# Processes
+from models.ecoli.processes.complexation import Complexation
+from models.ecoli.processes.metabolism import Metabolism
+from models.ecoli.processes.rna_degradation import RnaDegradation
+from models.ecoli.processes.replication import Replication
+from models.ecoli.processes.translation.unique_polypeptide_initiation import UniquePolypeptideInitiation
+from models.ecoli.processes.translation.unique_polypeptide_elongation import UniquePolypeptideElongation
+from models.ecoli.processes.transcription.unique_transcript_initiation import UniqueTranscriptInitiation
+from models.ecoli.processes.transcription.unique_transcript_elongation import UniqueTranscriptElongation
+from models.ecoli.processes.protein_degradation import ProteinDegradation
+from models.ecoli.processes.atp_usage import AtpUsage
+
+# Listeners
+from models.ecoli.listeners.mass import Mass
+from models.ecoli.listeners.replication_fork import ReplicationForkPosition
+from models.ecoli.listeners.ntp_usage import NtpUsage
+from models.ecoli.listeners.aa_usage import AAUsage
+from models.ecoli.listeners.ribosome_stalling import RibosomeStalling
+from models.ecoli.listeners.gene_copy_number import GeneCopyNumber
+from models.ecoli.listeners.unique_molecule_counts import UniqueMoleculeCounts
+from wholecell.listeners.evaluation_time import EvaluationTime
+from models.ecoli.listeners.fba_results import FBAResults
+
+from models.ecoli.sim.initial_conditions import calcInitialConditions
+
+EcoliSimulation = simulationFactory(
+	states = [BulkMolecules, UniqueMolecules, BulkChromosome],
+	processes = [
+		Metabolism,
+		RnaDegradation,
+		UniqueTranscriptInitiation,
+		UniqueTranscriptElongation,
+		UniquePolypeptideInitiation,
+		UniquePolypeptideElongation,
+		Replication,
+		ProteinDegradation,
+		Complexation,
+		AtpUsage
+	], 
+	listeners = [
+		Mass,
+		ReplicationForkPosition,
+		NtpUsage,
+		AAUsage,
+		RibosomeStalling,
+		GeneCopyNumber,
+		UniqueMoleculeCounts,
+		EvaluationTime,
+		FBAResults
+	],
+	hooks = [], # same as default
+	lengthSec = 3600, # same as default
+	timeStepSec = 1, # same as default
+	logToShell = True, # same as default
+	shellColumnHeaders = [
+		"Time (s)",
+		"Dry mass (fg)",
+		"Dry mass fold change",
+		"Protein fold change",
+		"RNA fold change",
+		"Expected fold change"
+	],
+	logToDisk = False, # same as default
+	initialConditionsFunction = calcInitialConditions,
+	)
+
+sim = EcoliSimulation() # basic sim
+
+sim = EcoliSimulation(seed = 1, lengthSec = 100)
+
+sim = EcoliSimulation(logToDisk = True, outputDir = ..., logToDiskEvery = 10)
+
+
+"""
+
+Factory (required):
+
+states
+processes
+listeners
+kbLocation
+initialConditionsFunction
+
+Factory (optional):
+
+hooks (default ())
+lengthSec (default 3600)
+timeStepSec (default 1)
+logToShell (default True)
+shellColumnHeaders (default ?)
+logToDisk (default False)
+outputDir (default None)
+logToDiskEvery (default 1)
+overwriteExistingFiles (default False)
+
+Instantiation (optional):
+
+kbLocation
+seed
+lengthSec
+logToShell
+logToDisk
+outputDir
+logToDiskEvery
+overwriteExistingFiles
+
+"""
