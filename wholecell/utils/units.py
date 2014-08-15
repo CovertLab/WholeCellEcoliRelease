946381d5f96a5500757523581cf9e1828776b190
nruggero <nruggero@server>
2014-08-15 11:37:54 -0700

    Added sum and dot functions to units

--- wholecell/utils/units.py
+++ wholecell/utils/units.py
@@ -11,9 +11,27 @@ Defines/registers custom units for Pint
 """
 
 import scipy.constants
+import numpy as np
 from unum.units import *
 from unum import Unum
 
 count = Unum.unit('count',mol/(scipy.constants.Avogadro))
 nt = Unum.unit('nucleotide', count)
 aa = Unum.unit('amino_acid', count)
+
+
+def sum(array, axis = None, dtype=None, out=None, keepdims=False):
+	if type(array) != Unum:
+		raise Exception, 'Only works on Unum!\n'
+	units = array.copy()
+	units._value = 1
+	return units * np.sum(array.asNumber(), axis, dtype, out, keepdims)
+
+def dot(a, b, out):
+	if type(a) != Unum or type(b) != Unum:
+		raise Exception, 'Only works on Unum!\n'
+	a_units = a.copy()
+	a_units._value = 1
+	b_units = b.copy()
+	b_units._value = 1
+	return a_units * b_units * np.dot(a,b,out)
