841e622d6e477b5ee981694f5dc3d1fc85128198
nruggero <nruggero@server>
2014-08-18 16:51:00 -0700

    Added docstring

--- reconstruction/ecoli/fitter.py
+++ reconstruction/ecoli/fitter.py
@@ -216,6 +216,20 @@ def normalize(array):
 	return np.array(array).astype("float") / np.linalg.norm(array, 1)
 
 def countsFromMassAndExpression(mass, mws, relativeExpression, nAvogadro):
+	"""
+	countsFromMassAndExpression
+
+	mass 				- float -				Total mass you want counts to sum to
+	mws					- ndarray of floats -	Molecular weights of each species
+	relativeExpression	- ndarray of floats	-	Relative expression of each species
+	nAvogadro 			- float -				Avogadro's number
+
+	Example:
+		mass = 10.
+		mws = [10., 5.]
+		relativeExpression = [0.33, 0.66]
+		countsFromMassAndExpression(mass, mws, relativeExpression, nAvogadro) = 1.93e23
+	"""
 	assert np.allclose(np.sum(relativeExpression), 1)
 	assert type(mass) != unum.Unum
 	assert type(mws) != unum.Unum
