84fa4e5670e04ae2f32bcc2ff6a3afa927eef505
nruggero <nruggero@server>
2014-08-19 10:25:58 -0700

    Added aa:trna groups and all trna ids to KB

--- reconstruction/ecoli/knowledge_base.py
+++ reconstruction/ecoli/knowledge_base.py
@@ -885,7 +885,41 @@ class KnowledgeBaseEcoli(object):
 		self._cellInorganicIonFractionData['massFraction'] = fracInorganicIonMass
 
 	def _loadTrnaData(self):
-		# 
+		# Organize tRNA id data
+		self._trna_aa_sets = collections.OrderedDict([('A', ['alaT-tRNA[c]', 'alaU-tRNA[c]', 'alaV-tRNA[c]', 'alaW-tRNA[c]']),
+				('R', ['alaX-tRNA[c]', 'argQ-tRNA[c]', 'argU-tRNA[c]', 'argV-tRNA[c]', 'argW-tRNA[c]', 'argX-tRNA[c]', 'argY-tRNA[c]']),
+				('N', ['argZ-tRNA[c]', 'asnT-tRNA[c]', 'asnU-tRNA[c]', 'asnV-tRNA[c]']),
+				('D', ['RNA0-304[c]', 'aspT-tRNA[c]', 'aspU-tRNA[c]']),
+				('C', ['aspV-tRNA[c]']),
+				('E', ['glnX-tRNA[c]', 'gltT-tRNA[c]', 'gltU-tRNA[c]', 'gltV-tRNA[c]']),
+				('Q', ['cysT-tRNA[c]', 'glnU-tRNA[c]', 'glnV-tRNA[c]', 'glnW-tRNA[c]']),
+				('G', ['gltW-tRNA[c]', 'glyT-tRNA[c]', 'glyU-tRNA[c]', 'glyV-tRNA[c]', 'glyW-tRNA[c]', 'glyX-tRNA[c]']),
+				('H', ['glyY-tRNA[c]']),
+				('I', ['hisR-tRNA[c]', 'ileT-tRNA[c]', 'ileU-tRNA[c]', 'ileV-tRNA[c]', 'ileX-tRNA[c]']),
+				('L', ['RNA0-305[c]', 'leuP-tRNA[c]', 'leuQ-tRNA[c]', 'leuT-tRNA[c]', 'leuU-tRNA[c]', 'leuV-tRNA[c]', 'leuW-tRNA[c]', 'leuX-tRNA[c]']),
+				('K', ['leuZ-tRNA[c]', 'RNA0-303[c]', 'lysT-tRNA[c]', 'lysV-tRNA[c]', 'lysW-tRNA[c]', 'RNA0-301[c]']),
+				('M', ['RNA0-302[c]', 'metT-tRNA[c]', 'metU-tRNA[c]', 'RNA0-306[c]', 'metW-tRNA[c]', 'metY-tRNA[c]']),
+				('F', ['metZ-tRNA[c]', 'pheU-tRNA[c]']),
+				('P', ['pheV-tRNA[c]', 'proK-tRNA[c]', 'proL-tRNA[c]']),
+				('S', ['selC-tRNA[c]', 'serT-tRNA[c]', 'serU-tRNA[c]', 'serV-tRNA[c]', 'serW-tRNA[c]']),
+				('T', ['serX-tRNA[c]', 'thrT-tRNA[c]', 'thrU-tRNA[c]', 'thrV-tRNA[c]']),
+				('W', ['thrW-tRNA[c]']),
+				('Y', ['trpT-tRNA[c]', 'tyrT-tRNA[c]', 'tyrU-tRNA[c]']),
+				('U', ['proM-tRNA[c]']),
+				('V', ['tyrV-tRNA[c]', 'valT-tRNA[c]', 'valU-tRNA[c]', 'valV-tRNA[c]', 'valW-tRNA[c]', 'valX-tRNA[c]', 'valY-tRNA[c]'])])
+
+		self._trna_frame_ids = ['alaT-tRNA[c]','alaU-tRNA[c]','alaV-tRNA[c]','alaW-tRNA[c]','alaX-tRNA[c]','argQ-tRNA[c]','argU-tRNA[c]',
+		'argV-tRNA[c]','argW-tRNA[c]','argX-tRNA[c]','argY-tRNA[c]','argZ-tRNA[c]','asnT-tRNA[c]','asnU-tRNA[c]','asnV-tRNA[c]','RNA0-304[c]','aspT-tRNA[c]',
+		'aspU-tRNA[c]','aspV-tRNA[c]','cysT-tRNA[c]','glnU-tRNA[c]','glnV-tRNA[c]','glnW-tRNA[c]','glnX-tRNA[c]','gltT-tRNA[c]','gltU-tRNA[c]','gltV-tRNA[c]',
+		'gltW-tRNA[c]','glyT-tRNA[c]','glyU-tRNA[c]','glyV-tRNA[c]','glyW-tRNA[c]','glyX-tRNA[c]','glyY-tRNA[c]','hisR-tRNA[c]','ileT-tRNA[c]','ileU-tRNA[c]',
+		'ileV-tRNA[c]','ileX-tRNA[c]','RNA0-305[c]','leuP-tRNA[c]','leuQ-tRNA[c]','leuT-tRNA[c]','leuU-tRNA[c]','leuV-tRNA[c]','leuW-tRNA[c]','leuX-tRNA[c]',
+		'leuZ-tRNA[c]','RNA0-303[c]','lysT-tRNA[c]','lysV-tRNA[c]','lysW-tRNA[c]','RNA0-301[c]','RNA0-302[c]','metT-tRNA[c]','metU-tRNA[c]','RNA0-306[c]',
+		'metW-tRNA[c]','metY-tRNA[c]','metZ-tRNA[c]','pheU-tRNA[c]','pheV-tRNA[c]','proK-tRNA[c]','proL-tRNA[c]','proM-tRNA[c]','selC-tRNA[c]','serT-tRNA[c]',
+		'serU-tRNA[c]','serV-tRNA[c]','serW-tRNA[c]','serX-tRNA[c]','thrT-tRNA[c]','thrU-tRNA[c]','thrV-tRNA[c]','thrW-tRNA[c]','trpT-tRNA[c]','tyrT-tRNA[c]',
+		'tyrU-tRNA[c]','tyrV-tRNA[c]','valT-tRNA[c]','valU-tRNA[c]','valV-tRNA[c]','valW-tRNA[c]','valX-tRNA[c]','valY-tRNA[c]','RNA0-300[c]']
+
+
+
 
 
 		# Organize counts data from Jakubowski et al.
