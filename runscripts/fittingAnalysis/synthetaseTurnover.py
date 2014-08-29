4bbfdce132606c8d8e885592a1684b4ec8aa078c
nruggero <nruggero@server>
2014-08-29 10:10:56 -0700

    Fixed bugs in plotting script for turnover rates

--- runscripts/fittingAnalysis/synthetaseTurnover.py
+++ runscripts/fittingAnalysis/synthetaseTurnover.py
@@ -17,15 +17,27 @@ from matplotlib import pyplot as plt
 import wholecell.utils.constants
 
 def main(plotOutDir, plotOutFileName, kbDirectory):
-	unfitKbFileName = os.join(kbDirectory, wholecell.utils.constants.SERIALIZED_KB_UNFIT_FILENAME)
-	mostfitKbFileName = os.join(kbDirectory, wholecell.utils.constants.SERIALIZED_KB_MOST_FIT_FILENAME)
+	unfitKbFileName = os.path.join(kbDirectory, wholecell.utils.constants.SERIALIZED_KB_UNFIT_FILENAME)
+	mostfitKbFileName = os.path.join(kbDirectory, wholecell.utils.constants.SERIALIZED_KB_MOST_FIT_FILENAME)
 
 	unfitKb = cPickle.load(open(unfitKbFileName, "rb"))
 	mostfitKb = cPickle.load(open(mostfitKbFileName, "rb"))
 
+	measured_synthetase_rates = unfitKb.trna_synthetase_rates
+	fit_synthetase_rates = mostfitKb.trna_synthetase_rates
+	amino_acid_labels = mostfitKb.aa_trna_groups.keys()
+
+
+	# Plotting
 	plt.figure(figsize = (8.5, 11))
 
-	measured_synthetase_rates = unfitKb.
+	plt.plot(measured_synthetase_rates, 'x', label='Jakubowski et al. 1984')
+	plt.plot(fit_synthetase_rates, 'o', label='fit')
+
+	plt.xlabel('amino acid')
+	plt.ylabel('1/s')
+	plt.title('trna synthetase turnover rates')
+	plt.legend()
 
 	plt.savefig(os.path.join(plotOutDir, plotOutFileName))
 
@@ -36,7 +48,7 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("plotOutDir", help = "Directory containing plot output (will get created if necessary)", type = str)
 	parser.add_argument("plotOutFileName", help = "File name to produce", type = str)
-	parser.add_argument("--kbDirectory", help = "KB file name", type = str, default = defaultKBFile)
+	parser.add_argument("--kbDirectory", help = "KB file name", type = str, default = defaultKBDirectory)
 
 	args = parser.parse_args().__dict__
 
