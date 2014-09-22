749617d868d0d333c37b0a4369237cb76fcbd70d
nruggero <nruggero@server>
2014-09-22 16:00:43 -0700

    Adding tests for fitkb1

---
+++ reconstruction/tests/test_fitkb1.py
@@ -0,0 +1,36 @@
+"""
+Test fitkb1.py
+
+@author: Nick Ruggero
+@organization: Covert Lab, Department of Bioengineering, Stanford University
+@date: Created 9/23/2014
+"""
+
+from __future__ import division
+
+import unittest
+import warnings
+
+import cPickle
+import os
+
+import wholecell.utils.constants
+
+import nose.plugins.attrib as noseAttrib
+
+class Test_fitkb1(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		pass
+
+	@classmethod
+	def tearDownClass(cls):
+		pass
+
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
