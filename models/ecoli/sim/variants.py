1706846484330d921e80a3ed4cdc1067ab19e5e7
John Mason <jmason42@stanford.edu>
2014-12-09 14:16:08 -0800

    Added a basic variant function for knockouts.

---
+++ models/ecoli/sim/variants.py
@@ -0,0 +1,32 @@
+
+CONTROL_OUTPUT = dict(
+	shortName = "control",
+	desc = "Control simulation"
+	)
+
+def variant_prototype_KO(kb, index):
+	# Knocks-out genes in order
+
+	nGenes = kb.rnaData.fullArray().size
+
+	nConditions = nGenes + 1
+
+	if (index + nGenes) % nConditions == 0:
+		return CONTROL_OUTPUT
+
+	geneIndex = (index - 1) % nConditions
+
+	synthProb = kb.rnaData["synthProb"]
+
+	synthProb[geneIndex] = 0.0
+
+	synthProb /= synthProb.sum()
+
+	kb.rnaData["synthProb"] = synthProb
+
+	geneID = kb.rnaData["id"][geneIndex]
+
+	return dict(
+		shortName = "{}_KO".format(geneID),
+		desc = "Complete knockout of {}.".format(geneID)
+		)
