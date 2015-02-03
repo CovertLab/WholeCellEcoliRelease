0658c1a93c7fed90e910d5b2cd5c0464f45e53ae
javiercarrera <jcarrera@server.edu>
2015-02-02 17:13:08 -0800

    minor change

--- models/ecoli/analysis/single/rnaseCounts.py
+++ models/ecoli/analysis/single/rnaseCounts.py
@@ -38,7 +38,7 @@ def main(simOutDir, plotOutDir, plotOutFileName, kbFile):
 	#rnapIndex = moleculeIds.index(rnapId)
 	#rnapCountsBulk = bulkMolecules.read(0, None, 1, "counts")[:, rnapIndex]
 
-	RNAP_RNA_IDS = ["EG10856-MONOMER[p]", "EG11620-MONOMER[c]", "EG10857-MONOMER[c]", "G7175-MONOMER[c]", "EG10858-MONOMER[c]", "EG10859-MONOMER[c]", "EG11299-MONOMER[c]", "EG10860-MONOMER[c]", "EG10861-MONOMER[c]", "G7365-MONOMER[c]", "EG10862-MONOMER[c]", "EG10863-MONOMER[c]", "EG11259-MONOMER[c]", "EG11547-MONOMER[c]"]
+	RNAP_RNA_IDS = ["EG10856-MONOMER[p]", "EG11620-MONOMER[c]", "EG10857-MONOMER[c]", "G7175-MONOMER[c]", "EG10858-MONOMER[c]", "EG10859-MONOMER[c]", "EG11299-MONOMER[c]", "EG10860-MONOMER[c]", "EG10861-MONOMER[c]", "G7365-MONOMER[c]", "EG10862-MONOMER[c]", "EG10863-MONOMER[c]", "EG11259-MONOMER[c]", "EG11547-MONOMER[c]", "EG10746-MONOMER[c]", "G7842-MONOMER[c]", "EG10743-MONOMER[c]"]
 	rnapRnaIndexes = np.array([moleculeIds.index(rnapRnaId) for rnapRnaId in RNAP_RNA_IDS], np.int)
 	rnapRnaCounts = bulkMolecules.read(0, None, 1, "counts")[:, rnapRnaIndexes]
 	#import ipdb
@@ -57,17 +57,17 @@ def main(simOutDir, plotOutDir, plotOutFileName, kbFile):
 	plt.rc('xtick', labelsize=5) 
 	plt.rc('ytick', labelsize=5)
 
-	for subplotIdx in xrange(0, 14):
+	for subplotIdx in xrange(0, 17):
 		rnapRnaCountsIdx = subplotIdx
 	
-		plt.subplot(14, 1, 1 + subplotIdx)
+		plt.subplot(17, 1, 1 + subplotIdx)
 
 		plt.plot(time / 60., rnapRnaCounts[:, rnapRnaCountsIdx])
 		plt.xlabel("Time (min)", fontsize = 5)
 		plt.ylabel("Protein counts", fontsize = 5)
 		plt.title(RNAP_RNA_IDS[rnapRnaCountsIdx], fontsize = 5)
 
-	plt.subplots_adjust(hspace = 1, top = 0.95, bottom = 0.05)
+	plt.subplots_adjust(hspace = 1.2, top = 0.95, bottom = 0.05)
 	plt.savefig(os.path.join(plotOutDir, plotOutFileName))
 
 	# h.close()
