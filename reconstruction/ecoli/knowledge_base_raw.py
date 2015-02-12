060cfba66ea9829ffdf52e0bb403e311de47edc9
nruggero <nruggero@server>
2015-02-11 16:17:23 -0800

    Added knowledge_base_raw.py file

---
+++ reconstruction/ecoli/knowledge_base_raw.py
@@ -0,0 +1,23 @@
+"""
+KnowledgeBase for Ecoli
+
+Whole-cell knowledge base for Ecoli. Contains all raw, un-fit data processed
+directly from CSV flat files.
+
+@author: Nick Ruggero
+@organization: Covert Lab, Department of Bioengineering, Stanford University
+@date: Created 02/11/2015
+"""
+from __future__ import division
+
+import os
+import csv
+
+class KnowledgeBaseEcoli(object):
+	""" KnowledgeBaseEcoli """
+
+	def __init__(self):
+		pass
+
+	def load_tsv(self):
+		pass
