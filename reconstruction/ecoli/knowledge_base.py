5173976ac44325e16dd6c599bda3b5c77152a430
javiercarrera <jcarrera@server.edu>
2015-02-13 16:25:35 -0800

    RNA decay already fitted

--- reconstruction/ecoli/knowledge_base.py
+++ reconstruction/ecoli/knowledge_base.py
@@ -1819,7 +1819,7 @@ class KnowledgeBaseEcoli(object):
 		self._parameterData['fracInitFreeAAs'] = 0.001
 		self._parameterData['avgCellCellCycleProgress'] = 0.44
 		self._parameterData['timeStep'] = 1*units.s
-		self._parameterData['KcatEndoRNaseFullRNA'] = 0.1/units.s
+		self._parameterData['KcatEndoRNaseFullRNA'] = 0.008/units.s
 
 	def _loadComputeParameters(self):
 		self._parameterData['avgCellToInitalCellConvFactor'] = np.exp(np.log(2) * self._parameterData['avgCellCellCycleProgress'])
