7cba9e1dab36f3e9936fd1e934b6e75ccce278a9
nruggero <nruggero@server>
2015-02-19 09:55:58 -0800

    Fixed getMass function

--- reconstruction/ecoli/simulation_data.py
+++ reconstruction/ecoli/simulation_data.py
@@ -21,10 +21,6 @@ from reconstruction.ecoli.dataclasses.moleculeGroups import moleculeGroups
 from reconstruction.ecoli.dataclasses.state.state import State
 from reconstruction.ecoli.dataclasses.process.process import Process
 
-# Unit imports
-from wholecell.utils import units
-from wholecell.utils.unit_struct_array import UnitStructArray
-
 class SimulationDataEcoli(object):
 	""" SimulationDataEcoli """
 
@@ -38,34 +34,6 @@ class SimulationDataEcoli(object):
 		self.moleculeGroups = moleculeGroups(raw_data, self)
 		self.process = Process(raw_data, self)
 		self.state = State(raw_data, self)
-
-		# Functions
-		self._buildAllMasses(raw_data)
-
-	def _buildAllMasses(self, raw_data):
-		size = len(raw_data.rnas) + len(raw_data.proteins) + len(raw_data.proteinComplexes) + len(raw_data.metabolites) + len(raw_data.polymerized)
-		allMass = np.empty(size,
-			dtype = [
-					('id',		'a50'),
-					('mass',	"f8")
-					]
-			)
-
-		listMass = []
-		listMass.extend([(x['id'],np.sum(x['mw'])) for x in raw_data.rnas])
-		listMass.extend([(x['id'],np.sum(x['mw'])) for x in raw_data.proteins])
-		listMass.extend([(x['id'],np.sum(x['mw'])) for x in raw_data.proteinComplexes])
-		listMass.extend([(x['id'],np.sum(x['mw7.2'])) for x in raw_data.metabolites])
-		listMass.extend([(x['id'],np.sum(x['mw'])) for x in raw_data.polymerized])
-
-		allMass[:] = listMass
-
-		field_units = {
-			'id'		:	None,
-			'mass'		:	units.g / units.mol,
-			}
-
-		self._allMass = UnitStructArray(allMass, field_units)
 
 	def _addHardCodedAttributes(self):
 		self.molecular_weight_keys = [
