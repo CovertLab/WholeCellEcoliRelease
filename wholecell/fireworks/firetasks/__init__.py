d344a7954217cffecf568abab6e32812e9a4ea8b
macklin <macklin@server>
2015-04-03 12:07:11 -0700

    Importing custom firetasks in wholecell/fireworks/firetasks/__init__.py.

---
+++ wholecell/fireworks/firetasks/__init__.py
@@ -0,0 +1,6 @@
+from initKb import InitKbTask
+from symlink import SymlinkTask
+from fitKb import FitKbTask
+from variantKb import VariantKbTask
+from simulation import SimulationTask
+from analysisSingle import AnalysisSingleTask
