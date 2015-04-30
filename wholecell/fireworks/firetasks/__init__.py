b25913b8c4f5094c9ed203ce0171c6d7e8ca6575
macklin <macklin@server>
2015-04-29 18:18:16 -0700

    Adding SimulationDaughterTask import to wholecell/fireworks/firetasks/__init__.py.

--- wholecell/fireworks/firetasks/__init__.py
+++ wholecell/fireworks/firetasks/__init__.py
@@ -3,4 +3,5 @@ from symlink import SymlinkTask
 from fitKb import FitKbTask
 from variantKb import VariantKbTask
 from simulation import SimulationTask
+from simulationDaughter import SimulationDaughterTask
 from analysisSingle import AnalysisSingleTask
