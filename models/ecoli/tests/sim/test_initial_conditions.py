b2cf042662bb3d6dc57044406ea713be61a25464
nruggero <nruggero@server>
2015-07-23 14:09:42 -0700

    Added blank unit test for initial conditions

---
+++ models/ecoli/tests/sim/test_initial_conditions.py
@@ -0,0 +1,29 @@
+from __future__ import division
+
+import unittest
+import warnings
+import nose.plugins.attrib as noseAttrib
+
+import numpy as np
+import cPickle
+import os
+
+class Test_InitialConditions(unittest.TestCase):
+	@classmethod
+	def setUpClass(cls):
+		pass
+
+	@classmethod
+	def tearDownClass(cls):
+		pass
+
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+
+	@noseAttrib.attr('replicationTest')
+	def test_1(self):
+		pass
