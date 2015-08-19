fb612980400e8809683e2959d092b1a869833bd3
javiercarrera <jcarrera@stanford.edu>
2015-08-19 15:48:36 -0700

    Adding fragmentNT_IDs, endoRnaseIds, and exoRnaseIds molecule groups.

--- reconstruction/ecoli/dataclasses/moleculeGroups.py
+++ reconstruction/ecoli/dataclasses/moleculeGroups.py
@@ -54,8 +54,9 @@ class moleculeGroups(object):
 										'RRFE-RRNA[c]','RRFG-RRNA[c]','RRFH-RRNA[c]'],
 			's50_fullComplex'		:	['CPLX-50SA[c]'],
 			'aaIDs'					:	sim_data.amino_acid_1_to_3_ordered.values(),
-			'fragmentNT_IDs'		:	[x['id'].replace('Polymerized','Fragment') for x in raw_data.polymerized if x['is_ntp'] and not x['is_end']], 
-
+			'fragmentNT_IDs'		:	[x['id'].replace('Polymerized','Fragment') for x in raw_data.polymerized if x['is_ntp'] and not x['is_end']],
+			'endoRnaseIds' 			:	['EG10856-MONOMER[p]', 'EG10857-MONOMER[c]', 'G7175-MONOMER[c]', 'EG10859-MONOMER[c]', 'EG11299-MONOMER[c]', 'EG10860-MONOMER[c]', 'EG10861-MONOMER[c]', 'G7365-MONOMER[c]', 'EG10862-MONOMER[c]'],
+			'exoRnaseIds' 			:	['EG11620-MONOMER[c]', 'G7175-MONOMER[c]', 'EG10858-MONOMER[c]', 'EG10863-MONOMER[c]', 'EG11259-MONOMER[c]', 'EG11547-MONOMER[c]', 'EG10746-MONOMER[c]', 'EG10743-MONOMER[c]', 'G7842-MONOMER[c]'],
 		}
 
 		bulkMoleculesBinomialDivision = createIdsWithCompartments(raw_data.metabolites)
