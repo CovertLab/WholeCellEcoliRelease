a606200471e87060d4b9de12c132218672e44760
macklin <macklin@server>
2015-08-20 14:37:53 -0700

    Adding equilibriumReactions.tsv to knowledge_base_raw.py.

--- reconstruction/ecoli/knowledge_base_raw.py
+++ reconstruction/ecoli/knowledge_base_raw.py
@@ -41,6 +41,7 @@ LIST_OF_DICT_FILENAMES = (
 	"water.tsv",
 	"chromosome.tsv",
 	"massAtReplicationInitiation.tsv",
+	"equilibriumReactions.tsv",
 	os.path.join("massFractions", "glycogenFractions.tsv"),
 	os.path.join("massFractions", "ionFractions.tsv"),
 	os.path.join("massFractions", "LPSFractions.tsv"),
