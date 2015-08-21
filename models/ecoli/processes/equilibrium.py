17defd6a023b0325a1ddcaa2702d44ca3b62fd49
macklin <macklin@server>
2015-08-21 12:38:24 -0700

    Adding _makeMatrices() and _derivatives() to models/ecoli/processes/equilibrium.py.

--- models/ecoli/processes/equilibrium.py
+++ models/ecoli/processes/equilibrium.py
@@ -34,6 +34,7 @@ class Equilibrium(wholecell.processes.process.Process):
 		# Create matrices and vectors
 
 		self.stoichMatrix = kb.process.equilibrium.stoichMatrix().astype(np.int64)
+		self._makeMatrices()
 
 		# Build views
 
@@ -52,3 +53,50 @@ class Equilibrium(wholecell.processes.process.Process):
 		moleculeCounts = self.molecules.counts()
 
 		self.molecules.countsIs(moleculeCounts)
+
+	def _makeMatrices(self):
+		EPS = 1e-9
+
+		S = self.stoichMatrix
+		S1 = np.zeros_like(S)
+		S1[S < -1 * EPS] = -1
+		S1[S > EPS] = 1
+
+		R = (-1 * (S < 0) * S).T
+		P = (	  (S > 0) * S).T
+
+		Rp =  1. * (S1 < 0)
+		Rn = -1. * (S1 < 0)
+		Pp =  1. * (S1 > 0)
+		Pn = -1. * (S1 > 0)
+
+		self.S1 = S1
+		self.R = R
+		self.P = P
+		self.Rp = Rp
+		self.Rn = Rn
+		self.Pp = Pp
+		self.Pn = Pn
+
+		metsToRxnFluxes = self.stoichMatrix.copy()
+
+		metsToRxnFluxes[(np.abs(metsToRxnFluxes) > EPS).sum(axis = 1) > 1, : ] = 0
+		for colIdx in xrange(metsToRxnFluxes.shape[1]):
+			try:
+				firstNonZeroIdx = np.where(np.abs(metsToRxnFluxes[:, colIdx]) > EPS)[0][0]
+			except IndexError:
+				raise Exception, "Column %d of S matrix not linearly independent!" % colIdx
+			metsToRxnFluxes[:firstNonZeroIdx, colIdx] = 0
+			metsToRxnFluxes[(firstNonZeroIdx + 1):, colIdx] = 0
+
+		self.metsToRxnFluxes = metsToRxnFluxes
+
+	def _derivatives(self, v, t):
+
+		vLog = np.log(v)
+		fluxReactant = self.ratesFwd * np.exp(np.dot(self.R, vLog))
+		fluxProduct = self.ratesRev * np.exp(np.dot(self.P, vLog))
+		return (
+		 np.dot(self.Rn, fluxReactant) + np.dot(self.Rp, fluxProduct) +
+		 np.dot(self.Pp, fluxReactant) + np.dot(self.Pn, fluxProduct)
+		)
