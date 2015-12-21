a9cc823502700a01b96cb74ee887598ba9747d09
nruggero <nruggero@server>
2015-12-21 10:42:26 -0800

    Removed unused code

--- models/ecoli/processes/transcript_initiation.py
+++ models/ecoli/processes/transcript_initiation.py
@@ -66,19 +66,12 @@ class TranscriptInitiation(wholecell.processes.process.Process):
 
 		self.inactiveRnaPolys = self.bulkMoleculeView("APORNAP-CPLX[c]")
 
-
 	def calculateRequest(self):
 		self.inactiveRnaPolys.requestAll()
 
 
 	# Calculate temporal evolution
 	def evolveState(self):
-
-		#### GROWTH RATE CONTROL ####
-		ribosomeElongationRate = self.readFromListener("RibosomeData", "effectiveElongationRate") * self.timeStepSec
-		#initiationRateRrn = n_rrn * 151.595 * np.exp(0.038*-0.298 * (self.maxRibosomeElongationRate - ribosomeElongationRate))
-		#### GROWTH RATE CONTROL ####
-
 		self.activationProb = self._calculateActivationProb(
 			self.fracActiveRnap,
 			self.rnaLengths,
