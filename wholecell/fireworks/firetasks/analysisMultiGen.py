8c225829413b4df38d2acfa60f95c8d2e8ff9467
macklin <macklin@server>
2016-02-17 12:07:40 -0800

    Adding multiprocessing capability to wholecell/fireworks/firetasks/analysisMultiGen.py.

--- wholecell/fireworks/firetasks/analysisMultiGen.py
+++ wholecell/fireworks/firetasks/analysisMultiGen.py
@@ -5,6 +5,7 @@ import os
 from fireworks import FireTaskBase, explicit_serialize
 import models.ecoli.analysis.multigen
 import importlib
+import multiprocessing as mp
 
 @explicit_serialize
 class AnalysisMultiGenTask(FireTaskBase):
@@ -29,21 +30,40 @@ class AnalysisMultiGenTask(FireTaskBase):
 		fileList = os.listdir(directory)
 		fileList.sort(key=lambda x: os.stat(os.path.join(directory, x)).st_mtime, reverse=True)
 
+		if "WC_ANALYZE_FAST" in os.environ:
+			pool = mp.Pool(processes = 8)
+
 		for f in fileList:
 			if f.endswith(".pyc") or f == "__init__.py":
 				continue
 
-			print "%s: Running %s" % (time.ctime(), f)
-
 			mod = importlib.import_module("models.ecoli.analysis.multigen." + f[:-3])
-			mod.main(
+			args = (
 				self["input_seed_directory"],
 				self["output_plots_directory"],
 				f[:-3],
 				self["input_sim_data"],
 				self["input_validation_data"],
-				self["metadata"]
+				self["metadata"],
 				)
 
+			if "WC_ANALYZE_FAST" in os.environ:
+				pool.apply_async(run_function, args = (mod.main, args, f))
+			else:
+				print "%s: Running %s" % (time.ctime(), f)
+				mod.main(*args)
+
+			mod.main(*args)
+
+		if "WC_ANALYZE_FAST" in os.environ:
+			pool.close()
+			pool.join()
 		timeTotal = time.time() - startTime
 		print "Completed multiple generation analysis in %s" % (time.strftime("%H:%M:%S", time.gmtime(timeTotal)))
+
+def run_function(f, args, name):
+	try:
+		print "%s: Running %s" % (time.ctime(), name)
+		f(*args)
+	except KeyboardInterrupt:
+		import sys; sys.exit(1)
