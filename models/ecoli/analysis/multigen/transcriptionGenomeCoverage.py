974175c0e29ec8d5b36906e8941748f5c050f338
heejochoi <heejochoi@gmail.com>
2016-06-27 19:27:46 -0700

    Changing format of transcriptionGenomeCoverage plot from a scatter plot to subplots of vertical lines, and adding an exploratory secondary plot.

--- models/ecoli/analysis/multigen/transcriptionGenomeCoverage.py
+++ models/ecoli/analysis/multigen/transcriptionGenomeCoverage.py
@@ -26,6 +26,10 @@ def main(seedOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 	if not os.path.exists(plotOutDir):
 		os.mkdir(plotOutDir)
 
+
+
+
+
 	# Get all cells
 	ap = AnalysisPaths(seedOutDir, multi_gen_plot = True)
 	allDir = ap.get_cells()
@@ -34,52 +38,115 @@ def main(seedOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 	sim_data = cPickle.load(open(simDataFile, "rb"))
 	rnaIds = sim_data.process.transcription.rnaData["id"]
 	isMRna = sim_data.process.transcription.rnaData["isMRna"]
+	basalExpression = sim_data.process.transcription.rnaExpression["basal"]
+	# basalExpression = sim_data.process.transcription.rnaSynthProb["basal"]
+
+
 
 	mRnaIds = np.where(isMRna)[0]
+	mRnaBasalExpression = np.array([basalExpression[x] for x in mRnaIds])
 	mRnaNames = np.array([rnaIds[x] for x in mRnaIds])
+
+	# Sort in order of decreasing basal expression
+	descendingOrderIndexing = np.argsort(mRnaBasalExpression)[::-1]
+	mRnaBasalExpressionSorted = mRnaBasalExpression[descendingOrderIndexing]
+	mRnaNamesSorted = mRnaNames[descendingOrderIndexing]
 
 	# Get number of mRNAs transcribed
 	numGenesTranscribed = []
 	mRnasTranscribedPerGen = []
+	transcribedBool = []
+
 	for simDir in allDir:
 		simOutDir = os.path.join(simDir, "simOut")
 
 		bulkMolecules = TableReader(os.path.join(simOutDir, "BulkMolecules"))
-		moleculeCounts = bulkMolecules.readColumn("counts")[:, mRnaIds]
+		moleculeIds = bulkMolecules.readAttribute("objectNames")
+		mRnaIndexes = np.array([moleculeIds.index(x) for x in mRnaNamesSorted])
+		moleculeCounts = bulkMolecules.readColumn("counts")[:, mRnaIndexes]
 		bulkMolecules.close()
 
 		moleculeCountsSumOverTime = moleculeCounts.sum(axis = 0)
 		mRnasTranscribed = np.array([x != 0 for x in moleculeCountsSumOverTime])
-		numGenesTranscribed.append(sum(mRnasTranscribed))
+		numGenesTranscribed.append(np.sum(mRnasTranscribed))
 
-		# mRnasNotTranscribed = np.logical_not(mRnasTranscribed)
 		mRnasTranscribedNames = [mRnaNames[x] for x in np.arange(mRnaNames.shape[0]) if mRnasTranscribed[x]]
 		mRnasTranscribedPerGen.append(mRnasTranscribedNames)
 
+		transcribedBool.append(mRnasTranscribed)
+
+	transcribedBool = np.array(transcribedBool)
+	numGenesTranscribed = np.array(numGenesTranscribed)
+
 	# Plot
-	numGens = len(numGenesTranscribed)
-	ax = plt.subplot(1, 1, 1)
-	ax.scatter(np.arange(numGens), numGenesTranscribed)	
-	ax.set_xlabel("Generation", fontsize = 10)
-	ax.set_ylabel("mRNAs transcribed", fontsize = 10)
-	ax.set_title("Number of mRNAs transcribed\n(" + str(mRnaIds.shape[0]) + " total mRNA transcripts)", fontsize = 10)
-	ax.set_xticks(np.arange(numGens))
-	ax.set_ylim([0, 4353])
+	numGens = numGenesTranscribed.shape[0]
+	numMRnas = mRnaNamesSorted.shape[0]
+	fig = plt.figure(figsize = (20, 10))
 
-	# Plot names of transcribed mRNAs on the plot
+	rows = numGens + 1
+	cols = 1
+	xvals = np.arange(numMRnas)
+	intersection = np.ones((1, numMRnas), dtype = bool)
 
-	# for gen in np.arange(numGens):
-	# 	text = str()
-	# 	for mRna in mRnasTranscribedPerGen[gen]:
-	# 		text += str(mRna) + "\n"
-	# 	ax.text(gen, numGenesTranscribed[gen], text, fontsize = 6)
+	for idx in np.arange(numGens):
+		ax = plt.subplot(rows, cols, idx + 1)
+		ax.vlines(xvals, [0], transcribedBool[idx], color = "0.25")
+		ax.set_title("Generation %s" % idx, fontsize = 14)
+		ax.set_yticks([])
+		ax.set_xlim([0, numMRnas])
 
+		ax.vlines(xvals[:2600], [0], np.logical_not(transcribedBool[idx][:2600]), color = "#3399ff")
+
+		intersection = np.logical_and(intersection, transcribedBool[idx])
+
+	ax = plt.subplot(rows, cols, rows)
+	ax.set_title("Intersection of all generations", fontsize = 14)
+	ax.vlines(xvals, [0], intersection[0])
+	# ax.vlines(xvals[:2500], [0], np.logical_not(intersection[idx][:2500]), color = "#3399ff")
+	ax.set_xlabel("mRNA transcripts\nin order of decreasing expected basal expression", fontsize = 14)
+	ax.set_yticks([])
+	ax.set_xlim([0, numMRnas])
+
+	plt.subplots_adjust(hspace = 0.5, wspace = 0)
 
 	from wholecell.analysis.analysis_tools import exportFigure
 	exportFigure(plt, plotOutDir, plotOutFileName, metadata)
 
 	plt.close("all")
 
+
+	# Plot polar representation of the most differing set of mRNAs
+	mostDifferentlyTranscribed = []
+	for mRnaIdx in np.arange(numMRnas):
+		if np.sum(transcribedBool[:, mRnaIdx]) not in [0, 3]:
+			mostDifferentlyTranscribed.append(mRnaIdx)
+
+	mostDifferentlyTranscribed = np.array(mostDifferentlyTranscribed)
+	numMRnasPolar = mostDifferentlyTranscribed.shape[0]
+
+	fig = plt.figure(figsize = (12, 12))
+	plotOutFileName = "transcriptionGenomeCoveragePolar"
+
+	rGen = 1
+	rSpace = 0.25
+	ax.set_ylim([0, ((rSpace + rSpace)* numGens)])
+	ax = plt.subplot(1, 1, 1, polar = True)
+
+	for idx in np.arange(numGens - 1, -1, -1):
+		radius = np.ones(numMRnasPolar) * (idx + 1) * (rGen + rSpace) * transcribedBool[idx, mostDifferentlyTranscribed]
+		ax.plot(np.linspace(0, 2*np.pi, numMRnasPolar), radius)
+		ax.fill_between(np.linspace(0, 2*np.pi, numMRnasPolar), radius, 0)
+
+	ax.plot(np.linspace(0, 2*np.pi, 2), [True, False])
+
+	ax.grid(False)
+	ax.set_xticks([])
+	ax.set_yticks([])
+
+	from wholecell.analysis.analysis_tools import exportFigure
+	exportFigure(plt, plotOutDir, plotOutFileName, metadata)
+
+	plt.close("all")
 
 if __name__ == "__main__":
 	defaultSimDataFile = os.path.join(
