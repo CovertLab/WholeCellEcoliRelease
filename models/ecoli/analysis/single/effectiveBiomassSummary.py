Fix steady-state fluxes in fitter (still not complete), implement kcat-based enzyme-reaction matrix, add units to previous biomass flux file (and file creation in analysis script - not tested).

models/ecoli/analysis/single/effectiveBiomassSummary.py
@@ -72,22 +72,22 @@ def main(simOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile
 		logMeanNormData.append(np.log10(timecourse[BURN_IN_PERIOD:] / np.mean(timecourse[BURN_IN_PERIOD:])))
 
 	# Read previous biomass fluxes from file
-	previousBiomassMeans = sim_data.process.metabolism.previousBiomassMeans
-	previousBiomassLog10Means = sim_data.process.metabolism.previousBiomassLog10Means
-	previousBiomassStds = sim_data.process.metabolism.previousBiomassStds
+	previousBiomassMeans = {key:value.asNumber(COUNTS_UNITS / VOLUME_UNITS / TIME_UNITS) for key, value in sim_data.process.metabolism.previousBiomassMeans.iteritems()}
+	previousBiomassLog10Means = {key:value.asNumber(COUNTS_UNITS / VOLUME_UNITS / TIME_UNITS) for key, value in sim_data.process.metabolism.previousBiomassLog10Means.iteritems()}
+	previousBiomassStds = {key:value.asNumber(COUNTS_UNITS / VOLUME_UNITS / TIME_UNITS) for key, value in sim_data.process.metabolism.previousBiomassStds.iteritems()}
 
 	# Find output fluxes differing by more than a given factor from the values predicted in reconstruction
 	differingMeans, differingStds = set(), set()
 	for molID, simulationMean, simulationStd in zip(outputMoleculeIDs, mean_biomass, std_biomass):
 		if molID not in previousBiomassMeans or molID not in previousBiomassStds:
 			continue
-		observedMean = previousBiomassMeans[molID]
-		observedStd = previousBiomassStds[molID]
-		if np.abs(simulationMean - observedMean) / observedMean > (MEAN_DIFFERENCE_TOLERANCE - 1):
-			if simulationMean - observedMean > LOWER_CONC_RELEVANCE_BOUND:
+		previousMean = previousBiomassMeans[molID]
+		previousStd = previousBiomassStds[molID]
+		if np.abs(simulationMean - previousMean) / previousMean > (MEAN_DIFFERENCE_TOLERANCE - 1):
+			if simulationMean - previousMean > LOWER_CONC_RELEVANCE_BOUND:
 				differingMeans.add(molID)
-		if np.abs(simulationStd - observedStd) / observedStd > (STD_DIFFERENCE_TOLERANCE - 1):
-			if simulationStd - observedStd > LOWER_CONC_RELEVANCE_BOUND:
+		if np.abs(simulationStd - previousStd) / previousStd > (STD_DIFFERENCE_TOLERANCE - 1):
+			if simulationStd - previousStd > LOWER_CONC_RELEVANCE_BOUND:
 				differingStds.add(molID)
 
 	plt.suptitle("Fluxes for Output Molecule Reactions")
@@ -158,7 +158,13 @@ def main(simOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile
 
 	# Write current biomass fluxes to file
 	with open(os.path.join(plotOutDir, plotOutFileName + '.tsv'), 'w') as output:
-		output.write("\"molecule id\"\t\"mean flux\"\t\"mean log10 flux\"\t\"standard deviation\"\n")
+		output.write(
+			"\"molecule id\"\t\"mean flux (units.{counts} / units.{volume} / units.{time})\"\t\"mean log10 flux (units.{counts} / units.{volume} / units.{time})\"\t\"standard deviation (units.{counts} / units.{volume} / units.{time})\"\n".format(
+				counts=COUNTS_UNITS.strUnit().strip('[]'),
+				volume=VOLUME_UNITS.strUnit().strip('[]'),
+				time=TIME_UNITS.strUnit().strip('[]')
+			)
+		)
 		for molID, meanFlux, meanLogFlux, stdFlux in zip(outputMoleculeIDs, mean_biomass, mean_log10_biomass, std_biomass):
 			output.write("\t".join(['"'+unicode.encode(molID)+'"', str(meanFlux), str(meanLogFlux), str(stdFlux)]) + "\n")
 

reconstruction/ecoli/dataclasses/process/metabolism.py
@@ -228,7 +228,7 @@ class Metabolism(object):
 			for enzymeID in enzyme_list:
 				if enzymeID in enzymeExceptions:
 					enzyme_list.remove(enzymeID)
-			reactionEnzymes[reactionID] = enzyme_list
+			reactionEnzymes[reactionID] = {enzymeID:1 for enzymeIDs in enzyme_list}
 
 			# Add the reverse reaction
 			if reversible:
@@ -238,7 +238,7 @@ class Metabolism(object):
 					for moleculeID, stoichCoeff in reactionStoich[reactionID].viewitems()
 					}
 
-				reactionEnzymes[reverseReactionID] = enzyme_list
+				reactionEnzymes[reverseReactionID] = {enzymeID:1 for enzymeIDs in enzyme_list}
 				reversibleReactions.append(reactionID)
 
 		reactionRateInfo = {}
@@ -400,18 +400,30 @@ class Metabolism(object):
 	def enzymeReactionMatrix(self, reactionIDs, enzymeNames, reactionEnzymesDict):
 		"""
 		Builds a (num reactions) by (num enzymes) matrix which maps enzyme concentrations to overall reaction rate.
-		reactionEnzymesDict is a dict from reactionID:[list of enzymes catalyzing this reaction]
+		reactionEnzymesDict is a dict from reactionID:{dict of enzymes catalyzing this reaction:their associated kcat}
 		"""
 		assert sorted(reactionIDs) == sorted(reactionEnzymesDict.keys())
 
 		enzymeReactionMatrix = np.zeros((len(reactionIDs),len(enzymeNames)))
 		for rxnIdx, reactionID in enumerate(reactionIDs):
 			if reactionID in reactionEnzymesDict:
-				for enzymeName in reactionEnzymesDict[reactionID]:
+				for enzymeName, kcat in reactionEnzymesDict[reactionID].iteritems():
 					if enzymeName in enzymeNames:
 						enzymeIdx = enzymeNames.index(enzymeName)
-						enzymeReactionMatrix[rxnIdx, enzymeIdx] = 1
+						enzymeReactionMatrix[rxnIdx, enzymeIdx] = kcat
 		return enzymeReactionMatrix
+
+	def buildEnzymeReactionKcatLinks(self, reactionRateInfo, reactionEnzymesDict):
+		for constraintID, reactionInfo in reactionRateInfo.iteritems():
+			reactionID = reactionInfo["reactionID"]
+			enzymeIDs = reactionInfo["enzymeIDs"]
+			kcat = reactionInfo["kcat"][0]
+			if reactionID in reactionEnzymesDict:
+				for enzymeID in enzymeIDs:
+					if enzymeID in reactionEnzymesDict[reactionID]:
+						if kcat > reactionEnzymesDict[reactionID][enzymeID]:
+							reactionEnzymesDict[reactionID][enzymeID] = kcat
+		return reactionEnzymesDict
 
 
 class ConcentrationUpdates(object):

reconstruction/ecoli/fit_sim_data_1.py
@@ -25,6 +25,8 @@ RNA_POLY_MRNA_DEG_RATE_PER_S = np.log(2) / 30. # half-life of 30 seconds
 FRACTION_INCREASE_RIBOSOMAL_PROTEINS = 0.2  # reduce stochasticity from protein expression
 FRACTION_INCREASE_RNAP_PROTEINS = 0.05
 
+NUMERICAL_ZERO = 1e-10
+
 # TODO: establish a controlled language for function behaviors (i.e. create* set* fit*)
 
 FITNESS_THRESHOLD = 1e-9
@@ -37,9 +39,10 @@ VERBOSE = False
 
 from models.ecoli.processes.metabolism import SECRETION_PENALTY_COEFF
 
-COUNTS_UNITS = units.mmol
+COUNTS_UNITS = units.dmol
 VOLUME_UNITS = units.L
 MASS_UNITS = units.g
+TIME_UNITS = units.s
 
 def fitSimData_1(raw_data):
 
@@ -72,7 +75,7 @@ def fitSimData_1(raw_data):
 	buildTfConditionCellSpecifications(sim_data, cellSpecs)
 
 	# Fit kinetic parameters
-	findKineticCoeffs(sim_data, cellSpecs["basal"]["bulkContainer"])
+	# findKineticCoeffs(sim_data, cellSpecs["basal"]["bulkContainer"])
 
 	for condition in sorted(cellSpecs):
 		spec = cellSpecs[condition]
@@ -1228,7 +1231,7 @@ def findKineticCoeffs(sim_data, bulkContainer):
 	energyCostPerWetMass = sim_data.constants.darkATP * initDryMass / totalCellMassInit
 
 	reactionStoich = sim_data.process.metabolism.reactionStoich
-	externalExchangeMolecules = sorted(sim_data.nutrientData["secretionExchangeMolecules"])
+	externalExchangeMolecules = sorted(sim_data.nutrientData["externalExchangeMolecules"]["minimal"])
 	extMoleculeMasses = sim_data.getter.getMass(externalExchangeMolecules)
 
 	moleculeMasses = dict(zip(
@@ -1237,8 +1240,9 @@ def findKineticCoeffs(sim_data, bulkContainer):
 		))
 
 	# Make previously observed external molecule fluxes into coefficients of a biomass reaction
-	jfbaBiomassReactionStoich = {molID:-coeff for molID, coeff in sim_data.process.metabolism.previousBiomassMeans.iteritems()}
+	jfbaBiomassReactionStoich = {molID:-coeff.asNumber(COUNTS_UNITS/VOLUME_UNITS/TIME_UNITS) for molID, coeff in sim_data.process.metabolism.previousBiomassMeans.iteritems() if np.abs(coeff.asNumber(COUNTS_UNITS / VOLUME_UNITS / TIME_UNITS)) > NUMERICAL_ZERO}
 	jfbaBiomassReactionStoich["biomass"] = 1
+
 	reactionStoichWithBiomass = reactionStoich.copy()
 	reactionStoichWithBiomass["JFBA-BIOMASS-RXN"] = jfbaBiomassReactionStoich
 
@@ -1247,7 +1251,7 @@ def findKineticCoeffs(sim_data, bulkContainer):
 		externalExchangedMolecules = externalExchangeMolecules,
 		objective = {"biomass": 1},
 		objectiveType = "standard",
-		moleculeMasses=dict(zip(externalExchangeMolecules, extMoleculeMasses.asNumber(MASS_UNITS / COUNTS_UNITS))),
+		moleculeMasses=moleculeMasses,
 		secretionPenaltyCoeff = SECRETION_PENALTY_COEFF,
 		solver = "glpk",
 		maintenanceCostGAM = energyCostPerWetMass.asNumber(COUNTS_UNITS / MASS_UNITS),
@@ -1290,12 +1294,13 @@ def findKineticCoeffs(sim_data, bulkContainer):
 	constraints = [lowerBounds <= x, x <= upperBounds, S_matrix*x == 0]
 	prob = cvxpy.Problem(objective, constraints)
 
-	import ipdb; ipdb.set_trace()
-	expectedFlux = prob.solve(solver=GLPK)
+	expectedFlux = prob.solve(solver="GLPK")
 
 	fluxes = np.array(x.value)
 	fluxNames = np.array(reactionNames).reshape(-1, 1)
 	fluxesDict = dict(zip([fluxName[0] for fluxName in fluxNames], [flux[0] for flux in fluxes]))
+	
+	import ipdb; ipdb.set_trace()
 
 	# Use rabinowitz metabolite concentrations as the estimate
 	metaboliteConcentrationsDict = sim_data.process.metabolism.concDict
@@ -1332,6 +1337,8 @@ def findKineticCoeffs(sim_data, bulkContainer):
 		proteinConcDict[proteinId] = ((1. / sim_data.constants.nAvogadro / cellVolumeInit) * count)
 
 	# proteinConcDict = dict(zip(proteinIds, proteinConcentrations.asNumber(COUNTS_UNITS/VOLUME_UNITS)))
+
+	return
 
 	enzymeKinetics = EnzymeKinetics(
 	reactionRateInfo = sim_data.process.metabolism.reactionRateInfo,