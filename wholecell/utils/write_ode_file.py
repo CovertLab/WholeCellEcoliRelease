fabdc7db0b21695b5a5be129b71d9718666dcfeb
macklin <macklin@server>
2016-08-26 14:20:34 -0700

    Adding writeOdeFileWithRates() method to wholecell/utils/write_ode_file.py.

--- wholecell/utils/write_ode_file.py
+++ wholecell/utils/write_ode_file.py
@@ -10,3 +10,14 @@ def writeOdeFile(fileName, derivatives, derivativesJacobian):
 	h.write("def derivativesJacobian(y, t):\n")
 	h.write("\treturn np.array(" + str(derivativesJacobian)[7:-1] + ")\n")
 	h.close()
+
+def writeOdeFileWithRates(fileName, derivativesWithRatesAsVariables, derivativesJacobianWithRatesAsVariables):
+	h = open(fileName, "w")
+	h.write("import numpy as np\n")
+	h.write("\n")
+	h.write("def derivatives(y, t, kf, kr):\n")
+	h.write("\treturn np.array(" + str(derivativesWithRatesAsVariables)[7:-1] + ").reshape(-1)\n")
+	h.write("\n")
+	h.write("def derivativesJacobian(y, t, kf, kr):\n")
+	h.write("\treturn np.array(" + str(derivativesJacobianWithRatesAsVariables)[7:-1] + ")\n")
+	h.close()
