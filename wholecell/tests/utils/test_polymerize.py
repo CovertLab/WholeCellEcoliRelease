cb4df4544e54e27079ec50e24bc161136d46e443
Jerry Morrison <1fish2@users.noreply.github.com>
2016-10-19 02:13:19 -0700

    Copy faster-polymerize to a fresh fork of master.
    * utils/_fastsums.pyx implements the sum_monomers() inner loop in Cython
    for polymerize using typed Memoryviews. It turns out to be a bit tricky to
    ensure Cython code actually runs faster than Python code -- sensitive to
    details like testing that an index is out of range using
    `if i < 0 or i >= max` instead of `if i not in xrange(max)`.
    * Split out the profiling code from polymerize.py to
    tests/utils/profile_polymerize.py where it can tweak the sys.path as a way
    to workaround a Python import problem.
    * Add unit tests for the added Cython code.
    * In polymerize.py, define a no-op version of @profile when needed, so we
    don't have to modify the source code to enable line profiling.
    * Change Makefile to find the pyenv virtualenv-prefix rather than
    hard-coding it.
    * Use numpy.testing.assert_equal() to make test_polymerize.py simpler and
    more informative.

--- wholecell/tests/utils/test_polymerize.py
+++ wholecell/tests/utils/test_polymerize.py
@@ -1,23 +1,28 @@
-#!/usr/bin/env python
-
 """
-Test polymerize_new.py
+Test polymerize.py
+
+	cd wcEcoli
+	nosetests wholecell/tests/utils/test_polymerize.py
 
 @author: Nick Ruggero
 @organization: Covert Lab, Department of Chemical Engineering, Stanford University
 @date: Created 1/22/2013
 """
-from wholecell.utils.polymerize import  buildSequences, polymerize, computeMassIncrease, PAD_VALUE
+
+from wholecell.utils.polymerize import (buildSequences, polymerize,
+	computeMassIncrease, PAD_VALUE, sum_monomers,
+	sum_monomers_reference_implementation)
 
 import numpy as np
+from numpy.testing import assert_equal
 
 import nose.plugins.attrib as noseAttrib
 import nose.tools as noseTools
 import unittest
 
+
 class Test_polymerize(unittest.TestCase):
 	
-
 	@classmethod
 	def setUpClass(cls):
 		pass
@@ -31,6 +36,48 @@ class Test_polymerize(unittest.TestCase):
 
 	def tearDown(self):
 		pass
+
+	def makeSequenceMonomers(self, sequences):
+		"""
+		Set self.sequences, self.nSequences, self.sequenceLength, self.nMonomers,
+		self.sequenceMonomers, self.activeSequences from sequences.
+		"""
+		self.sequences = sequences
+		self.nSequences, self.sequenceLength = sequences.shape
+		self.nMonomers = np.amax(sequences) + 1 # [0 .. max(monomer #)]
+		self.sequenceMonomers = np.empty((self.nMonomers, self.nSequences, self.sequenceLength), dtype=np.bool)
+		for monomerIndex in xrange(self.nMonomers):
+			self.sequenceMonomers[monomerIndex, ...] = (sequences == monomerIndex)
+		self.activeSequences = np.array(xrange(self.nSequences))
+
+	@noseAttrib.attr('polymerizeNew')
+	@noseAttrib.attr('smalltest')
+	def test_sum_monomers(self):
+		sequences = np.array([[0,0,0,0,PAD_VALUE], [3,3,3,3,PAD_VALUE], [0,1,3,0,0]])
+		self.makeSequenceMonomers(sequences)
+		expected = np.array([[2,3,4,6,7], [0,1,1,1,1], [0,0,0,0,0], [1,2,4,5,5]])
+		tot = sum_monomers(self.sequenceMonomers, self.activeSequences, 0)
+		assert_equal(tot, expected)
+
+		tot2 = sum_monomers_reference_implementation(self.sequenceMonomers,
+			self.activeSequences, 0)
+		assert_equal(tot2, expected)
+
+	@noseAttrib.attr('polymerizeNew')
+	@noseAttrib.attr('smalltest')
+	def test_partial_sum_monomers(self):
+		sequences = np.array([[0,0, 0,0,PAD_VALUE], [3,3, 3,3,PAD_VALUE],
+			[0,0, 0,1,0], [2,2, 2,2,2]])
+		self.makeSequenceMonomers(sequences)
+		self.activeSequences = np.array([0,2]) # 2/4 sequences active
+		currentStep = 2
+		expected = np.array([[2,3,4], [0,1,1], [0,0,0], [0,0,0]])
+		tot = sum_monomers(self.sequenceMonomers, self.activeSequences, currentStep)
+		assert_equal(tot, expected)
+
+		tot2 = sum_monomers_reference_implementation(self.sequenceMonomers,
+			self.activeSequences, currentStep)
+		assert_equal(tot2, expected)
 
 	@noseAttrib.attr('polymerizeNew')
 	@noseAttrib.attr('smalltest')
@@ -54,8 +101,8 @@ class Test_polymerize(unittest.TestCase):
 		progress, baseCosts, energyCost = polymerize(sequences, baseAmounts, energy, np.random.RandomState(0))
 		np.random.seed()
 		
-		self.assertTrue((np.array([3, 3]) == progress).all())
-		self.assertTrue((np.array([2, 4]) == baseCosts).all())
+		assert_equal(progress, np.array([3, 3]))
+		assert_equal(baseCosts, np.array([2, 4]))
 		self.assertEqual(6, energyCost)
 	
 	
@@ -73,8 +120,8 @@ class Test_polymerize(unittest.TestCase):
 		progress, baseCosts, energyCost = polymerize(sequences, baseAmounts, energy, np.random.RandomState(0))
 		np.random.seed()
 		
-		self.assertTrue((np.array([10,9,9,0]) == progress).all())
-		self.assertTrue((np.array([3,3,11,11]) == baseCosts).all())
+		assert_equal(progress, np.array([10,9,9,0]))
+		assert_equal(baseCosts, np.array([3,3,11,11]))
 		self.assertEqual(28, energyCost)
 	
 	
@@ -92,8 +139,8 @@ class Test_polymerize(unittest.TestCase):
 		progress, baseCosts, energyCost = polymerize(sequences, baseAmounts, energy, np.random.RandomState(0))
 		np.random.seed()
 		
-		self.assertTrue((np.array([12,9,9,0]) == progress).all())
-		self.assertTrue((np.array([3,5,11,11]) == baseCosts).all())
+		assert_equal(progress, np.array([12,9,9,0]))
+		assert_equal(baseCosts, np.array([3,5,11,11]))
 		self.assertEqual(30, energyCost)
 				
 	
@@ -111,7 +158,7 @@ class Test_polymerize(unittest.TestCase):
 		progress, baseCosts, energyCost = polymerize(sequences, baseAmounts, energy, np.random.RandomState(0))
 		np.random.seed()
 		
-		self.assertTrue((np.array([12,12,12,3]) == progress).all())
+		assert_equal(progress, np.array([12,12,12,3]))
 
 
 	@noseAttrib.attr('polymerizeNew')
@@ -141,9 +188,7 @@ class Test_polymerize(unittest.TestCase):
 		comparison_sequence[1,:] = allSequences[5,4:4+elngRate]
 		comparison_sequence[2,:] = allSequences[8,9:9+elngRate]
 
-		self.assertTrue(np.all(
-			sequences == comparison_sequence
-			))
+		assert_equal(sequences, comparison_sequence)
 
 		# Un-padded case should throw exception
 		allSequences = np.random.randint(3, size=(20,10)).astype(np.int8, copy=False)
@@ -156,7 +201,6 @@ class Test_polymerize(unittest.TestCase):
 			Exception,
 			buildSequences, allSequences, sequenceIndexes, polymerizedLengths, elngRate
 			)
-
 
 	@noseAttrib.attr('polymerizeNew')
 	@noseAttrib.attr('smalltest')
@@ -175,4 +219,4 @@ class Test_polymerize(unittest.TestCase):
 		for i in range(sequences.shape[0]):
 			comparison_massIncrease[i] = np.sum(monomerWeights[sequences[i][:sequenceElongations[i]]])
 
-		self.assertTrue(np.all(massIncrease == comparison_massIncrease))
+		assert_equal(massIncrease, comparison_massIncrease)
