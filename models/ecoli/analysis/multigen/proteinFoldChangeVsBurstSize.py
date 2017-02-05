f107ae8b29ce970bb39b4852dd182296fab52b18
nruggero <nruggero@server>
2017-02-04 20:11:55 -0800

    Fixed error

--- models/ecoli/analysis/multigen/proteinFoldChangeVsBurstSize.py
+++ models/ecoli/analysis/multigen/proteinFoldChangeVsBurstSize.py
@@ -16,7 +16,7 @@ import cPickle
 
 from wholecell.containers.bulk_objects_container import BulkObjectsContainer
 from wholecell.utils import units
-FROM_CACHE = True
+FROM_CACHE = False
 
 CLOSE_TO_DOUBLE = 0.1
 FONT_SIZE = 9
