b08bc0f9347b31001476bdaf19c44decd6c451d6
nruggero <nruggero@server>
2017-02-06 14:54:06 -0800

    Moved over replication plot for cohort analysis

--- models/ecoli/analysis/cohort/replication.py
+++ models/ecoli/analysis/cohort/replication.py
@@ -18,7 +18,7 @@ PLACE_HOLDER = -1
 
 CRITICAL_N = [1, 2, 4, 8]
 
-def main(variantDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile = None, metadata = None):
+def main(variantDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile, metadata = None):
 	if not os.path.isdir(variantDir):
 		raise Exception, "variantDir does not currently exist as a directory"
 
@@ -38,80 +38,75 @@ def main(variantDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 	fig, axesList = plt.subplots(6, sharex = True)
 	fig.set_size_inches(11, 11)
 	for simDir in allDir:
-		try:
-			simOutDir = os.path.join(simDir, "simOut")
-			time = TableReader(os.path.join(simOutDir, "Main")).readColumn("time") / 60. / 60.
-			mass = TableReader(os.path.join(simOutDir, "Mass"))
+		simOutDir = os.path.join(simDir, "simOut")
+		time = TableReader(os.path.join(simOutDir, "Main")).readColumn("time") / 60. / 60.
+		mass = TableReader(os.path.join(simOutDir, "Mass"))
 
-			division_time = time.max()
+		division_time = time.max()
 
-			# Plot dna polymerase position
-			sequenceIdx = TableReader(os.path.join(simOutDir, "ReplicationData")).readColumn("sequenceIdx")
-			# sequenceLength = TableReader(os.path.join(simOutDir, "ReplicationData")).readColumn("sequenceLength")
-			# reverseIdx = 1
-			# reverseCompIdx = 3
-			# reverseSequences = np.logical_or(sequenceIdx == reverseIdx, sequenceIdx == reverseCompIdx)
-			# sequenceLength[reverseSequences] = -1 * sequenceLength[reverseSequences]
-			# sequenceLength[sequenceLength == PLACE_HOLDER] = np.nan
+		# Plot dna polymerase position
+		sequenceIdx = TableReader(os.path.join(simOutDir, "ReplicationData")).readColumn("sequenceIdx")
+		sequenceLength = TableReader(os.path.join(simOutDir, "ReplicationData")).readColumn("sequenceLength")
+		reverseIdx = 1
+		reverseCompIdx = 3
+		reverseSequences = np.logical_or(sequenceIdx == reverseIdx, sequenceIdx == reverseCompIdx)
+		sequenceLength[reverseSequences] = -1 * sequenceLength[reverseSequences]
+		sequenceLength[sequenceLength == PLACE_HOLDER] = np.nan
 
-			# # Down sample dna polymerase position, every position is only plotted once here
-			# # using numpy ninja-ness
-			# unique, index, value = np.unique(sequenceLength, return_index=True, return_inverse=True)
-			# m = np.zeros_like(value, dtype=bool)
-			# m[index] = True
-			# m = m.reshape(sequenceLength.shape)
-			# sequenceLength[~m] = np.nan
+		# Down sample dna polymerase position, every position is only plotted once here
+		# using numpy ninja-ness
+		unique, index, value = np.unique(sequenceLength, return_index=True, return_inverse=True)
+		m = np.zeros_like(value, dtype=bool)
+		m[index] = True
+		m = m.reshape(sequenceLength.shape)
+		sequenceLength[~m] = np.nan
 
-			# downsample = 10
-			# axesList[0].plot(time[::downsample], sequenceLength[::downsample,::downsample], marker=',', markersize=1, linewidth=0)
-			# axesList[0].set_xlim([0, time.max()])
-			# axesList[0].set_yticks([-1 * genomeLength / 2, 0, genomeLength / 2])
-			# axesList[0].set_yticklabels(['-terC', 'oriC', '+terC'])
-			# axesList[0].set_ylabel("DNA polymerase\nposition (nt)")
+		downsample = 10
+		axesList[0].plot(time[::downsample], sequenceLength[::downsample,::downsample], marker=',', markersize=1, linewidth=0)
+		axesList[0].set_xlim([0, time.max()])
+		axesList[0].set_yticks([-1 * genomeLength / 2, 0, genomeLength / 2])
+		axesList[0].set_yticklabels(['-terC', 'oriC', '+terC'])
+		axesList[0].set_ylabel("DNA polymerase\nposition (nt)")
 
-			# Plot dna polymerase counts
-			pairsOfForks = (sequenceIdx != PLACE_HOLDER).sum(axis = 1) / 4
+		# Plot dna polymerase counts
+		pairsOfForks = (sequenceIdx != PLACE_HOLDER).sum(axis = 1) / 4
 
-			axesList[1].plot(time, pairsOfForks, linewidth=2)
-			axesList[1].set_yticks(np.arange(0,7))
-			axesList[1].set_ylim([0, 6])
-			axesList[1].set_xlim([0, time.max()])
-			axesList[1].plot([time.max(), time.max()], axesList[1].get_ylim(), 'k')
-			axesList[1].set_ylabel("Pairs of\nforks")
-			
+		axesList[1].plot(time, pairsOfForks, linewidth=2)
+		axesList[1].set_yticks(np.arange(0,7))
+		axesList[1].set_ylim([0, 6])
+		axesList[1].set_xlim([0, time.max()])
+		axesList[1].plot([time.max(), time.max()], axesList[1].get_ylim(), 'k')
+		axesList[1].set_ylabel("Pairs of\nforks")
 
-			# Factors of critical initiation mass
-			totalMass = mass.readColumn("cellMass")
-			criticalInitiationMass = TableReader(os.path.join(simOutDir, "ReplicationData")).readColumn("criticalInitiationMass")
-			criticalMassEquivalents = totalMass / criticalInitiationMass
+		# Factors of critical initiation mass
+		totalMass = mass.readColumn("cellMass")
+		criticalInitiationMass = TableReader(os.path.join(simOutDir, "ReplicationData")).readColumn("criticalInitiationMass")
+		criticalMassEquivalents = totalMass / criticalInitiationMass
 
-			axesList[2].plot(time, criticalInitiationMass, linewidth=2)
-			for N in CRITICAL_N:
-				axesList[2].plot([0, time.max()], [N]*2, 'k', linestyle='--')
-			axesList[2].set_ylabel("Factors of critical\ninitiation mass")
-			axesList[2].set_xlim([0, time.max()])
+		axesList[2].plot(time, criticalInitiationMass, linewidth=2)
+		for N in CRITICAL_N:
+			axesList[2].plot([0, time.max()], [N]*2, 'k', linestyle='--')
+		axesList[2].set_ylabel("Factors of critical\ninitiation mass")
+		axesList[2].set_xlim([0, time.max()])
 
-			# Dry mass
-			dryMass = mass.readColumn("dryMass")
-			axesList[3].plot(time, dryMass, linewidth = 2)
-			axesList[3].set_ylabel("Dry mass (fg)")
-			axesList[3].set_xlim([0, time.max()])
-			axesList[3].plot([0, time.max()],[criticalInitiationMass, criticalInitiationMass])
-			mass.close()
+		# Dry mass
+		dryMass = mass.readColumn("dryMass")
+		axesList[3].plot(time, dryMass, linewidth = 2)
+		axesList[3].set_ylabel("Dry mass (fg)")
+		axesList[3].set_xlim([0, time.max()])
+		mass.close()
 
-			# Number of oriC
-			numberOfOric = TableReader(os.path.join(simOutDir, "ReplicationData")).readColumn("numberOfOric")
-			axesList[4].plot(time, numberOfOric, linewidth=2)
-			axesList[4].set_ylabel("Number of\noriC")
-			axesList[4].set_xlim([0, time.max()])
+		# Number of oriC
+		numberOfOric = TableReader(os.path.join(simOutDir, "ReplicationData")).readColumn("numberOfOric")
+		axesList[4].plot(time, numberOfOric, linewidth=2)
+		axesList[4].set_ylabel("Number of\noriC")
+		axesList[4].set_xlim([0, time.max()])
 
-			# Mass per oriC
-			criticalMassPerOriC = TableReader(os.path.join(simOutDir, "ReplicationData")).readColumn("criticalMassPerOriC")
-			axesList[5].plot(time, criticalMassPerOriC, linewidth=2)
-			axesList[5].set_ylabel("Critical mass\nper oriC")
-			axesList[5].set_xlim([0, time.max()])
-		except:
-			pass
+		# Mass per oriC
+		criticalMassPerOriC = TableReader(os.path.join(simOutDir, "ReplicationData")).readColumn("criticalMassPerOriC")
+		axesList[5].plot(time, criticalMassPerOriC, linewidth=2)
+		axesList[5].set_ylabel("Critical mass\nper oriC")
+		axesList[5].set_xlim([0, time.max()])
 
 	axesList[0].set_title("Replication plots")
 	axesList[-1].set_xlabel("Time (hr)")
