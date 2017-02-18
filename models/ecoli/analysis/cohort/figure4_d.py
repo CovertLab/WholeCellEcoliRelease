a5e2054b84d9c5e0db5e750db52de3b6ab1c0a03
nruggero <nruggero@server>
2017-02-17 16:31:37 -0800

    Changed ylim

--- models/ecoli/analysis/cohort/figure4_d.py
+++ models/ecoli/analysis/cohort/figure4_d.py
@@ -123,41 +123,12 @@ def main(seedOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 	axis.plot(timeMultigen[:-width] / 60., proteinGrowthRateMultigen[:-width] * 60., color = colors[1], alpha=0.9, label="Protein fraction", linewidth=linewidth)
 	axis.plot(timeMultigen[:-width] / 60., rnaGrowthRateMultigen[:-width] * 60., color = colors[2], alpha=0.9, label="RNA fraction", linewidth=linewidth)
 	axis.legend(loc=4,frameon=False)
-	# axis.set_ylim([0.00020, 0.00035])
+	axis.set_ylim([0.015, 0.033])
 
 	whitePadSparklineAxis(axis)
 	axis.set_xlabel("Time (min)")
 	axis.set_ylabel("Averaged instantanious growth rate " + r"$(\frac{\frac{dX}{dt}}{X} [=] \frac{1}{min})$")
 	plt.subplots_adjust(bottom = 0.2, left = 0.2)
-		# axis.plot(time[:-1] / 60., rnaGrowthRate, color = colors[1], alpha=0.5)
-		# axis.plot(time[:-1] / 60., dnaGrowthRate, color = colors[2], alpha=0.5)
-
-		# for idx, massType in enumerate(massNames):
-		# 	massToPlot = mass.readColumn(massNames[idx])
-		# 	dMassToPlot = ((2. ** gen) * massToPlot[1:] - (2. ** gen) * massToPlot[:-1])
-		# 	axis.plot(time[1 + NUM_SKIP_TIMESTEPS_AT_GEN_CHANGE:-NUM_SKIP_TIMESTEPS_AT_GEN_CHANGE] / 60. / 60., dMassToPlot[NUM_SKIP_TIMESTEPS_AT_GEN_CHANGE:-NUM_SKIP_TIMESTEPS_AT_GEN_CHANGE], color = colors[idx])
-
-		# 	# Todo: plot exponential fit to intial condition
-
-	# ax1.legend(cleanNames, loc = "best", fontsize = 10)
-	# ax1.set_ylabel("Mass (fg)")
-	# ax1.set_xlabel("Time (hour)")
-	# if T_ADD_AA != None and T_CUT_AA !=None:
-	# 	ax1.plot((T_ADD_AA / 60. / 60., T_ADD_AA / 60. / 60.), ax1.get_ylim(), "k--")
-	# 	ax1.plot((T_CUT_AA / 60. / 60., T_CUT_AA / 60. / 60.), ax1.get_ylim(), "k--")
-
-	# ax2.set_title("Instantaneous change in mass (fg / second)")
-	# ax2.set_xlim((-0.1, ax2.get_xlim()[1]))
-	# ax5.set_xlabel("Time (hour)")
-	# for idx, ax in enumerate(axesList):
-	# 	ax.set_ylabel(cleanNames[idx])
-	# 	ax.set_xlim(0, time[-1] / 60. / 60.)
-	# 	if T_ADD_AA != None and T_CUT_AA !=None:
-	# 		ax.plot((T_ADD_AA / 60. / 60., T_ADD_AA / 60. / 60.), ax.get_ylim(), "k--")
-	# 		ax.plot((T_CUT_AA / 60. / 60., T_CUT_AA / 60. / 60.), ax.get_ylim(), "k--")
-
-	# plt.subplots_adjust(hspace = 0.2, wspace = 0.2)
-
 	from wholecell.analysis.analysis_tools import exportFigure
 	exportFigure(plt, plotOutDir, plotOutFileName,metadata)
 
