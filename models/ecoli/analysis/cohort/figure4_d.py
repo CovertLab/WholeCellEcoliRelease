e84a58b809d5ddf1bb4978a88026ff223f1a2e88
nruggero <nruggero@server>
2017-02-19 08:34:39 -0800

    Added alternate size

--- models/ecoli/analysis/cohort/figure4_d.py
+++ models/ecoli/analysis/cohort/figure4_d.py
@@ -23,7 +23,7 @@ NUM_SKIP_TIMESTEPS_AT_GEN_CHANGE = 1
 from wholecell.utils.sparkline import whitePadSparklineAxis
 
 trim = 0.03
-
+trim_1 = 0.06
 def mm2inch(value):
 	return value * 0.0393701
 
@@ -156,6 +156,95 @@ def main(seedOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 	exportFigure(plt, plotOutDir, plotOutFileName + "_stripped" ,metadata, transparent = True)
 	plt.close("all")
 
+	################ ALTERNATE SIZE ######################
+	mult = 3
+
+	fig, axis = plt.subplots(1,1)
+	fig.set_figwidth(mm2inch(29.25)*mult)
+	fig.set_figheight(mm2inch(24.401)*mult)
+
+	width = 200
+
+	timeMultigen = np.zeros(0)
+	cellMassGrowthRateMultigen = np.zeros(0)
+	proteinGrowthRateMultigen = np.zeros(0)
+	rnaGrowthRateMultigen = np.zeros(0)
+
+
+	for gen, simDir in enumerate(allDir):
+		simOutDir = os.path.join(simDir, "simOut")
+		time = TableReader(os.path.join(simOutDir, "Main")).readColumn("time")
+		timeStep = TableReader(os.path.join(simOutDir, "Main")).readColumn("timeStepSec")
+		mass = TableReader(os.path.join(simOutDir, "Mass"))
+
+		cellMass = mass.readColumn("cellMass")
+		proteinMass = mass.readColumn("proteinMass")
+		rnaMass = mass.readColumn("rnaMass")
+		dnaMass = mass.readColumn("dnaMass")
+
+		cellmassGrowthRate = np.diff(cellMass) / cellMass[1:] / timeStep[:-1]
+		proteinGrowthRate = np.diff(proteinMass) / proteinMass[1:] / timeStep[:-1]
+		rnaGrowthRate = np.diff(rnaMass) / rnaMass[1:] / timeStep[:-1]
+		dnaGrowthRate = np.diff(dnaMass) / dnaMass[1:] / timeStep[:-1]
+
+		timeMultigen = np.hstack((timeMultigen, time[:-1]))
+		cellMassGrowthRateMultigen = np.hstack((cellMassGrowthRateMultigen, cellmassGrowthRate))
+		proteinGrowthRateMultigen = np.hstack((proteinGrowthRateMultigen, proteinGrowthRate))
+		rnaGrowthRateMultigen = np.hstack((rnaGrowthRateMultigen, rnaGrowthRate))
+
+	# seriesScrubber(cellMassGrowthRateMultigen, 1.5)
+	# seriesScrubber(proteinGrowthRateMultigen, 1.5)
+	# seriesScrubber(rnaGrowthRateMultigen, 1.5)
+
+
+	cellMassGrowthRateMultigen = np.convolve(cellMassGrowthRateMultigen, np.ones(width) / width, mode = "same")
+	proteinGrowthRateMultigen = np.convolve(proteinGrowthRateMultigen, np.ones(width) / width, mode = "same")
+	rnaGrowthRateMultigen = np.convolve(rnaGrowthRateMultigen, np.ones(width) / width, mode = "same")
+	# seriesScrubber(proteinGrowthRateMultigen, 2)
+
+		# rnaGrowthRate = np.convolve(rnaGrowthRate, np.ones(width) / width, mode = "same")
+		# dnaGrowthRate = np.convolve(dnaGrowthRate, np.ones(width) / width, mode = "same")
+
+	# 	seriesScrubber(rnaGrowthRate,1.25)
+		# seriesScrubber(dnaGrowthRate,3.25)
+
+	linewidth = 2
+	axis.plot(timeMultigen[:-width] / 60., cellMassGrowthRateMultigen[:-width] * 60., color = colors[0], alpha=0.9, label="Cell mass", linewidth=linewidth)
+	axis.plot(timeMultigen[:-width] / 60., proteinGrowthRateMultigen[:-width] * 60., color = colors[1], alpha=0.9, label="Protein fraction", linewidth=linewidth)
+	axis.plot(timeMultigen[:-width] / 60., rnaGrowthRateMultigen[:-width] * 60., color = colors[2], alpha=0.9, label="RNA fraction", linewidth=linewidth)
+	# axis.legend(loc=4,frameon=False)
+	axis.set_ylim([0.015, 0.033])
+
+	whitePadSparklineAxis(axis)
+	axis.set_xlabel("Time (min)")
+	axis.set_ylabel("Averaged instantaneous growth rate " + r"$(\frac{\frac{dX}{dt}}{X} [=] \frac{1}{min})$")
+	plt.subplots_adjust(bottom = 0.2, left = 0.2)
+	from wholecell.analysis.analysis_tools import exportFigure
+	exportFigure(plt, plotOutDir, plotOutFileName + "_altsize",metadata)
+
+
+	for axes in [axis]:
+		axes.tick_params(
+			axis='x',          # changes apply to the x-axis
+			which='both',      # both major and minor ticks are affected
+			bottom='off',      # ticks along the bottom edge are off
+			top='off',         # ticks along the top edge are off
+			labelbottom='off') # labels along the bottom edge are off
+		axes.tick_params(
+			axis='y',          # changes apply to the x-axis
+			which='both',      # both major and minor ticks are affected
+			left='off',      # ticks along the bottom edge are off
+			right='off',         # ticks along the top edge are off
+			labelleft='off') # labels along the bottom edge are off
+
+		axes.set_xlabel("")
+		axes.set_ylabel("")
+
+	plt.subplots_adjust(top = 1, bottom = trim_1, left = trim_1, right = 1)
+
+	from wholecell.analysis.analysis_tools import exportFigure
+	exportFigure(plt, plotOutDir, plotOutFileName + "_altsize_stripped" ,metadata, transparent = True)
+	plt.close("all")
 
 if __name__ == "__main__":
 	defaultSimDataFile = os.path.join(
