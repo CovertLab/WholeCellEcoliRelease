cda96cff7373e9ed80c6ca3a31f542a88b432afb
heejochoi <heejochoi@gmail.com>
2017-03-07 17:05:38 -0800

    Several updates: fix indexing bug for 1 generation, option to indicate specific genes of interest, create clean versions (no labels).

--- models/ecoli/analysis/multigen/figure5B.py
+++ models/ecoli/analysis/multigen/figure5B.py
@@ -22,6 +22,7 @@ from wholecell.utils import units
 from wholecell.utils.sparkline import whitePadSparklineAxis
 
 BUILD_CACHE = True
+PLOT_GENES_OF_INTEREST = False
 
 def main(seedOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile, metadata = None):
 	if not os.path.isdir(seedOutDir):
@@ -32,6 +33,9 @@ def main(seedOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 
 	# Get all cells
 	ap = AnalysisPaths(seedOutDir, multi_gen_plot = True)
+	if 0 not in ap._path_data["seed"]:
+		print "Skipping -- figure5B only runs for seed 0"
+		return
 	allDir = ap.get_cells(seed = [0])
 
 	# Get mRNA data
@@ -43,6 +47,7 @@ def main(seedOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 
 	mRnaSynthProb = np.array([synthProb[x] for x in mRnaIndexes])
 	mRnaIds = np.array([rnaIds[x] for x in mRnaIndexes])
+
 
 	if BUILD_CACHE:
 		time = []
@@ -87,6 +92,14 @@ def main(seedOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 		simulatedSynthProbsOrdered = np.mean(simulatedSynthProbs, axis = 0)[indexingOrder]
 		transcriptionEventsOrdered = transcriptionEvents[:, indexingOrder]
 		mRnaIdsOrdered = mRnaIds[indexingOrder]
+		transcriptionIndex = sim_data.process.transcription.rnaData["id"].tolist()
+		geneIdsOrdered = [sim_data.process.transcription.rnaData["geneId"][transcriptionIndex.index(x)] for x in mRnaIdsOrdered]
+
+		raw_data = cPickle.load(open("out/SET_A_000000/rawData.cPickle", "rb"))
+		geneIdToGeneSymbol = dict([(x["id"].encode("utf-8"), x["symbol"].encode("utf-8")) for x in raw_data.genes])
+		geneSymbolsOrdered = [geneIdToGeneSymbol[x] for x in geneIdsOrdered]
+		cPickle.dump({"geneId": geneIdsOrdered, "geneSymbol": geneSymbolsOrdered}, open(os.path.join(plotOutDir, "figure5B_genes.pickle"), "wb"))
+
 
 		alwaysPresentIndexes = np.where(transcribedBoolOrdered == 1.)[0]
 		neverPresentIndexes = np.where(transcribedBoolOrdered == 0.)[0]
@@ -95,8 +108,14 @@ def main(seedOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 		colors[alwaysPresentIndexes] = "b"
 		colors[neverPresentIndexes] = "r"
 		always = transcribedBoolOrdered[alwaysPresentIndexes]
-		sometimes = transcribedBoolOrdered[sometimesPresentIndexes]
 		never = transcribedBoolOrdered[neverPresentIndexes]
+		
+		if len(allDir) == 1:
+			sometimes = np.array([])
+		else:
+			sometimes = transcribedBoolOrdered[sometimesPresentIndexes]
+		
+
 
 		alwaysTranscriptionEvents = []
 		for i in alwaysPresentIndexes:
@@ -132,6 +151,7 @@ def main(seedOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 			"neverTranscriptionEvents": neverTranscriptionEvents,
 			}, open(os.path.join(plotOutDir, "figure5B.pickle"), "wb"))
 
+
 	else:
 		D = cPickle.load(open(os.path.join(plotOutDir, "figure5B.pickle"), "r"))
 		time = D["time"]
@@ -145,9 +165,9 @@ def main(seedOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 		sometimesTranscriptionEvents = D["sometimesTranscriptionEvents"]
 		neverTranscriptionEvents = D["neverTranscriptionEvents"]
 
+
 	# Plot
 	fig = plt.figure(figsize = (16, 8))
-	plt.suptitle("Frequency of observing at least 1 transcript per generation", fontsize = 12)
 	scatterAxis = plt.subplot2grid((2, 4), (0, 0), colspan = 3, rowspan = 2)
 	histAxis = plt.subplot2grid((2, 4), (0, 3), colspan = 1, rowspan = 2, sharey = scatterAxis)
 
@@ -155,9 +175,8 @@ def main(seedOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 	scatterAxis.set_xlim([0, len(transcribedBoolOrdered)])
 	scatterAxis.set_ylim([0, 1])
 	whitePadSparklineAxis(scatterAxis)
-	scatterAxis.text(0.5 * scatterAxis.get_xlim()[1], -0.1, "Genes ordered by simulated synthesis probability", horizontalalignment = "center")
 
-	N, bins, patches = histAxis.hist(transcribedBoolOrdered, bins = len(allDir), orientation = 'horizontal')
+	N, bins, patches = histAxis.hist(transcribedBoolOrdered, bins = len(allDir) + 1, orientation = 'horizontal')
 	for i in xrange(1, len(patches) - 1):
 		plt.setp(patches[i], facecolor = "none", edgecolor = "g")
 	plt.setp(patches[0], facecolor = "none", edgecolor = "r")
@@ -165,26 +184,53 @@ def main(seedOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 	histAxis.set_xscale("log")
 	whitePadSparklineAxis(histAxis)
 	histAxis.xaxis.tick_bottom()
+	histXmin, histXmax = histAxis.get_xlim()
 
 	scatterAxis.set_ylim([-.01, 1.01])
-
-	histAxis.text(histAxis.get_xlim()[1] * 1.7, 0, "%s genes\n(%0.1f%%)" % (len(never), 100. * (len(never) / float(len(transcribedBoolOrdered)))), fontsize = 12, verticalalignment = "center", color = "r")
-	histAxis.text(histAxis.get_xlim()[1] * 1.7, 1, "%s genes\n(%0.1f%%)" % (len(always), 100. * (len(always) / float(len(transcribedBoolOrdered)))), fontsize = 12, verticalalignment = "center", color = "b")
-	histAxis.text(histAxis.get_xlim()[1] * 1.7, 0.5, "%s genes\n(%0.1f%%)" % (len(sometimes), 100. * (len(sometimes) / float(len(transcribedBoolOrdered)))), fontsize = 12, verticalalignment = "center", color = "g")
+	scatterAxis.set_yticklabels([])
+	scatterAxis.set_xticklabels([])
+	histAxis.set_yticklabels([])
+	histAxis.set_xticklabels([])
 
 	plt.subplots_adjust(wspace = 0.6, hspace = 0.4, right = 0.9, bottom = 0.1, left = 0.1, top = 0.9)
 	from wholecell.analysis.analysis_tools import exportFigure
+	exportFigure(plt, plotOutDir, plotOutFileName + "__top__clean", metadata)
+
+	if PLOT_GENES_OF_INTEREST:
+		## Identifying particular genes
+		dcurId = "G7826_RNA[c]"
+		clppId = "EG10158_RNA[c]"
+		dcucId = "G6347_RNA[c]"
+		dcurIndex = np.where(mRnaIdsOrdered == dcurId)[0]
+		clppIndex = np.where(mRnaIdsOrdered == clppId)[0]
+		dcucIndex = np.where(mRnaIdsOrdered == dcucId)[0]
+
+		scatterAxis.scatter(dcurIndex, transcribedBoolOrdered[dcurIndex], facecolor = "orange", edgecolors = "none")
+		scatterAxis.scatter(clppIndex, transcribedBoolOrdered[clppIndex], facecolor = "orange", edgecolors = "none")
+		scatterAxis.scatter(dcucIndex, transcribedBoolOrdered[dcucIndex], facecolor = "orange", edgecolors = "none")
+		scatterAxis.text(dcurIndex + 500, transcribedBoolOrdered[dcurIndex], "dcuR", fontsize = 18)
+		scatterAxis.text(clppIndex + 500, transcribedBoolOrdered[clppIndex], "clpP", fontsize = 18)
+		scatterAxis.text(dcucIndex + 500, transcribedBoolOrdered[dcucIndex], "dcuC", fontsize = 18)
+		exportFigure(plt, plotOutDir, plotOutFileName + "__top__clean__genes", metadata)
+
+	plt.suptitle("Frequency of observing at least 1 transcript per generation", fontsize = 14)
+	scatterAxis.set_xlabel("Genes ordered by simulated synthesis probability", fontsize = 12)
+	histAxis.text(histAxis.get_xlim()[1] * 1.6, 0, "%s genes\n(%0.1f%%)" % (len(never), 100. * (len(never) / float(len(transcribedBoolOrdered)))), fontsize = 14, verticalalignment = "center", color = "r")
+	histAxis.text(histAxis.get_xlim()[1] * 1.6, 1, "%s genes\n(%0.1f%%)" % (len(always), 100. * (len(always) / float(len(transcribedBoolOrdered)))), fontsize = 14, verticalalignment = "center", color = "b")
+	histAxis.text(histAxis.get_xlim()[1] * 1.6, 0.5, "%s genes\n(%0.1f%%)" % (len(sometimes), 100. * (len(sometimes) / float(len(transcribedBoolOrdered)))), fontsize = 14, verticalalignment = "center", color = "g")
+	scatterAxis.set_yticklabels([0, 1])
+	scatterAxis.set_xticklabels([0, len(transcribedBoolOrdered)])
+	histAxis.set_yticklabels([0, 1])
+	histAxis.set_xticklabels([histXmin, histXmax])
 	exportFigure(plt, plotOutDir, plotOutFileName + "__top", metadata)
 	plt.close("all")
 
 
 	fig = plt.figure(figsize = (16, 8))
-	plt.suptitle("Transcription initiation events", fontsize = 12)
 	alwaysAxis = plt.subplot(2, 1, 1)
 	sometimesAxis = plt.subplot(2, 1, 2, sharex = alwaysAxis)
 
-	alwaysAxis.eventplot(alwaysTranscriptionEvents, orientation = "horizontal", linewidths = 2., linelengths = 1., colors = "b")
-	alwaysAxis.set_ylabel("Frequency == 1", fontsize = 12)
+	alwaysAxis.eventplot(alwaysTranscriptionEvents, orientation = "horizontal", linewidths = 2., linelengths = 4., color = "b")
 	alwaysAxis.set_xlim([0, time[-1] / 3600.])
 	alwaysAxis.set_ylim([-1, len(always)])
 	alwaysAxis.set_xticks([])
@@ -193,19 +239,25 @@ def main(seedOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 	alwaysAxis.tick_params(bottom = "off")
 	alwaysAxis.tick_params(axis = "x", labelbottom = 'off')
 	
-	sometimesAxis.eventplot(sometimesTranscriptionEvents, orientation = "horizontal", linewidths = 2., linelengths = 1., colors = "g")
-	sometimesAxis.set_ylabel("0 < Frequency < 1", fontsize = 12)
+	sometimesAxis.eventplot(sometimesTranscriptionEvents, orientation = "horizontal", linewidths = 2., linelengths = 4., color = "g")
 	sometimesAxis.set_xlim([0, time[-1] / 3600.])
 	sometimesAxis.set_ylim([-1, len(sometimes)])
 	sometimesAxis.set_xticks([0, time[-1] / 3600.])
 	sometimesAxis.set_yticks([])
 	sometimesAxis.tick_params(top = "off")
 	sometimesAxis.tick_params(which = 'both', direction = 'out', labelsize = 12)
-	sometimesAxis.set_xlabel("Time (hour)", fontsize = 12)
-	
+	sometimesXmin, sometimesXmax = sometimesAxis.get_xlim()
+	sometimesAxis.set_xticklabels([])
+
 	plt.subplots_adjust(wspace = 0, hspace = 0, right = 0.9, bottom = 0.1, left = 0.1, top = 0.9)
-	
 	from wholecell.analysis.analysis_tools import exportFigure
+	exportFigure(plt, plotOutDir, plotOutFileName + "__bottom__clean", metadata)
+
+	plt.suptitle("Transcription initiation events", fontsize = 14)
+	alwaysAxis.set_ylabel("Freq. = 1", fontsize = 14)
+	sometimesAxis.set_ylabel("0 < Freq. < 1", fontsize = 14)
+	sometimesAxis.set_xlabel("Time (hr)", fontsize = 14)
+	sometimesAxis.set_xticklabels([sometimesXmin, "%0.2f" % sometimesXmax])
 	exportFigure(plt, plotOutDir, plotOutFileName + "__bottom", metadata)
 	plt.close("all")
 
