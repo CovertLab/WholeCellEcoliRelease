a72be00942c5df3ae41758ee8d01077a0c19ec2c
heejochoi <heejochoi@gmail.com>
2017-03-21 15:25:15 -0700

    Add comments to two_component_system.py.

--- models/ecoli/processes/two_component_system.py
+++ models/ecoli/processes/two_component_system.py
@@ -5,11 +5,10 @@ Two component system
 
 Two component system sub-model
 
-@author: Derek Macklin
+@author: Heejo Choi
 @organization: Covert Lab, Department of Bioengineering, Stanford University
 @date: Created 5/3/2016
 
-@author: Heejo Choi
 """
 
 from __future__ import division
@@ -41,11 +40,11 @@ class TwoComponentSystem(wholecell.processes.process.Process):
 	def initialize(self, sim, sim_data):
 		super(TwoComponentSystem, self).initialize(sim, sim_data)
 
+		# Get constants
 		self.nAvogadro = sim_data.constants.nAvogadro.asNumber(1 / units.mmol)
 		self.cellDensity = sim_data.constants.cellDensity.asNumber(units.g / units.L)
 
 		# Create matrices and vectors
-
 		self.stoichMatrix = sim_data.process.two_component_system.stoichMatrix().astype(np.int64)
 		self.Rp = sim_data.process.two_component_system.Rp
 		self.Pp = sim_data.process.two_component_system.Pp
@@ -55,29 +54,40 @@ class TwoComponentSystem(wholecell.processes.process.Process):
 		self.moleculesToNextTimeStep = sim_data.process.two_component_system.moleculesToNextTimeStep
 
 		# Build views
-
 		self.moleculeNames = sim_data.process.two_component_system.moleculeNames
 		self.molecules = self.bulkMoleculesView(self.moleculeNames)
 
 		# Set priority to a lower value (but greater priority than metabolism)
 		self.bulkMoleculesRequestPriorityIs(REQUEST_PRIORITY_TWO_COMPONENT_SYSTEM)
 
+
 	def calculateRequest(self):
+		# Get molecule counts
 		moleculeCounts = self.molecules.total()
 
+		# Get cell mass and volume
 		cellMass = (self.readFromListener("Mass", "cellMass") * units.fg).asNumber(units.g)
 		self.cellVolume = cellMass / self.cellDensity
 
+		# Solve ODEs to next time step
 		self.req, self.allMoleculeChanges = self.moleculesToNextTimeStep(moleculeCounts, self.cellVolume, self.nAvogadro, self.timeStepSec())
 
+		# Request counts of molecules needed
 		self.molecules.requestIs(self.req)
 
 
 	def evolveState(self):
+		# Get counts of molecules allocated to this process
 		moleculeCounts = self.molecules.counts()
 
+		# Check if any molecules were allocated fewer counts than requested
 		if (self.req > moleculeCounts).any():
+
+			# Solve ODEs to next time step using the the counts of molecules allocated to this process
 			_, self.allMoleculeChanges = self.moleculesToNextTimeStep(moleculeCounts, self.cellVolume, self.nAvogadro, self.timeStepSec())
+
+			# Increment changes in molecule counts
 			self.molecules.countsInc(self.allMoleculeChanges)
 		else:
+			# Increment changes in molecule counts
 			self.molecules.countsInc(self.allMoleculeChanges)
