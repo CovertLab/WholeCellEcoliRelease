9043dd73b4a7761fc97c2f383a21ce692a862f41
nruggero <nruggero@server>
2017-03-22 10:46:31 -0700

    Added comments and cleand up code for polypeptide initalizeation

--- models/ecoli/processes/polypeptide_initiation.py
+++ models/ecoli/processes/polypeptide_initiation.py
@@ -5,13 +5,6 @@ PolypeptideInitiation
 
 Polypeptide initiation sub-model.
 
-TODO:
-- translate off of transcribed transcription units
-- co-transcriptional translation
-- retain mRNA while translating
-- match observed active ribosome fraction
-- use actual ribosomes (all isoforms, all subunits) instead of representative rRNAs
-
 @author: Derek Macklin
 @organization: Covert Lab, Department of Bioengineering, Stanford University
 @date: Created 4/30/14
@@ -32,53 +25,35 @@ class PolypeptideInitiation(wholecell.processes.process.Process):
 
 	_name = "PolypeptideInitiation"
 
-	# Constructor
 	def __init__(self):
-		# Parameters
-		self.proteinLens = None
-
-		# Views
-
-		self.activeRibosomes = None
-		self.ribosome30S = None
-		self.ribosome50S = None
-		self.mRnas = None
-
 		super(PolypeptideInitiation, self).__init__()
 
-
-	# Construct object graph
 	def initialize(self, sim, sim_data):
 		super(PolypeptideInitiation, self).initialize(sim, sim_data)
 
 		# Load parameters
-
 		mrnaIds = sim_data.process.translation.monomerData["rnaId"]
-
 		self.proteinLens = sim_data.process.translation.monomerData["length"].asNumber()
+		self.translationEfficiencies = normalize(sim_data.process.translation.translationEfficienciesByMonomer)
 
-		# Views
-
+		# Create view on to active 70S ribosomes
 		self.activeRibosomes = self.uniqueMoleculesView('activeRibosome')
 
+		# Create views onto bulk 30S and 50S ribosomal subunits
 		self.ribosome30S = self.bulkMoleculeView(sim_data.moleculeGroups.s30_fullComplex[0])
 		self.ribosome50S = self.bulkMoleculeView(sim_data.moleculeGroups.s50_fullComplex[0])
 
+		# Create view onto bulk mRNAs
 		self.mRnas = self.bulkMoleculesView(mrnaIds)
-		self.translationEfficiencies = normalize(sim_data.process.translation.translationEfficienciesByMonomer)
-
 
 	def calculateRequest(self):
 		self.ribosome30S.requestAll()
 		self.ribosome50S.requestAll()
-
 		self.mRnas.requestAll()
 
-	# Calculate temporal evolution
 	def evolveState(self):
-		# Sample a multinomial distribution of synthesis probabilities to 
-		# determine what molecules are initialized
-
+		# Calculate number of ribosomes that could potentially be initalized based on
+		# counts of free 30S and 50S subunits
 		inactiveRibosomeCount = np.min([
 			self.ribosome30S.count().sum(),
 			self.ribosome50S.count().sum(),
@@ -87,23 +62,26 @@ class PolypeptideInitiation(wholecell.processes.process.Process):
 		if inactiveRibosomeCount == 0:
 			return
 
+		# Calculate initiation probabilities for ribosomes based on mRNA counts and associated
+		# mRNA translational efficiencies
 		proteinInitProb = normalize(
 			self.mRnas.counts() * self.translationEfficiencies
 			)
 
+		# Sample multinomial distribution to determine which mRNAs have full 70S
+		# ribosomes initalized on them
 		nNewProteins = self.randomState.multinomial(
 			inactiveRibosomeCount,
 			proteinInitProb
 			)
 
-		nonzeroCount = (nNewProteins > 0)
-
+		# Check that sampling produced expected result
 		assert nNewProteins.sum() == inactiveRibosomeCount
 
-		# Build list of protein indexes
-
+		# Each ribosome is assigned a protein index for the protein that corresponds to the
+		# polypeptide it will polymerize. This is done in blocks of protein ids for efficiency.
 		proteinIndexes = np.empty(inactiveRibosomeCount, np.int64)
-
+		nonzeroCount = (nNewProteins > 0)
 		startIndex = 0
 		for proteinIndex, counts in itertools.izip(
 				np.arange(nNewProteins.size)[nonzeroCount],
@@ -114,8 +92,7 @@ class PolypeptideInitiation(wholecell.processes.process.Process):
 
 			startIndex += counts
 
-		# Create the active ribosomes
-
+		# Create active 70S ribosomes and assign their protein indexes calculated above
 		activeRibosomes = self.activeRibosomes.moleculesNew(
 			"activeRibosome",
 			inactiveRibosomeCount
@@ -125,7 +102,9 @@ class PolypeptideInitiation(wholecell.processes.process.Process):
 			proteinIndex = proteinIndexes
 			)
 
+		# Decrement free 30S and 70S ribosomal subunit counts
 		self.ribosome30S.countDec(nNewProteins.sum())
 		self.ribosome50S.countDec(nNewProteins.sum())
 
+		# Write number of initalized ribosomes to listener
 		self.writeToListener("RibosomeData", "didInitialize", nNewProteins.sum())
