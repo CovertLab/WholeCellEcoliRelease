7cbb1a3414cb4228cdfc9ce8cdc454a12714852b
javiercarrera <jcarrera@stanford.edu>
2017-03-23 12:53:37 -0700

    protein degradation process edits

--- models/ecoli/processes/protein_degradation.py
+++ models/ecoli/processes/protein_degradation.py
@@ -6,7 +6,8 @@ ProteinDegradation
 Protein degradation sub-model. Encodes molecular simulation of protein degradation as a Poisson process
 
 TODO:
-- complexes
+- protein complexes
+- add protease functionality
 
 @author: Nick Ruggero
 @organization: Covert Lab, Department of Bioengineering, Stanford University
@@ -28,6 +29,7 @@ class ProteinDegradation(wholecell.processes.process.Process):
 
 	# Constructor
 	def __init__(self):
+
 		# Parameters
 		self.proteinLengths = None		# Protein lengths
 		self.proteinDegSMatrix = None	# Protein degradation stoichiometry matrix [metabolite x rna]
@@ -43,61 +45,58 @@ class ProteinDegradation(wholecell.processes.process.Process):
 	def initialize(self, sim, sim_data):
 		super(ProteinDegradation, self).initialize(sim, sim_data)
 
+		# Load protein degradation rates (based on N-end rule)
 		self.rawDegRate = sim_data.process.translation.monomerData['degRate'].asNumber(1 / units.s)
 
-		# Metabolite IDs for S matrix
+		# Build metabolite IDs for S matrix
 		h2oId = ["WATER[c]"]
-
 		metaboliteIds = sim_data.moleculeGroups.aaIDs + h2oId
-
 		aaIdxs = np.arange(0, len(sim_data.moleculeGroups.aaIDs))
 		h2oIdx = metaboliteIds.index('WATER[c]')
 
-		# Protein IDs for S matrix
+		# Build protein IDs for S matrix
 		proteinIds = sim_data.process.translation.monomerData['id']
 
-		# Proteins
+		# Load protein length
 		self.proteinLengths = sim_data.process.translation.monomerData['length']
 
+		# Build S matrix
 		self.proteinDegSMatrix = np.zeros((len(metaboliteIds), len(proteinIds)), np.int64)
 		self.proteinDegSMatrix[aaIdxs, :] = np.transpose(sim_data.process.translation.monomerData["aaCounts"].asNumber())
 		self.proteinDegSMatrix[h2oIdx, :]  = -(np.sum(self.proteinDegSMatrix[aaIdxs, :], axis = 0) - 1)
 
-		# Views
+		# Build Views
 		self.metabolites = self.bulkMoleculesView(metaboliteIds)
 		self.h2o = self.bulkMoleculeView('WATER[c]')
 		self.proteins = self.bulkMoleculesView(proteinIds)
 
 		self.bulkMoleculesRequestPriorityIs(REQUEST_PRIORITY_DEGRADATION)
 
-		# TODO: Curate which proteases to use here
-		# self.protease = self.bulkMoleculeView('EG11259-MONOMER[c]')
-
 	def calculateRequest(self):
+
+		# Determine how many proteins to degrade based on the degradation rates and counts of each protein
 		nProteinsToDegrade = np.fmin(
 			self.randomState.poisson(self._proteinDegRates() * self.proteins.total()),
 			self.proteins.total()
 			)
 
+		# Determine the number of hydrolysis reactions
 		nReactions = np.dot(self.proteinLengths.asNumber(), nProteinsToDegrade)
 
+		# Determine the amount of water required to degrade the selected proteins
 		# Assuming one N-1 H2O is required per peptide chain length N
 		self.h2o.requestIs(nReactions - np.sum(nProteinsToDegrade))
 		self.proteins.requestIs(nProteinsToDegrade)
-		#self.protease.requestAll()
 
 
 	def evolveState(self):
-		# Check if protease expressed
-		#if self.protease.count() == 0:
-		#	return
 
-		# Degrade protein
+		# Degrade selected proteins, release amino acids from those proteins back into the cell, 
+		# and consume H_2O that is required for the degradation process
 		self.metabolites.countsInc(np.dot(
 			self.proteinDegSMatrix,
 			self.proteins.counts()
 			))
-
 		self.proteins.countsIs(0)
 
 	def _proteinDegRates(self):
