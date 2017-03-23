bcada274397e61edefeafe663b29aa4c9d2e712b
heejochoi <heejochoi@gmail.com>
2017-03-22 18:17:06 -0700

    Clean comments.

--- reconstruction/ecoli/dataclasses/process/transcription_regulation.py
+++ reconstruction/ecoli/dataclasses/process/transcription_regulation.py
@@ -1,4 +1,6 @@
-
+"""
+Transcription regulation.
+"""
 from __future__ import division
 
 import numpy as np
@@ -10,29 +12,16 @@ import collections
 
 class TranscriptionRegulation(object):
 	def __init__(self, raw_data, sim_data):
+		# Build lookups
 		self._buildLookups(raw_data, sim_data)
 
-		# self.tfs = sorted(set([x["TF"].encode("utf-8") for x in raw_data.foldChanges]))
-		# self.tfTarget = collections.defaultdict(list)
-		# self.targetTF = collections.defaultdict(list)
-		# self.tfIdTargetId = collections.defaultdict(list)
-		# self.targetIdTFId = collections.defaultdict(list)
+		# Build dictionary mapping transcription factors to their Kds
 		self.tfKd = {}
 		mRNASet = set([x["id"].encode("utf-8") for x in raw_data.rnas if x["type"] != "rRNA" and x["type"] != "tRNA"])
 		for D in raw_data.foldChanges:
-			# self.tfTarget[ D["TF"].encode("utf-8") ].append( D["Target"].encode("utf-8") )
-			# self.targetTF[ D["Target"].encode("utf-8") ].append( D["TF"].encode("utf-8") )
-
-			# # Ignore things like two-component systems
-			# if D["TF"].encode("utf-8") not in self.abbrToActiveId:
-			# 	continue
-
-			# for activeId in self.abbrToActiveId[ D["TF"].encode("utf-8") ]:
-			# 	self.tfIdTargetId[activeId].append(self.abbrToRnaId[ D["Target"].encode("utf-8") ])
-			# 	self.targetIdTFId[self.abbrToRnaId[ D["Target"].encode("utf-8") ]].append(activeId)
-
 			self.tfKd[self.abbrToActiveId[D["TF"].encode("utf-8")][0]] = D["kd"]
 
+		# Build dictionary mapping RNA targets to its regulators
 		self.targetTf = {}
 		for tf in sim_data.tfToFC:
 			targets = sim_data.tfToFC[tf]
@@ -47,18 +36,28 @@ class TranscriptionRegulation(object):
 			for targetToRemove in targetsToRemove:
 				sim_data.tfToFC[tf].pop(targetToRemove)
 
+		# Build dictionaries mapping transcription factors to the number of its targets, 
+		# active transcription factors to their bound form, and transcription factors to their regulating type
 		self.tfNTargets = dict([(key, len(val)) for key,val in sim_data.tfToFC.iteritems()])
 		self.activeToBound = dict([(x["active TF"].encode("utf-8"), x["metabolite bound form"].encode("utf-8")) for x in raw_data.tfOneComponentBound])
 		self.tfToTfType = dict([(x["active TF"].encode("utf-8"), x["TF type"].encode("utf-8")) for x in raw_data.condition.tf_condition])
 		return
 
 	def pPromoterBoundTF(self, tfActive, tfInactive):
+		'''
+		Computes probability of a transcription factor binding promoter.
+		'''
 		return float(tfActive) / (float(tfActive) + float(tfInactive))
 
 	def pPromoterBoundSKd(self, signal, Kd, power):
+		'''
+		Computes probability of a one-component transcription factor binding promoter.
+		'''
 		return float(signal)**power / (float(signal)**power + float(Kd))
 
 	def pPromoterBound(self, dissocConstant, nPromoters, nTfs):
+		'''
+		'''
 		b = float(dissocConstant + nPromoters + nTfs)
 		c = float(nPromoters * nTfs)
 		pos = (b + np.sqrt(b**2 - 4 * c)) / (2 * nPromoters)
@@ -72,6 +71,8 @@ class TranscriptionRegulation(object):
 		raise Exception, "Neither solution to the quadratic equation is between 0 and 1"
 
 	def pTfBound(self, dissocConstant, nPromoters, nTfs):
+		'''
+		'''
 		b = float(dissocConstant + nPromoters + nTfs)
 		c = float(nPromoters * nTfs)
 		pos = (b + np.sqrt(b**2 - 4 * c)) / (2 * nTfs)
@@ -85,6 +86,9 @@ class TranscriptionRegulation(object):
 		raise Exception, "Neither solution to the quadratic equation is between 0 and 1"
 
 	def _buildLookups(self, raw_data, sim_data):
+		'''
+		Builds dictionaries for mapping transcription factor abbreviations to their RNA IDs, and to their active form.
+		'''
 		geneIdToRnaId = dict([(x["geneId"].encode("utf-8"), x["id"].encode("utf-8")) for x in raw_data.rnas])
 
 		self.abbrToRnaId = {}
@@ -94,32 +98,3 @@ class TranscriptionRegulation(object):
 			self.abbrToRnaId[lookupInfo["TF"].encode("utf-8")] = geneIdToRnaId[lookupInfo["geneId"].encode("utf-8")]
 
 		self.abbrToActiveId = dict([(x["TF"].encode("utf-8"), x["activeId"].encode("utf-8").split(", ")) for x in raw_data.tfIds if len(x["activeId"]) > 0])
-
-		# # Get rid of two-component systems, which we will incorporate later
-		# keysToDelete = []
-		# excludes = ["MONOMER0-1",
-		# 			"PROTEIN-NRIP",
-		# 			"MONOMER0-4180",
-		# 			"CPLX0-7754",
-		# 			"CPLX0-7884",
-		# 			"MONOMER0-4276",
-		# 			"CPLX0-7748",
-		# 			"MONOMER0-4288",
-		# 			"CPLX0-7721",
-		# 			"MONOMER0-2741",
-		# 			"MONOMER0-4147",
-		# 			"CPLX0-7795",
-		# 			"MONOMER0-4341",
-		# 			]
-		# for key, val in self.abbrToActiveId.iteritems():
-		# 	if val[0].startswith("PHOSPHO") or val[0] in excludes:
-		# 		keysToDelete.append(key)
-		# 		continue
-		# 	locations = sim_data.getter.getLocation(val)
-		# 	L = []
-		# 	for frameId, loc in zip(val, locations):
-		# 		L.append(frameId + "[" + loc[0] + "]")
-		# 	self.abbrToActiveId[key] = L
-
-		# for key in keysToDelete:
-		# 	del self.abbrToActiveId[key]
