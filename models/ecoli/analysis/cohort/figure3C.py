777d8dffcc9e5367faa53a6082f4086779c336b1
Travis Horst <thorst@stanford.edu>
2017-04-14 16:57:10 -0700

    Update figure3C cohort analysis

--- models/ecoli/analysis/cohort/figure3C.py
+++ models/ecoli/analysis/cohort/figure3C.py
@@ -29,7 +29,8 @@ from wholecell.analysis.plotting_tools import COLORS_LARGE
 
 from models.ecoli.processes.metabolism import COUNTS_UNITS, VOLUME_UNITS, TIME_UNITS, MASS_UNITS
 
-BURN_IN_STEPS = 20
+# ignore data from metabolism burnin period
+BURN_IN_TIME = 1
 
 def main(variantDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile = None, metadata = None):
 	if not os.path.isdir(variantDir):
@@ -41,7 +42,6 @@ def main(variantDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 	# Get all cells
 	ap = AnalysisPaths(variantDir, cohort_plot = True)
 	allDir = ap.get_cells()
-	# allDir = ap.get_cells(generation = [0, 1, 2])
 
 	sim_data = cPickle.load(open(simDataFile, "rb"))
 
@@ -52,15 +52,16 @@ def main(variantDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 
 	targetFluxList = []
 	actualFluxList = []
+	reactionConstraintlist = []
 
 	for simDir in allDir:
 		simOutDir = os.path.join(simDir, "simOut")
 
 		mainListener = TableReader(os.path.join(simOutDir, "Main"))
-		initialTime = mainListener.readAttribute("initialTime")
-		time = mainListener.readColumn("time") - initialTime
-		timeStepSec = mainListener.readColumn("timeStepSec")
+		time = mainListener.readColumn("time")
 		mainListener.close()
+		burnIn = time > BURN_IN_TIME
+		burnIn[0] = False
 
 		massListener = TableReader(os.path.join(simOutDir, "Mass"))
 		cellMass = massListener.readColumn("cellMass")
@@ -79,52 +80,60 @@ def main(variantDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 		targetFluxes = targetFluxes.asNumber(units.mmol / units.g / units.h)
 		actualFluxes = actualFluxes.asNumber(units.mmol / units.g / units.h)
 
-		targetAve = np.nanmean(targetFluxes[BURN_IN_STEPS:, :], axis = 0)
-		actualAve = np.nanmean(actualFluxes[BURN_IN_STEPS:, :], axis = 0)
+		targetAve = np.nanmean(targetFluxes[burnIn, :], axis = 0)
+		actualAve = np.nanmean(actualFluxes[burnIn, :], axis = 0)
 
 		if len(targetFluxList) == 0:
 			targetFluxList = np.array([targetAve])
 			actualFluxList = np.array([actualAve])
+			reactionConstraintList = np.array(reactionConstraint[burnIn, :])
 		else:
 			targetFluxList = np.concatenate((targetFluxList, np.array([targetAve])), axis = 0)
 			actualFluxList = np.concatenate((actualFluxList, np.array([actualAve])), axis = 0)
+			reactionConstraintList = np.concatenate((reactionConstraintList, np.array(reactionConstraint[burnIn, :])), axis = 0)
 
+	# determine average across all cells
 	targetAve = np.nanmean(targetFluxList, axis = 0)
 	actualAve = np.nanmean(actualFluxList, axis = 0)
+
+	# categorize reactions that use constraints with only kcat, Km and kcat, or switch between both types of constraints
+	kcatOnlyReactions = np.all(constraintIsKcatOnly[reactionConstraintList], axis = 0)
+	kmAndKcatReactions = ~np.any(constraintIsKcatOnly[reactionConstraintList], axis = 0)
+	mixedReactions = ~(kcatOnlyReactions ^ kmAndKcatReactions)
 
 	disabledReactions = np.zeros(len(constrainedReactions), dtype = bool)
 	if not useAllConstraints:
 		for rxn in constraintsToDisable:
-			disabledReactions[np.where(constrainedReactions == rxn)[0]] = True
+			idx = np.where(constrainedReactions == rxn)[0]
+			disabledReactions[idx] = True
+			kcatOnlyReactions[idx] = False
+			kmAndKcatReactions[idx] = False
+			mixedReactions[idx] = False
 
+	# categorize how well the actual flux matches the target flux
 	thresholds = [2, 10]
 	categorization = np.zeros(reactionConstraint.shape[1])
+	for i, threshold in enumerate(thresholds):
+		categorization[kmAndKcatReactions & (actualAve / targetAve < 1. / threshold)] = i + 1
+		categorization[actualAve / targetAve > threshold] = i + 1
 	categorization[actualAve == 0] = -2
 	categorization[actualAve == targetAve] = -1
-	for i, threshold in enumerate(thresholds):
-		# categorization[targetAve / actualAve > threshold] = i + 1
-		categorization[actualAve / targetAve > threshold] = i + 1
 
-	# url for ecocyc to highlight fluxes that are 0 on metabolic network diagram
-	siteStr = "https://ecocyc.org/overviewsWeb/celOv.shtml?zoomlevel=1&orgid=ECOLI"
-	excluded = ['RXN0-2201', 'RXN-16000', 'RXN-12583', 'RXN-11496', 'DIMESULFREDUCT-RXN', '3.6.1.41-R[4/63051]5-NUCLEOTID-RXN'] # reactions not recognized by ecocyc
-	rxns = []
-	for i, reaction in enumerate(constrainedReactions[categorization == -2]):
-		rxn = re.findall(".+RXN", reaction)
-		if len(rxn) == 0:
-			rxn = re.findall("RXN[^-]*-[0-9]+", reaction)
-		if rxn[0] not in excluded:
-			siteStr += "&rnids=%s" % rxn[0]
-		rxns.append(rxn[0])
-	# print siteStr
-
+	# write data for each reaction to a file
 	csvFile = open(os.path.join(plotOutDir, plotOutFileName + ".tsv"), "wb")
 	output = csv.writer(csvFile, delimiter = "\t")
-	output.writerow(["ecocyc link:", siteStr])
-	output.writerow(["", "Target", "Actual", "Category"])
-
-	for reaction, target, flux, category in zip(constrainedReactions[~disabledReactions], targetAve[~disabledReactions], actualAve[~disabledReactions], categorization[~disabledReactions]):
+	output.writerow(["Km and kcat", "Target", "Actual", "Category"])
+	for reaction, target, flux, category in zip(constrainedReactions[kmAndKcatReactions], targetAve[kmAndKcatReactions], actualAve[kmAndKcatReactions], categorization[kmAndKcatReactions]):
 		output.writerow([reaction, target, flux, category])
+
+	output.writerow(["kcat only"])
+	for reaction, target, flux, category in zip(constrainedReactions[kcatOnlyReactions], targetAve[kcatOnlyReactions], actualAve[kcatOnlyReactions], categorization[kcatOnlyReactions]):
+		output.writerow([reaction, target, flux, category])
+
+	if np.sum(mixedReactions):
+		output.writerow(["mixed constraints"])
+		for reaction, target, flux, category in zip(constrainedReactions[mixedReactions], targetAve[mixedReactions], actualAve[mixedReactions], categorization[mixedReactions]):
+			output.writerow([reaction, target, flux, category])
 
 	if np.sum(disabledReactions):
 		output.writerow(["disabled constraints"])
@@ -133,13 +142,15 @@ def main(variantDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 
 	csvFile.close()
 
+	# add small number to allow plotting of 0 flux on log scale
 	targetAve += 1e-6
 	actualAve += 1e-6
 
+	# plot data
 	plt.figure(figsize = (8, 8))
 	ax = plt.axes()
 	from scipy.stats import pearsonr
-	plt.plot([-7, 4], [-7, 4], 'k')
+	plt.plot([-8, 4], [-8, 4], 'k')
 	plt.plot(np.log10(targetAve[~disabledReactions]), np.log10(actualAve[~disabledReactions]), "ob", markeredgewidth = 0.25, alpha = 0.25)
 	plt.xlabel("Log10(Target Flux [mmol/g/hr])")
 	plt.ylabel("Log10(Actual Flux [mmol/g/hr])")
@@ -154,132 +165,6 @@ def main(variantDir, plotOutDir, plotOutFileName, simDataFile, validationDataFil
 	exportFigure(plt, plotOutDir, plotOutFileName)
 	plt.close("all")
 
-	return
-
-	# Bokeh
-	from bokeh.plotting import figure, output_file, ColumnDataSource, show
-	from bokeh.charts import Bar
-	from bokeh.models import HoverTool, BoxZoomTool, LassoSelectTool, PanTool, WheelZoomTool, ResizeTool, UndoTool, RedoTool
-
-	source = ColumnDataSource(
-		data = dict(
-			x = targetAve,
-			y = actualAve,
-			reactionName = constrainedReactions)
-		)
-
-	hover = HoverTool(
-		tooltips = [
-			("Reaction", "@reactionName"),
-			]
-		)
-	
-	TOOLS = [hover, 
-		BoxZoomTool(),
-		LassoSelectTool(),
-		PanTool(),
-		WheelZoomTool(),
-		ResizeTool(),
-		UndoTool(),
-		RedoTool(),
-		 "reset"
-		 ]
-
-	p1 = figure(x_axis_label = "Target",
-		x_axis_type = "log",
-		x_range = [min(targetAve[targetAve > 0]), max(targetAve)],
-		y_axis_label = "Actual",
-		y_axis_type = "log",
-		y_range = [min(actualAve[actualAve > 0]), max(actualAve)],
-		width = 800,
-		height = 800,
-		tools = TOOLS,
-		)
-
-	p1.scatter(targetAve, actualAve, source = source, size = 8)
-	p1.line([1e-15, 10], [1e-15, 10], line_color = "red", line_dash = "dashed")
-	
-
-	## bar plot of error
-	# sortedReactions = [constrainedReactions[x] for x in np.argsort(aveError)[::-1]]
-	# aveError[np.log10(aveError) == -np.inf] = 0
-
-	# source = ColumnDataSource(
-	# 	data = dict(
-	# 			x = sorted(relError, reverse = True),
-	# 			reactionName = sortedReactions
-	# 		)
-	# 	)
-
-	# p2 = Bar(data, values = "x")
-
-	# hover2 = p2.select(dict(type=HoverTool))
-	# hover2.tooltips = [("Reaction", "@reactionName")]
-
-	## flux for each reaction
-	hover2 = HoverTool(
-		tooltips = [
-			("Reaction", "@reactionName"),
-			]
-		)
-
-	TOOLS2 = [hover2, 
-		BoxZoomTool(),
-		LassoSelectTool(),
-		PanTool(),
-		WheelZoomTool(),
-		ResizeTool(),
-		UndoTool(),
-		RedoTool(),
-		 "reset"
-		 ]
-
-	p2 = figure(x_axis_label = "Time(s)", 
-		y_axis_label = "Flux",
-		y_axis_type = "log",
-		y_range = [1e-8, 1],
-		width = 800,
-		height = 800,
-		tools = TOOLS2,
-		)
-
-	colors = COLORS_LARGE
-	nTimesteps = len(time[BURN_IN_STEPS:])
-	x = time[BURN_IN_STEPS:]
-	y = actualFluxes[BURN_IN_STEPS:, 0]
-	reactionName = np.repeat(constrainedReactions[0], nTimesteps)
-
-	source = ColumnDataSource(
-		data = dict(
-			x = x,
-			y = y,
-			reactionName = reactionName)
-		)
-
-	p2.line(x, y, line_color = colors[0], source = source)
-
-	# Plot remaining metabolites onto initialized figure
-	for m in np.arange(1, actualFluxes.shape[1]):
-		y = actualFluxes[BURN_IN_STEPS:, m]
-		reactionName = np.repeat(constrainedReactions[m], nTimesteps)
-
-		source = ColumnDataSource(
-			data = dict(
-				x = x,
-				y = y,
-				reactionName = reactionName)
-		)
-
-		p2.line(x, y, line_color = colors[m % len(colors)], source = source)
-
-	if not os.path.exists(os.path.join(plotOutDir, "html_plots")):
-		os.makedirs(os.path.join(plotOutDir, "html_plots"))
-
-	import bokeh.io
-	p = bokeh.io.vplot(p1, p2)
-	bokeh.io.output_file(os.path.join(plotOutDir, "html_plots", plotOutFileName + ".html"), title=plotOutFileName, autosave=False)
-	bokeh.io.save(p)
-
 if __name__ == "__main__":
 	defaultSimDataFile = os.path.join(
 			wholecell.utils.constants.SERIALIZED_KB_DIR,
@@ -291,7 +176,8 @@ if __name__ == "__main__":
 	parser.add_argument("plotOutDir", help = "Directory containing plot output (will get created if necessary)", type = str)
 	parser.add_argument("plotOutFileName", help = "File name to produce", type = str)
 	parser.add_argument("--simDataFile", help = "KB file name", type = str, default = defaultSimDataFile)
+	parser.add_argument("--validationDataFile", help = "KB file name", type = str)
 
 	args = parser.parse_args().__dict__
-	
-	main(args["simOutDir"], args["plotOutDir"], args["plotOutFileName"], args["simDataFile"])
+
+	main(args["simOutDir"], args["plotOutDir"], args["plotOutFileName"], args["simDataFile"], args["validationDataFile"])
