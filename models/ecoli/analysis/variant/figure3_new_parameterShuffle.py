58423f0b388d70745c09abe8bfdd1a2d58f0ef0c
heejochoi <heejochoi@gmail.com>
2017-12-13 17:05:28 -0800

    Update layout of figure 3.

--- models/ecoli/analysis/variant/figure3_new_parameterShuffle.py
+++ models/ecoli/analysis/variant/figure3_new_parameterShuffle.py
@@ -13,8 +13,9 @@ from models.ecoli.analysis.AnalysisPaths import AnalysisPaths
 
 TITLE_FONTSIZE = 10
 LABEL_FONTSIZE = 8.5
-MARKERSIZE = 20
+MARKERSIZE = 30
 SCATTER_MSIZE = 5
+LINEWIDTH = 4
 
 GENERATE_CLEAN_SUBPLOTS = True
 
@@ -67,7 +68,6 @@ def main(inputDir, plotOutDir, plotOutFileName, validationDataFile, metadata = N
 	data = []
 	for varId, row in enumerate(xrange(2, data_raw.nrows)):
 		row_values = data_raw.row_values(row)
-		
 		if (varId + 1) in failures:
 			flag1 += 1
 		elif row_values[col_divisionTime] == 180:
@@ -87,16 +87,16 @@ def main(inputDir, plotOutDir, plotOutFileName, validationDataFile, metadata = N
 	protAx = plt.subplot2grid((nrows, ncols), (0, 3 + 1), rowspan = 2, colspan = 2)
 	fluxAx = plt.subplot2grid((nrows, ncols), (0, 6 + 1), rowspan = 2, colspan = 2)
 	dpAx = plt.subplot2grid((nrows, ncols), (4, 0 + 1), rowspan = 2, colspan = 2)
-	dfAx = plt.subplot2grid((nrows, ncols), (4, 3 + 1), rowspan = 2, colspan = 2)
-	pfAx = plt.subplot2grid((nrows, ncols), (4, 6 + 1), rowspan = 2, colspan = 2)
+	pfAx = plt.subplot2grid((nrows, ncols), (4, 3 + 1), rowspan = 2, colspan = 2)
+	fdAx = plt.subplot2grid((nrows, ncols), (4, 6 + 1), rowspan = 2, colspan = 2)
 	dp_dAx = plt.subplot2grid((nrows, ncols), (3, 1), rowspan = 1, colspan = 2)
-	df_dAx = plt.subplot2grid((nrows, ncols), (3, 4), rowspan = 1, colspan = 2)
+	pf_pAx = plt.subplot2grid((nrows, ncols), (3, 4), rowspan = 1, colspan = 2)
 	dp_pAx = plt.subplot2grid((nrows, ncols), (4, 0), rowspan = 2, colspan = 1)
-	pf_pAx = plt.subplot2grid((nrows, ncols), (3, 7), rowspan = 1, colspan = 2)
-	df_fAx = plt.subplot2grid((nrows, ncols), (4, 3), rowspan = 2, colspan = 1)
-	pf_fAx = plt.subplot2grid((nrows, ncols), (4, 6), rowspan = 2, colspan = 1)
+	fd_fAx = plt.subplot2grid((nrows, ncols), (3, 7), rowspan = 1, colspan = 2)
+	pf_fAx = plt.subplot2grid((nrows, ncols), (4, 3), rowspan = 2, colspan = 1)
+	fd_dAx = plt.subplot2grid((nrows, ncols), (4, 6), rowspan = 2, colspan = 1)
 	histAxes = [diviAx, protAx, fluxAx]
-	axesList = [diviAx, protAx, fluxAx, dpAx, dfAx, pfAx, dp_dAx, dp_pAx, df_dAx, pf_pAx, df_fAx, pf_fAx]
+	axesList = [diviAx, protAx, fluxAx, dpAx, pfAx, fdAx, dp_dAx, dp_pAx, pf_pAx, fd_fAx, pf_fAx, fd_dAx]
 
 	# Plot histograms
 	nBins = 50
@@ -106,8 +106,8 @@ def main(inputDir, plotOutDir, plotOutFileName, validationDataFile, metadata = N
 		if i == 2:
 			fluxAx.set_ylim([fluxAx.get_ylim()[0], 55])
 		ax.hist(data[:, i], bins = nBins, edgecolor = "none", facecolor = "k", alpha = 0.5)
-		ax.plot([wt[i], wt[i]], [0, ax.get_ylim()[1]], "b")
-		ax.set_title(histTitles[i], fontsize = TITLE_FONTSIZE)
+		ax.plot([wt[i], wt[i]], [0, ax.get_ylim()[1]], "r", linewidth = LINEWIDTH)
+		ax.set_title("%s\nwt = %0.2f" % (histTitles[i], wt[i]), fontsize = TITLE_FONTSIZE)
 		ax.set_ylabel("Frequency", fontsize = TITLE_FONTSIZE)
 		ax.set_xlabel(histXlabels[i], fontsize = TITLE_FONTSIZE)
 
@@ -125,67 +125,36 @@ def main(inputDir, plotOutDir, plotOutFileName, validationDataFile, metadata = N
 		whitePadSparklineAxis(ax)
 
 	# Plot scatter plots
-	dpAx.scatter(data[:, 0], data[:, 1], s = 2, edgecolor = "none", facecolor = "k")
-	dpAx.plot(wt[0], wt[1], "b.", markersize = MARKERSIZE)
-	dpAx.set_xlim(diviAx.get_xlim())
-	dpAx.set_ylim(protAx.get_xlim())
-	whitePadSparklineAxis(dpAx)
-	dpAx.axvline(x = wt[0], linestyle = "--")
-	dpAx.axhline(y = wt[1], linestyle = "--")
-
-	dfAx.scatter(data[:, 0], data[:, 2], s = 2, edgecolor = "none", facecolor = "k")
-	dfAx.plot(wt[0], wt[2], "b.", markersize = MARKERSIZE)
-	dfAx.set_xlim(diviAx.get_xlim())
-	dfAx.set_ylim(fluxAx.get_xlim())
-	whitePadSparklineAxis(dfAx)
-	dfAx.axvline(x = wt[0], linestyle = "--")
-	dfAx.axhline(y = wt[2], linestyle = "--")
-
-	pfAx.scatter(data[:, 1], data[:, 2], s = 2, edgecolor = "none", facecolor = "k")
-	pfAx.plot(wt[1], wt[2], "b.", markersize = MARKERSIZE)
-	pfAx.set_xlim(protAx.get_xlim())
-	pfAx.set_ylim(fluxAx.get_xlim())
-	whitePadSparklineAxis(pfAx)
-	pfAx.axvline(x = wt[1], linestyle = "--")
-	pfAx.axhline(y = wt[2], linestyle = "--")
+	axesList = [dpAx, pfAx, fdAx]
+	xIndices = [0, 1, 2]
+	yIndices = [1, 2, 0]
+	for i, ax in enumerate(axesList):
+		ax.scatter(data[:, xIndices[i]], data[:, yIndices[i]], s = 2, edgecolor = "none", facecolor = "k")
+		ax.plot(wt[xIndices[i]], wt[yIndices[i]], "r.", markersize = MARKERSIZE)
+		ax.set_xlim(histAxes[xIndices[i]].get_xlim())
+		ax.set_ylim(histAxes[yIndices[i]].get_xlim())
+		whitePadSparklineAxis(ax)
+		ax.axvline(x = wt[xIndices[i]], linestyle = "--", color = "r")
+		ax.axhline(y = wt[yIndices[i]], linestyle = "--", color = "r")
 
 	# Plot small histograms
-	for ax in [dp_dAx, df_dAx]:
-		ax.hist(data[:, 0], bins = nBins, edgecolor = "none", facecolor = "k", alpha = 0.5)
-		ax.plot([wt[0], wt[0]], [0, ax.get_ylim()[1]], "b")
+	fd_fAx.set_ylim([fluxAx.get_ylim()[0], 55])
+	titles = ["Division Time (min)", "Proteome Comparison (PCC)", "Fluxome Comparison (PCC)"]
+	for i, ax in enumerate([dp_dAx, pf_pAx, fd_fAx]):
+		ax.hist(data[:, i], bins = nBins, edgecolor = "none", facecolor = "k", alpha = 0.5)
+		ax.axvline(wt[i], color = "r", linewidth = LINEWIDTH)
 		removeXAxis(ax)
 		whitePadSparklineAxis(ax)
+		ax.set_title(titles[i], fontsize = TITLE_FONTSIZE)
 
-	for ax in [dp_pAx, pf_pAx]:
-		orientation = "vertical"
-		if ax == dp_pAx:
-			orientation = "horizontal"
-		ax.hist(data[:, 1], bins = nBins, edgecolor = "none", orientation = orientation, facecolor = "k", alpha = 0.5)
-		if orientation == "horizontal":
-			ax.plot([0, ax.get_xlim()[1]], [wt[1], wt[1]], "b")
-			removeYAxis(ax)
-		else:
-			ax.plot([wt[1], wt[1]], [0, ax.get_ylim()[1]], "b")
-			removeXAxis(ax)
-		whitePadSparklineAxis(ax)
-
-	for ax in [df_fAx, pf_fAx]:
-		ax.hist(data[:, 2], bins = nBins, edgecolor = "none", orientation = "horizontal", facecolor = "k", alpha = 0.5)
-		ax.plot([0, ax.get_xlim()[1]], [wt[2], wt[2]], "b")
-		ax.set_xlim([0, 55])
+	pf_fAx.set_xlim([fluxAx.get_ylim()[0], 55])
+	for i, ax in enumerate([fd_dAx, dp_pAx, pf_fAx]):
+		ax.hist(data[:, i], bins = nBins, edgecolor = "none", facecolor = "k", alpha = 0.5, orientation = "horizontal")
+		ax.axhline(wt[i], color = "r", linewidth = LINEWIDTH)
 		removeYAxis(ax)
 		whitePadSparklineAxis(ax)
-
-	dp_dAx.set_title("Division Time (min)", fontsize = TITLE_FONTSIZE)
-	df_dAx.set_title("Division Time (min)", fontsize = TITLE_FONTSIZE)
-	dp_pAx.set_ylabel("Proteome Comparison (PCC)", fontsize = TITLE_FONTSIZE, rotation = "vertical")
-	pf_pAx.set_title("Proteome Comparison (PCC)", fontsize = TITLE_FONTSIZE)
-	df_fAx.set_ylabel("Fluxome Comparison (PCC)", fontsize = TITLE_FONTSIZE, rotation = "vertical")
-	pf_fAx.set_ylabel("Fluxome Comparison (PCC)", fontsize = TITLE_FONTSIZE, rotation = "vertical")
-
-	for ax in [dp_pAx, df_fAx, pf_fAx]:
+		ax.set_ylabel(titles[i], fontsize = TITLE_FONTSIZE)
 		ax.invert_xaxis()
-		ax.xaxis.set_label_position("top")
 
 	for ax in axesList:
 		ax.tick_params(labelsize = LABEL_FONTSIZE)
@@ -197,67 +166,54 @@ def main(inputDir, plotOutDir, plotOutFileName, validationDataFile, metadata = N
 	if GENERATE_CLEAN_SUBPLOTS:
 
 		# Histograms
-		fig, ax = plt.subplots(1, 1, figsize = (6, 6))
+		fig, ax = plt.subplots(1, 1, figsize = (5, 5))
 		filenames = ["Division", "Proteome", "Fluxome"]
 		axesList = [diviAx, protAx, fluxAx]
 		for i, filename in enumerate(filenames):
 			ax.set_xlim(axesList[i].get_xlim())
 			ax.set_ylim(axesList[i].get_ylim())
-			ax.plot([wt[i], wt[i]], [0, ax.get_ylim()[1]], "b")
+			ax.plot([wt[i], wt[i]], [0, ax.get_ylim()[1]], "r", linewidth = LINEWIDTH)
 			ax.hist(data[:, i], bins = nBins, edgecolor = "none", facecolor = "k", alpha = 0.5)
 			whitePadSparklineAxis(ax)
+			removeXAxis(ax)
 			cleanAxis(ax)
 			exportFigure(plt, plotOutDir, plotOutFileName + "_%s" % filename, metadata)
 			plt.cla()
 
-		# Sploms
+		# Scatter plots
 		fig, ax = plt.subplots(1, 1, figsize = (5, 5))
-		filenames = ["DP", "DF", "PF"]
-		axesList = [dpAx, dfAx, pfAx]
-		xIndices = [0, 0, 1]
-		yIndices = [1, 2, 2]
+		filenames = ["DP", "PF", "FD"]
+		axesList = [dpAx, pfAx, fdAx]
+		xIndices = [0, 1, 2]
+		yIndices = [1, 2, 0]
 		for i, filename in enumerate(filenames):
 			ax.scatter(data[:, xIndices[i]], data[:, yIndices[i]], s = SCATTER_MSIZE, edgecolor = "none", facecolor = "k")
-			ax.plot(wt[xIndices[i]], wt[yIndices[i]], "b.", markersize = MARKERSIZE)
+			ax.plot(wt[xIndices[i]], wt[yIndices[i]], "r.", markersize = MARKERSIZE)
 			ax.set_xlim(axesList[i].get_xlim())
 			ax.set_ylim(axesList[i].get_ylim())
 			removeYAxis(ax)
 			whitePadSparklineAxis(ax)
-			ax.axvline(x = wt[xIndices[i]], linestyle = "--")
-			ax.axhline(y = wt[yIndices[i]], linestyle = "--")
+			ax.axvline(x = wt[xIndices[i]], linestyle = "--", color = "r")
+			ax.axhline(y = wt[yIndices[i]], linestyle = "--", color = "r")
 			cleanAxis(ax)
 			exportFigure(plt, plotOutDir, plotOutFileName + "_%s" % filename, metadata)
 			plt.cla()
 		plt.close("all")
 
-		# Small histograms for splom
+		# Small histograms for splom (only generates side histograms)
 		filenames = ["Division", "Proteome", "Fluxome"]
 		axesList = [diviAx, protAx, fluxAx]
 		for i, filename in enumerate(filenames):
-
-			if i in [0, 1]:	
-				fig, ax = plt.subplots(1, 1, figsize = (5, 2))
-				ax.set_xlim(axesList[i].get_xlim())
-				ax.set_ylim(axesList[i].get_ylim())
-				ax.plot([wt[i], wt[i]], [0, ax.get_ylim()[1]], "b")
-				ax.hist(data[:, i], bins = nBins, edgecolor = "none", facecolor = "k", alpha = 0.5)
-				removeXAxis(ax)
-				whitePadSparklineAxis(ax)
-				cleanAxis(ax)
-				exportFigure(plt, plotOutDir, plotOutFileName + "_%s_%s_vertical" % (filename, "small"), metadata)
-				plt.close("all")
-
-			if i in [1, 2]:
-				fig, ax = plt.subplots(1, 1, figsize = (2, 5))
-				ax.set_ylim(axesList[i].get_xlim())
-				ax.set_xlim(axesList[i].get_ylim())
-				ax.plot([0, ax.get_xlim()[1]], [wt[i], wt[i]], "b")				
-				ax.hist(data[:, i], bins = nBins, edgecolor = "none", facecolor = "k", alpha = 0.5, orientation = "horizontal")
-				ax.invert_xaxis()
-				whitePadSparklineAxis(ax)
-				cleanAxis(ax)
-				exportFigure(plt, plotOutDir, plotOutFileName + "_%s_%s_horizontal" % (filename, "small"), metadata)
-				plt.close("all")
+			fig, ax = plt.subplots(1, 1, figsize = (2, 5))
+			ax.set_ylim(axesList[i].get_xlim())
+			ax.set_xlim(axesList[i].get_ylim())
+			ax.hist(data[:, i], bins = nBins, edgecolor = "none", facecolor = "k", alpha = 0.5, orientation = "horizontal")
+			ax.axhline(wt[i], color = "r", linewidth = LINEWIDTH)
+			ax.invert_xaxis()
+			whitePadSparklineAxis(ax)
+			cleanAxis(ax)
+			exportFigure(plt, plotOutDir, plotOutFileName + "_%s_%s" % (filename, "small"), metadata)
+			plt.close("all")
 
 	plt.close("all")
 
