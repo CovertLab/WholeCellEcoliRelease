8cb76364cb3ce193430293b1d3efe2e9cf843e64
Jerry Morrison <1fish2@users.noreply.github.com>
2018-02-08 21:41:21 -0800

    add more info to summarize_environment.py
    including np.show_config() and module versions.

--- runscripts/debug/summarize_environment.py
+++ runscripts/debug/summarize_environment.py
@@ -6,27 +6,43 @@ debugging and performance tuning.
 """
 
 import multiprocessing
+import numpy as np
 import os
 import scipy
 import sys
 
-def main():
-	print
-	print "multiprocessing.cpu_count(): {}".format(multiprocessing.cpu_count())
-	print
 
-	select_env = {key: value for (key, value) in os.environ.iteritems()
-				  if key in {'HOME', 'PYENV_ROOT'}}
-	print "os.environ: {}...".format(select_env)
+def subtitle(module):
+	"""Print a section subtitle with the module's name and version."""
+	print
+	print "{} {}".format(module.__name__, module.__dict__.get('__version__', ''))
+
+def print_dictionary(name, a_dict, select_keys):
+	"""Print select keys from a dict, as available, in order, one per line."""
+	print "{}:".format(name)
+	for key in select_keys:
+		value = "'{}'".format(a_dict[key]) if key in a_dict else '--'
+		print "  '{}': {}".format(key, value)
+
+def main():
+	subtitle(multiprocessing)
+	print "multiprocessing.cpu_count(): {}".format(multiprocessing.cpu_count())
+
+	subtitle(np)
+	np.show_config()
+
+	subtitle(os)
+	print_dictionary("os.environ", os.environ, [
+		'BOOST_NUMPY_LIB', 'HOME', 'LIBRARY_PATH', 'PI_HOME', 'PYENV_ROOT',
+		'PYTHONPATH', 'SHERLOCK'])
 	print "os.getcwd(): {}".format(os.getcwd())
 	print "os.uname(): {}".format(os.uname())
-	print
 
-	print "scipy.__config__.show()"
+	subtitle(scipy)
 	scipy.__config__.show()
-	print
 
-	print "sys.platform(): {}".format(sys.platform)
+	subtitle(sys)
+	print "sys.platform: {}".format(sys.platform)
 	print "sys.prefix: {}".format(sys.prefix)
 	print "sys.version: {}".format(sys.version)
 	print "sys.api_version: {}".format(sys.api_version)
