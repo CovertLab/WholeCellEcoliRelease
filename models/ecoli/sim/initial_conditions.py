b94bd14154ac65ce535e3ff27efce639421ac248
Gwanggyu Sun <ggsun@stanford.edu>
2018-06-21 17:10:51 -0700

    Refactor initializeReplication() and downstream functions

--- models/ecoli/sim/initial_conditions.py
+++ models/ecoli/sim/initial_conditions.py
@@ -186,41 +186,39 @@ def initializeComplexation(bulkMolCntr, sim_data, randomState):
 
 def initializeReplication(bulkMolCntr, uniqueMolCntr, sim_data):
 	"""
-	initializeReplication
-
-	Purpose: Create the appropriate number of replication forks given the cell growth rate.
+	Purpose: Initialize replication by creating an appropriate number of
+	replication forks given the cell growth rate.
 	"""
 
-	## Determine the number and location of replication forks at the start of the cell cycle
-	# Find growth rate constants
+	# Determine the number and location of replication forks at the start of
+	# the cell cycle
+	# Get growth rate constants
 	C = sim_data.growthRateParameters.c_period.asUnit(units.min)
 	D = sim_data.growthRateParameters.d_period.asUnit(units.min)
 	tau = sim_data.conditionToDoublingTime[sim_data.condition].asUnit(units.min)
+
+	# Calculate replication length
 	genome_length = sim_data.process.replication.genome_length
-	replication_length = np.ceil(.5*genome_length) * units.nt
+	replication_length = np.ceil(0.5*genome_length) * units.nt
 
-	# Generate arrays specifying appropriate replication conditions
-	sequenceIdx, sequenceLength, replicationRound, chromosomeIndex = determineChromosomeState(C, D, tau, replication_length)
-
-	# Determine the number of OriC's currently present in the cell
-	numOric = determineNumOriC(C, D, tau)
-	oriC = uniqueMolCntr.objectsNew('originOfReplication', numOric)
+	# Generate arrays specifying appropriate initial replication conditions
+	n_oric, sequenceIdx, sequenceLength, replicationRound, chromosomeIndex = determineChromosomeState(C, D, tau, replication_length)
+	uniqueMolCntr.objectsNew('originOfReplication', n_oric)
 
 	# Check that sequenceIdx, sequenceLength, replicationRound, and
-	# chromosomeIndex are equal length, numOric should be half the
-	# size (4 DNAP/fork, only 2 oriC/fork)
-	assert(len(sequenceIdx) == len(sequenceLength) == len(replicationRound) == len(chromosomeIndex) == 4*(numOric - 1))
+	# chromosomeIndex all have lengths equal to the number of polymerases.
+	assert len(sequenceIdx) == len(sequenceLength) == len(replicationRound) == len(chromosomeIndex) == 4*(n_oric - 1)
 
-	# Return if no replication is occuring at all
-	if(len(sequenceIdx) == 0):
+	# Return if no replication is occurring at all
+	if len(sequenceIdx) == 0:
 		return
 
-	## Update polymerases mass to account for already completed DNA
+	# Update mass to account for DNA strands that have already been elongated
 	# Determine the sequences of already-replicated DNA
 	sequences = sim_data.process.replication.replication_sequences
 	sequenceElongations = np.array(sequenceLength, dtype=np.int64)
 	massIncreaseDna = computeMassIncrease(
-			np.tile(sequences,(len(sequenceIdx) // 4,1)),
+			np.tile(sequences, (len(sequenceIdx)//4, 1)),
 			sequenceElongations,
 			sim_data.process.replication.replicationMonomerWeights.asNumber(units.fg)
 			)
@@ -444,85 +442,70 @@ def setDaughterInitialConditions(sim, sim_data):
 
 def determineChromosomeState(C, D, tau, replication_length):
 	"""
-	determineChromosomeState
+	Calculates the number and position of replicating DNA polymerases at the
+	beginning of the cell cycle.
 
-	Purpose: calculates the number and position of replicating DNA polymerases
-			 at the beginning of the cell cycle.
+	Inputs
+	--------
+	- C: the C period of the cell, the length of time between replication
+	initiation and replication completion.
+	- D: the D period of the cell, the length of time between completing
+	replication of the chromosome and division of the cell.
+	- tau: the doubling time of the cell
+	- replication_length: the amount of DNA to be replicated per fork, usually
+	half of the genome, in base-pairs
 
-	Inputs: C  - the C period of the cell, the length of time between
-			replication initiation and replication completion.
-			D  - the D period of the cell, the length of time between
-			completing replication of the chromosome and division of the cell.
-			tau - the doubling time of the cell
-			replication_length - the amount of DNA to be replicated per fork,
-			usually half of the genome, in base-pairs
+	Returns
+	--------
+	- n_oric: the number of OriC's in the cell at initiation.
+	- sequenceIdx: an index for each of the four types/directions of DNA
+	replication - leading and lagging strand of the forward and the reverse
+	fork = 4 total. This vector is always simply [0,1,2,3] repeated once for
+	each replication event. i.e. for three active replication events (6 forks,
+	12 polymerases) sequenceIdx = [0,1,2,3,0,1,2,3,0,1,2,3]
+	- sequenceLength: the position in the genome that each polymerase
+	referenced in sequenceIdx has reached, in base-pairs. This is handled such
+	that even though in reality some polymerases are replicating in different
+	directions,	all values here are calculated as though each starts at 0 and
+	goes up to the total number of base-pairs to be replicated.
+	- replicationRound: an integer stating in which replication round the
+	polymerase referenced by sequenceIdx has been initiated. Each time all
+	origins of replication in the cell fire, a new replication round has
+	started. This array is integer-valued, and counts from 0 (the oldest round)
+	up to n (the most recent round).
+	- chromosomeIndex: indicator variable for which daughter cell should
+	inherit which polymerases at division. This array is used to distinguish
+	between polymerases within a single replicationRound, not between rounds.
+	Within each round, half of the polymerases should have chromosomeIndex = 0,
+	and half should have chromosomeIndex = 1. This should be contiguous halves,
+	i.e. [0,0,0,0,1,1,1,1], NOT interspersed as in [0,1,0,1,0,1,0,1]. The
+	half-and-half rule is excepted for the oldest replication round with only 4
+	polymerases. In this case, chromosomeIndex doesn't matter/is effectively
+	NaN, but is set to all 0's to prevent conceptually dividing a single
+	chromosome between two daughter cells.
 
-	Outputs: a tuple of vectors for input into the dnaPoly.attrIs() function
-			of the initializeReplication() function in initial_conditions.py.
-			These  vectors are, in order:
-			sequenceIdx - an index for each of the four types/directions of DNA
-						replication - leading and lagging strand of the forward
-						and the reverse fork = 4 total. This vector is always
-						simply [0,1,2,3] repeated once for each replication
-						event. Ie for three active replication events (6 forks,
-						12 polymerases) sequenceIdx = [0,1,2,3,0,1,2,3,0,1,2,3]
-			sequenceLength - the position in the genome that each polymerase
-						referenced in sequenceIdx has reached, in base-pairs.
-						This is handled such that even though in reality some
-						polymerases are replicating in different directions all
-						inputs here are as though each starts at 0 and goes up
-						the the number of base-pairs to be replicated.
-			replicationRound - a unique integer stating in which replication
-						generation the polymerase referenced by sequenceIdx.
-						Each time all origins of replication in the cell fire,
-						a new replication generation has started. This array
-						is integer-valued, and counts from 0 (the oldest
-						generation) up to n (the most recent initiation) event.
-			chromosomeIndex - indicator variable for which daughter cell
-						should inherit which polymerase at division. This
-						array is only relevant to draw distinctions within a
-						generaation of replicationRound, now between rounds.
-						Within each generation in replicationRound (run of
-						numbers with the same value), half should have
-						chromosomeIndex = 0, and half chromosomeIndex
-						= 1. This should be contiguous halves, ie
-						[0,0,0,0,1,1,1,1], NOT interspersed like
-						[0,1,0,1,0,1,0,1]. The half-and-half rule is excepted
-						for any replication generation with only 4 polymerases
-						(the oldest replication generation should be the only
-						one	with fewer than 8 polymerases). In this case
-						chromosomeIndex doesn't matter/is effectively NaN,
-						but is set to all 0's to prevent conceptually dividing
-						a single chromosome between two daughter cells.
-
-	Notes: if NO polymerases are active at the start of the cell cycle,
-			equivalent to the C + D periods being shorter than the doubling
-			rate tau, then this function returns empty lists. dnaPoly.attrIs()
-			should not be run in this case, as no DNA replication will be
-			underway.
+	Notes
+	--------
+	If NO polymerases are active at the start of the cell cycle, equivalent to
+	the C + D periods being shorter than the doubling rate tau, then this
+	function returns empty lists. dnaPoly.attrIs() should not be run in this
+	case, as no DNA replication will be underway.
 	"""
 
-	## Error check inputs
-
-	# Check that all inputs have units
-	assert (units.hasUnit(C)), 'C must have units'
-	assert (units.hasUnit(D)), 'D must have units'
-	assert (units.hasUnit(tau)), 'tau must have units'
-	assert (units.hasUnit(replication_length)) and (units.getUnit(replication_length).strUnit() == units.nt.strUnit()), 'replication_length must have units of units.nt.'
-
 	# All inputs must be positive numbers
-	assert (C.asNumber(units.min) >= 0), "C value can't be negative."
-	assert (D.asNumber(units.min) >= 0), "D value can't be negative."
-	assert (tau.asNumber(units.min) >= 0), "tau value can't be negative."
-	assert (replication_length.asNumber(units.nt) >= 0), "replication_length value can't be negative."
+	assert C.asNumber(units.min) >= 0, "C value can't be negative."
+	assert D.asNumber(units.min) >= 0, "D value can't be negative."
+	assert tau.asNumber(units.min) >= 0, "tau value can't be negative."
+	assert replication_length.asNumber(units.nt) >= 0, "replication_length value can't be negative."
 
-	# Require that D is shorter than tau - time between completing replication
-	# and dividing must be shorter than the time between two divisions.
-	assert (D.asNumber(units.min) < tau.asNumber(units.min)), 'The D period must be shorter than the doubling time tau.'
+	# Require that D is shorter than tau - time between completing DNA
+	# replication and cell division must be shorter than the time between two
+	# cell divisions.
+	assert D.asNumber(units.min) < tau.asNumber(units.min), 'The D period must be shorter than the doubling time tau.'
 
-
-	# Number active replication generations (can be many initiations per gen.)
-	limit = np.floor((C.asNumber(units.min) + D.asNumber(units.min))/tau.asNumber(units.min))
+	# Calculate the number of active replication rounds and oriC's
+	n_round = np.floor((C.asNumber(units.min) + D.asNumber(units.min))/tau.asNumber(units.min))
+	n_oric = int(2**n_round)
 
 	# Initialize arrays to be returned
 	sequenceIdx = []
@@ -530,65 +513,43 @@ def determineChromosomeState(C, D, tau, replication_length):
 	replicationRound = []
 	chromosomeIndex = []
 
-	# Loop through the generations of replication which are active (if limit = 0
-	# skips loop entirely --> no active replication generations)
-	n = 1;
-	while n <= limit:
-		# Determine at what base each strand of a given replication event should start
-		# Replication forks should be at base (1 - (n*tau - D)/(C))(basepairs to be replicated)
+	# Loop through active replication rounds, starting from the oldest round.
+	# If n_round = 0 skip loop entirely - no active replication round.
+	for n in xrange(n_round):
+		# Determine at which location (base) of the chromosome the replication
+		# forks should be initialized to
+		rel_location = 1 - (((n + 1)*tau - D)/C)
+		rel_location = units.convertNoUnitToNumber(rel_location)
+		fork_location = np.floor(rel_location*(replication_length.asNumber(units.nt)))
 
-		ratio = (1 - ((n*tau - D)/(C)))
-		ratio = units.convertNoUnitToNumber(ratio)
-		fork_location = np.floor(ratio*(replication_length.asNumber(units.nt)))
+		# Add 2^n initiation events per round. A single initiation event
+		# generates two replication forks and four elongating strands
+		# (polymerases).
+		n_event = 2**n
 
-		# Add 2^(n-1) replication events (two forks, four strands per inintiaion event)
-		num_events = 2 ** (n-1)
-		# sequenceIdx refers to the type of elongation - ie forward and
-		# reverse, lagging and leading strands.
-		sequenceIdx += [0,1,2,3]*num_events
-		# sequenceLength refers to how far along in the replication process
-		# this event already is - at what basepair currently. All four are
-		# assumed to be equally far along.
-		sequenceLength += [fork_location]*4*num_events
-		# replicationRound is defined the same way as n - each time a
-		# replication round starts, all origins in the cell fire, and are then
-		# of the same generation and round number
-		replicationRound += [(n-1)]*4*num_events
-		# WITHIN each round, chromosomeIndex uniquely identifies individual
-		# origin initaion points. Loop through each intiation event in this
-		# generation (2 forks, 4 polymerases each), assign it an increaing,
-		# unique number, starting at zero.
-		chromosomeIndex += [0]*2*num_events + [0]*2*num_events
+		# sequenceIdx refers to the type of the strands that the polymerase is
+		# elongating - i.e. forward and reverse, lagging and leading strands.
+		sequenceIdx += [0, 1, 2, 3] * n_event
 
-		n += 1
+		# sequenceLength refers to how far along the chromosome the polymerases
+		# have elongated to. All four are assumed to have elongated up to the
+		# replication fork.
+		sequenceLength += [fork_location] * (4*n_event)
 
-	# The first replication generation should not be divided, so set all values
-	# to 0 (effectively NaN, the first four values are not used)
-	if len(chromosomeIndex):
-		chromosomeIndex[:4] = [0,0,0,0]
+		# replicationRound is the index of the replication round that the
+		# polymerases belong to - for each replication round, all origins in
+		# the cell are fired simultaneously, and the initiated polymerases
+		# share the same round index
+		replicationRound += [n] * (4*n_event)
 
-	return (sequenceIdx, sequenceLength, replicationRound, chromosomeIndex)
+		# chromosomeIndex indicates which daughter cell will inherit the
+		# polymerase. For a given round, the value for half of the polymerases
+		# are set to 0, and the rest to 1.
+		chromosomeIndex += [0] * (2*n_event) + [1] * (2*n_event)
 
+	# The polymerases in the oldest round should not be divided - set all
+	# values to 0 (effectively NaN, the first four values are not used)
+	if len(chromosomeIndex) != 0:
+		chromosomeIndex[:4] = [0, 0, 0, 0]
 
-
-def determineNumOriC(C, D, tau):
-	"""
-	determineNumOriC
-
-	Purpose: calculates the number of OriC's in a cell upon initiation, determined by the replication state of the chromosome.
-
-	Inputs: C  - the C period of the cell, the length of time between
-			replication initiation and replication completion.
-			D  - the D period of the cell, the length of time between
-			completing replication of the chromosome and division of the cell.
-			tau - the doubling time of the cell
-			replication_length - the amount of DNA to be replicated per fork,
-			usually half of the genome, in base-pairs
-
-	Outputs: the number of OriC's in the cell at initiation.
-	"""
-
-	# Number active replication generations (can be many initiations per gen.)
-	total_active_initiations = np.floor((C.asNumber() + D.asNumber())/tau.asNumber())
-
-	return int(2 ** (total_active_initiations))
+	return n_oric, sequenceIdx, sequenceLength, replicationRound, chromosomeIndex
