a3b07d1b7911c13cb58f9e0524eb5d70ef82870e
John Mason <jmason42@stanford.edu>
2018-06-28 16:45:35 -0700

    updated runscript with the parallel fitter option and the full number of sims (8x8)

--- runscripts/run_limited_fitting.sh
+++ runscripts/run_limited_fitting.sh
@@ -1,29 +1,28 @@
 
-# TODO: include PARALLEL_FITTER=1 option
-# I've tried this once already but Fireworks/Slurm only allocated 1 cpu for
-# the fitting task, which made it run egregiously slow (parallelization
-# overhead is considerable).
-
 echo Submitting job 1 - fit all
 
 DISABLE_RIBOSOME_CAPACITY_FITTING=0 DISABLE_RNAPOLY_CAPACITY_FITTING=0 \
-SINGLE_DAUGHTERS=1 N_GENS=8 N_INIT_SIMS=1 DESC="Both fit" \
+PARALLEL_FITTER=1 \
+SINGLE_DAUGHTERS=1 N_GENS=8 N_INIT_SIMS=8 DESC="Both fit" \
 python runscripts/fw_queue.py
 
 echo Submitting job 2 - fit only ribosomes
 
 DISABLE_RIBOSOME_CAPACITY_FITTING=0 DISABLE_RNAPOLY_CAPACITY_FITTING=1 \
-SINGLE_DAUGHTERS=1 N_GENS=8 N_INIT_SIMS=1 DESC="Ribosomes fit" \
+PARALLEL_FITTER=1 \
+SINGLE_DAUGHTERS=1 N_GENS=8 N_INIT_SIMS=8 DESC="Ribosomes fit" \
 python runscripts/fw_queue.py
 
 echo Submitting job 3 - fit only RNA polymerases
 
 DISABLE_RIBOSOME_CAPACITY_FITTING=1 DISABLE_RNAPOLY_CAPACITY_FITTING=0 \
-SINGLE_DAUGHTERS=1 N_GENS=8 N_INIT_SIMS=1 DESC="RNApoly fit" \
+PARALLEL_FITTER=1 \
+SINGLE_DAUGHTERS=1 N_GENS=8 N_INIT_SIMS=8 DESC="RNApoly fit" \
 python runscripts/fw_queue.py
 
 echo Submitting job 4 - fit neither ribosomes nor RNA polymerases
 
 DISABLE_RIBOSOME_CAPACITY_FITTING=1 DISABLE_RNAPOLY_CAPACITY_FITTING=1 \
-SINGLE_DAUGHTERS=1 N_GENS=8 N_INIT_SIMS=1 DESC="Neither fit" \
+PARALLEL_FITTER=1 \
+SINGLE_DAUGHTERS=1 N_GENS=8 N_INIT_SIMS=8 DESC="Neither fit" \
 python runscripts/fw_queue.py
