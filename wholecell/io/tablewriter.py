08a9af73e899238b85ba638f6492276e24ad2988
John Mason <jmason42@stanford.edu>
2018-07-24 12:24:45 -0700

    Doc string for the TableWriter class

--- wholecell/io/tablewriter.py
+++ wholecell/io/tablewriter.py
@@ -84,6 +84,81 @@ class _Column(object):
 
 
 class TableWriter(object):
+	"""
+	Generic live output writer for NumPy ndarrays.
+
+	NumPy can save and load arrays to disk.  This class provides a convenient
+	interface to repeated appending of NumPy array data to an output file,
+	which can be loaded one entry at a time or all at once via the companion
+	class, TableReader.
+
+	Output file structure:
+
+	<root directory> : Root path, provided by during instantiation.
+		/attributes : Directory for saved JSON data, provided by the user.
+			/<attribute name> : A JSON file.
+		/columns : Directory for the main output streams ("columns").
+			/<column name> : Directory for a specific column.
+				data : Contains the JSON-serialized format specification for
+					the Numpy array dtype and appended binary data from NumPy
+					ndarrays.
+				offsets : A list of integers (one per line) corresponding to
+					the associated byte offset for each entry in the column.
+		/metadata : Directory for saving any critical internals for TableWriter
+				and TableReader.
+			version : An integer representing the "version number" of the data
+				format specification.
+
+	Parameters
+	----------
+	path : str
+		Path to the output location (a directory) where data will be saved.  If
+			 he directory already exists, no error will be thrown.
+
+	See also
+	--------
+	whoelcell.io.tablereader.TableReader
+
+	Notes
+	-----
+
+	Data written to columns can be of fixed or variable size.  If fixed, the
+	output can be read in as a single, higher dimensional array by TableReader.
+	Otherwise output will need to be handled one line at a time.
+
+	0D and 1D array writing is supported.  Higher dimensions are outside of
+	spec, and will likely be flattened when read by TableReader.
+	TODO (John): throw an error for > 1D?
+
+	Both simple and structured ndarray dtypes are supported.  Structured arrays
+	are a good way to work around the dimensional limitations.
+
+	NumPy object arrays (e.g. arrays not of one or more standard dtypes) are
+	not supported.  They might work, but under the hood will rely on pickling
+	for saving and loading, which will be terribly slow.
+
+	TODO (John): Consider moving the Numpy dtype format specification out of
+		the 'data' file and into its own file.  This was done originally to
+		keep the format tightly associated with the data, improving portability
+		and consistency with the np.save implementation.  However it leads to
+		logical complications in saving and loading, and it mixes JSON string
+		data with array binary data in one file.
+
+	TODO (John): Drop the 'version' idea as well as the 'metadata' directory.
+		We so rarely try to run new scripts on old data (or vice-versa) that
+		I'm not sure this needs to exist or be maintained.
+
+	TODO (John): Move the _Column class into the TableWriter namespace.
+		There's no reason for it to be available at the module level.
+
+	TODO (John): Instead of saving many files and sub-directories under one
+		directory, save everything to an uncompressed archive.  Built-in module
+		zipfile seems like a good option; np.savez uses it.
+
+	TODO (John): Unit tests.
+
+	"""
+
 	def __init__(self, path):
 
 		dirMetadata = filepath.makedirs(path, DIR_METADATA)
