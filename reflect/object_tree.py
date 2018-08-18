f9a08bbdf63907879d048bacbe6de15edcb68ec8
Ryan Spangler <spanglry@stanford.edu>
2018-08-17 17:53:47 -0700

    transform object into tree of dictionaries

---
+++ reflect/object_tree.py
@@ -0,0 +1,100 @@
+import re
+import unum
+import sympy
+import types
+import Bio.Seq
+import numbers
+import functools
+import collections
+import numpy as np
+from sympy.matrices import eye
+import wholecell.utils.unit_struct_array
+
+leaf_types = (
+	set,
+	str,
+	unicode,
+	np.int_,
+	np.intc,
+	np.intp,
+	np.int8,
+	np.bool_,
+	np.dtype,
+	np.int16,
+	np.int32,
+	np.int64,
+	np.uint8,
+	np.uint16,
+	np.uint32,
+	np.uint64,
+	np.float_,
+	np.float16,
+	np.float32,
+	np.float64,
+	np.ndarray,
+	np.complex_,
+	np.complex64,
+	np.complex128,
+	unum.Unum,
+	Bio.Seq.Seq,
+	sympy.Basic,
+	numbers.Number,
+	functools.partial,
+	types.FunctionType,
+	type(eye(1)),
+	wholecell.utils.unit_struct_array.UnitStructArray)
+
+def is_hidden(attr):
+	"""
+	Predicate to determine if a given string represents an attribute or function we want to ignore.
+	In our case this means that it is flanked by double underscores.
+	"""
+	return re.search(r'^__.*__$', attr)
+
+def is_leaf(value, leaves=leaf_types):
+	"""
+	Predicate to determine if we have reached the end of how deep we want to traverse through 
+	the object tree. In this case this is true if the object is `callable()` or if it is an 
+	instance of one of our leaf types.
+	"""
+	return callable(value) or isinstance(value, leaves)
+
+def object_tree(o, path='', debug=None):
+	"""
+	Given an object, exhaustively traverse down all attributes it contains until leaves are
+	reached and convert everything found into a dictionary.
+
+	The resulting dictionary will mirror the structure of the original object, but instead of 
+	attributes with values it will be a dictionary where the keys are the attribute names. 
+	The type of the dictionarified object will be encoded under the key `!type` which is assumed
+	to not be in conflict with any other attributes. The result should aid in serialization and
+	deserialization of the object and is intended to be a translation of a pickled object.
+
+	Args:
+		o (object): The object to inspect. 
+		path (optional str): The root path of this object tree. This will be built upon for 
+			each child of the current object found and reported in a value is provided for `debug`.
+		debug (optional str): If provided, prints paths of the attributes encountered. If the
+			value is 'ALL', it will print every path. If the value is 'CALLABLE', it will only 
+			print methods and functions it finds.
+	"""
+
+	if debug == 'ALL':
+		print(path)
+
+	if is_leaf(o):
+		if callable(o) and (debug == 'CALLABLE'):
+			print(path)
+		return o
+	elif isinstance(o, collections.Mapping):
+		return {key: object_tree(o[key], "{}['{}']".format(path, key), debug) for key in o.keys()}
+	elif isinstance(o, collections.Sequence):
+		return [object_tree(subo, "{}[{}]".format(path, index), debug) for index, subo in enumerate(o)]
+	else:
+		attrs = dir(o)
+		tree = {attr: object_tree(getattr(o, attr), "{}.{}".format(path, attr), debug)
+		 for attr in attrs
+		 if not is_hidden(attr)}
+		tree['!type'] = type(o)
+
+		return tree
