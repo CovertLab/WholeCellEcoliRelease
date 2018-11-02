33af6ba61735fc908fba1c8ffd0fdc7dd7e813a7
Ryan Spangler <spanglry@stanford.edu>
2018-11-02 14:24:36 -0700

    removed analyses tagged for deletion

--- models/ecoli/analysis/variant/__init__.py
+++ models/ecoli/analysis/variant/__init__.py
@@ -2,14 +2,7 @@
 # Tip: Edit this during development to run ones you're working on first.
 ACTIVE = [
 	"adder_sizer.py",
-	"conditionalFlux.py",
-	"distribution_cell_division_time.py",
-	"distribution_division_fluxome_proteome.py",
-	"distribution_fluxome_correlation.py",
-	"distribution_proteome_correlation.py",
 	"doubling_time_histogram.py",
-	"parameterShuffle.py",
-	"parameterShuffleSplom.py",
 	"meneSensitivity.py",
 	"growth_condition_comparison_validation.py",
 	"growthConditionComparison.py",
