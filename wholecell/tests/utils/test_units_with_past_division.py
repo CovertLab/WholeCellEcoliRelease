575cdf7d7db1adeab966b930d4e252feec190f0e
Jerry Morrison <1fish2@users.noreply.github.com>
2019-01-25 00:44:40 -0800

    Add a test case for non-future __rdiv__
    And fix docstring copy/paste-o.

--- wholecell/tests/utils/test_units_with_past_division.py
+++ wholecell/tests/utils/test_units_with_past_division.py
@@ -6,14 +6,24 @@ import nose.plugins.attrib as noseAttrib
 
 from wholecell.utils import units
 
+PY2 = sys.version_info.major < 3
+
 
 @noseAttrib.attr('smalltest', 'units')
 class Test_units_with_past_division(unittest.TestCase):
 
 	def test_slash_div(self):
-		"""Test that Python 2 `/` WITHOUT __future__.division does __truediv__"""
+		"""Test Python 2 `/` *WITHOUT* __future__.division."""
 		x = 1 * units.s
 		y = 100 * units.s
 		quotient = (x / y).asNumber()
-		expected = 0 if sys.version_info.major < 3 else 0.01
+		expected = 0 if PY2 else 0.01
+		self.assertEqual(expected, quotient)
+
+	def test_slash_rdiv(self):
+		"""Test Python 2 __rdiv__ `/` *WITHOUT* __future__.division."""
+		x = 1
+		y = 100 * units.s
+		quotient = (x / y).asNumber()
+		expected = 0 if PY2 else 0.01
 		self.assertEqual(expected, quotient)
