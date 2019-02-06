f4a1623f1939ee58c7ef625c2af3ef4e279e36c9
Ryan Spangler <spanglry@stanford.edu>
2019-02-06 13:34:18 -0800

    add comment about turning off future division

--- wholecell/utils/units.py
+++ wholecell/utils/units.py
@@ -41,6 +41,8 @@ def __rtruediv__(self, other):
 # #244 workaround: Monkey patch Unum if it still has the broken implementation.
 # The test also ensures this only patches it once.
 # For some reason, `is` won't work here.
+
+# Turn off for now, see https://github.com/CovertLab/wcEcoli/issues/433
 if Unum.__truediv__ == Unum.__div__ and False:
 	Unum.__truediv__ = __truediv__
 	Unum.__rtruediv__ = __rtruediv__
