e2f07cb02b85b8a8102c34ee7b5bb75c0a0527c9
Eran Agmon <agmon.eran@gmail.com>
2019-04-04 10:24:56 -0700

    Make media (#480)
    * MakeMedia class has functions that makes media by combining two medias at different volumes, or adding ingredients to a base media
    
    * begin making media in external_state
    
    * begin integration of make_media with external_state in wcEcoli
    
    * mix all media specified in media_recipes, remove pre-specified media
    
    * clean up
    
    * update readme
    
    * simplify combine media calculation
    
    * make ingredients into dictionary with {mol_id: {weight:w, counts:c, volume,v}, to allow for adding counts rather than just weight
    
    * responding to @1fish2 comments
    
    * simplify loop in add_ingredients()
    
    * replace another 2 float(inf)
    
    * clean up media_recipes.tsv -- no more NaNs needed
    
    * responding to @prismofeverything comments

---
+++ environment/condition/make_media.py
@@ -0,0 +1,202 @@
+'''
+Functions for making media
+
+'''
+
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+
+from wholecell.utils import units
+
+# Raw data class
+from reconstruction.ecoli.knowledge_base_raw import KnowledgeBaseEcoli
+
+INF = float("inf")
+NEG_INF = float("-inf")
+
+COUNTS_UNITS = units.mmol
+VOLUME_UNITS = units.L
+MASS_UNITS = units.g
+CONC_UNITS = COUNTS_UNITS / VOLUME_UNITS
+
+
+class AddIngredientsError(Exception):
+	pass
+
+class Media(object):
+	'''
+	A media object is a factory for making new media by either combining
+	two saved media at different volumes (with self.combine_media(), or
+	adding ingredients to a saved media (with self.add_ingredients()).
+	Ingredients can be added by either specifying their weight (in grams)
+	or the counts (in mmol) in addition to the volume. The new media dicts
+	are returned to the caller, and are not saved in this object. A media
+	object holds dicts about stock media in ```self.stock_media``` and the
+	formula weight of environmental molecules in
+	```self.environment_molecules_fw```, which is needed for mixing in
+	ingredients at weights.
+	'''
+
+	def __init__(self):
+
+		raw_data = KnowledgeBaseEcoli()
+
+		# get dicts from knowledge base
+		self.environment_molecules_fw = self._get_environment_molecules_fw(raw_data)
+		self.stock_media = self._get_stock_media(raw_data)
+
+	def _get_environment_molecules_fw(self, raw_data):
+		'''get formula weight (units.g / units.mol) for all environmental molecules'''
+
+		environment_molecules_fw = {}
+		for row in raw_data.condition.environment_molecules:
+			mol = row["molecule id"]
+			fw = row["formula weight"]
+			if fw == 'None':
+				environment_molecules_fw[mol] = None
+			else:
+				environment_molecules_fw[mol] = float(fw) * (units.g / units.mol)
+
+		return environment_molecules_fw
+
+	def _get_stock_media(self, raw_data):
+		'''load all stock media'''
+
+		stock_media = {}
+		for label in vars(raw_data.condition.media):
+			# initiate all molecules with 0 concentrations
+			stock_media[label] = {
+				row["molecule id"]: 0.0 * CONC_UNITS
+				for row in raw_data.condition.environment_molecules}
+
+			# get non-zero concentrations (assuming units.mmol / units.L)
+			molecule_concentrations = getattr(raw_data.condition.media, label)
+
+			environment_non_zero_dict = {
+				row["molecule id"]: row["concentration"]
+				for row in molecule_concentrations}
+
+			# update environment_dict with non zero concentrations
+			stock_media[label].update(environment_non_zero_dict)
+
+		return stock_media
+
+	def combine_media(self, base_media, base_media_volume, mix_media, mix_media_volume):
+		'''
+		Combines two medias and returns a new media
+
+		Args:
+			base_media, mix_media (dict): dicts with {molecule_id: concentration}
+			base_media_volume, mix_media_volume (unum): the volumes of base_media and mix_media (floats) with a volume units (i.e. units.L)
+
+		Returns:
+			new_media (dict): {molecule_id: concentrations}
+		'''
+
+		# intialize new_media
+		new_media = {mol_id: 0.0 * CONC_UNITS for mol_id, conc in base_media.iteritems()}
+
+		# get new_media volume
+		new_volume = base_media_volume + mix_media_volume
+
+		for mol_id, base_conc in base_media.iteritems():
+			mix_conc = mix_media[mol_id]
+
+			if base_conc.asNumber() == INF or mix_conc.asNumber() == INF:
+				new_media[mol_id] = INF * CONC_UNITS
+			else:
+				base_counts = base_conc * base_media_volume
+				mix_counts = mix_conc * mix_media_volume
+				new_counts = base_counts + mix_counts
+				new_conc = new_counts / new_volume
+
+				# update media
+				new_media[mol_id] = new_conc
+
+		return new_media
+
+	def add_ingredients(self, base_media, base_media_volume, ingredients):
+		'''
+		Combines ingredients to existing media to make new media.
+
+		Args:
+			base_media (dict): {molecule_id: concentrations}
+			base_media_volume:
+			ingredients (dict): keys are ingredient ids, values are dicts with weight, counts, volume.
+				Only one of weights (in g) or counts (in mmol) is needed; if both are specified, it will use weight.
+				If weight or counts is Infinity, the new concentration is set to inf. If the weight or counts is -Infinity,
+				the new concentration is set to 0.
+				Example format of ingredients:
+					{mol_id_1: {'weight': 1.78 * units.g, 'volume': 0.025 * units.L),
+					mol_id_2: {'counts': 0.2 * units.mmol, 'volume': 0.1 * units.L),
+					}
+
+		Returns:
+			new_media (dict): {molecule_id: concentrations}
+		'''
+
+		# intialize new_media
+		new_media = {mol_id: 0.0 * CONC_UNITS for mol_id, conc in base_media.iteritems()}
+
+		# get new_media volume
+		ingredients_volume = 0 * VOLUME_UNITS
+		for quantities in ingredients.itervalues():
+			ingredients_volume += quantities['volume']
+		new_volume = base_media_volume + ingredients_volume
+
+		# get new_media concentrations from mixing ingredients
+		for mol_id, base_conc in base_media.iteritems():
+
+			if mol_id in ingredients:
+				base_counts = base_conc * base_media_volume
+				quantities = ingredients[mol_id]
+				weight = quantities.get('weight', None)
+				mix_counts = quantities.get('counts', None)
+
+				# calculate mix_counts from weight.
+				# This will overwrite added counts if those were specified
+				if weight is not None:
+					if weight.asNumber() == INF:
+						mix_counts = INF * COUNTS_UNITS
+					elif weight.asNumber() == NEG_INF:
+						mix_counts = NEG_INF * COUNTS_UNITS
+					elif weight.asNumber() >= 0:
+						if self.environment_molecules_fw[mol_id] is not None:
+							fw = self.environment_molecules_fw[mol_id]
+							mix_counts = weight / fw
+						else:
+							raise AddIngredientsError(
+								"No fw defined for {} in environment_molecules.tsv".format(mol_id)
+							)
+					else:
+						raise AddIngredientsError(
+							"Negative weight given for {}".format(mol_id)
+						)
+				elif mix_counts is None:
+					raise AddIngredientsError(
+						"No added added weight or counts for {}".format(mol_id)
+					)
+
+				# get new concentration
+				# make infinite concentration of ingredient if mix_counts is Infinity
+				if mix_counts.asNumber() == INF:
+					new_media[mol_id] = INF * CONC_UNITS
+
+				# remove ingredient from media if mix_counts is -Infinity
+				# this will override infinite concentrations in base_media
+				elif mix_counts.asNumber() == NEG_INF:
+					new_media[mol_id] = 0.0 * CONC_UNITS
+
+				else:
+					new_counts = base_counts + mix_counts
+					new_conc = new_counts / new_volume
+					new_media[mol_id] = new_conc
+
+			# if mol_id is not in ingredients, dilute its concentration in new_media
+			else:
+				base_counts = base_conc * base_media_volume
+				new_conc = base_counts / new_volume
+				new_media[mol_id] = new_conc
+
+		return new_media
--- reconstruction/ecoli/dataclasses/state/external_state.py
+++ reconstruction/ecoli/dataclasses/state/external_state.py
@@ -15,15 +15,16 @@ Initializes the environment using conditions and time series from raw_data.
 @organization: Covert Lab, Department of Bioengineering, Stanford University
 """
 
-from __future__ import division
+from __future__ import absolute_import, division, print_function
 
 from wholecell.utils import units
-from wholecell.utils.unit_struct_array import UnitStructArray
-
 from reconstruction.ecoli.dataclasses.state.environment import Environment
+from environment.condition.make_media import Media
 
-import re
-import numpy as np
+COUNTS_UNITS = units.mmol
+VOLUME_UNITS = units.L
+MASS_UNITS = units.g
+CONC_UNITS = COUNTS_UNITS / VOLUME_UNITS
 
 class ExternalState(object):
 	""" External State """
@@ -37,7 +38,6 @@ class ExternalState(object):
 
 		# create a dictionary of all nutrient time series
 		self.environment.nutrients_time_series = {}
-
 		for label in dir(raw_data.condition.timelines):
 			if label.startswith("__"):
 				continue
@@ -49,21 +49,45 @@ class ExternalState(object):
 					row["media"].encode("utf-8"),
 					))
 
-		# create a dictionary with all saved environments, including molecules of concentration == 0
+		# create a dictionary with all media conditions specified by media_recipes
+		make_media = Media()
 		self.environment.environment_dict = {}
-		for label in dir(raw_data.condition.media):
-			if label.startswith("__"):
-				continue
-			self.environment.environment_dict[label] = {}
+		for row in raw_data.condition.media_recipes:
+			new_media_id = row["media id"]
+			base_id = row["base media"]
+			added_media_id = row["added media"]
+			ingredient_ids = row["ingredients"]
+			base_media = make_media.stock_media[base_id]
 
-			#initiate all molecules with 0 concentrations
-			for row in raw_data.condition.environment_molecules:
-				self.environment.environment_dict[label].update({row["molecule id"]: 0})
+			if added_media_id:
+				added_media = make_media.stock_media[added_media_id]
+				base_vol = row["base media volume"]
+				added_vol = row["added media volume"]
+				new_media = make_media.combine_media(base_media, base_vol, added_media, added_vol)
+			elif ingredient_ids:
+				base_vol = row.get("base media volume", 0 * units.L)
+				added_weight = row.get("ingredients weight", None)
+				added_counts = row.get("ingredients counts", None)
+				added_vol = row.get("ingredients volume")  # the row is a list with units.L, even an empty list is read.
 
-			# update non-zero concentrations, remove units
-			molecule_concentrations = getattr(raw_data.condition.media, label)
-			for row in molecule_concentrations:
-				self.environment.environment_dict[label].update({row["molecule id"]: row["concentration"].asNumber()})
+				ingredients = {ingred_id: {} for ingred_id in ingredient_ids}
+				for index, ingred_id in enumerate(ingredient_ids):
+					if added_weight:
+						ingredients[ingred_id]['weight'] = added_weight[index]
+					if added_counts:
+						ingredients[ingred_id]['counts'] = added_counts[index]
+					if added_vol:
+						ingredients[ingred_id]['volume'] = added_vol[index]
+					else:
+						ingredients[ingred_id]['volume'] = 0 * units.L
+
+				new_media = make_media.add_ingredients(base_media, base_vol, ingredients)
+			else:
+				new_media = base_media
+
+			# remove concentration units, setting at CONC_UNITS
+			unitless_new_media = {mol: conc.asNumber(CONC_UNITS) for mol, conc in new_media.iteritems()}
+			self.environment.environment_dict[new_media_id] = unitless_new_media
 
 		# make mapping from external molecule to exchange molecule
 		self.environment.env_to_exchange_map = {
