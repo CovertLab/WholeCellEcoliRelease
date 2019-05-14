888d7131151725d59f421bd22d6f36e9b719bcab
Ryan Spangler <spanglry@stanford.edu>
2019-05-13 18:20:11 -0700

    added conditionals and multiplicities

--- runscripts/fireworks/fw_queue.py
+++ runscripts/fireworks/fw_queue.py
@@ -101,51 +101,50 @@ InitRawValidationData (fw_raw_validation_data)
 * InitValidationData
 
 InitValidationData (fw_validation_data)
-* CompressRawValidationData
-* CompressRawData
+* CompressRawValidationData if COMPRESS_OUTPUT
+* CompressRawData if COMPRESS_OUTPUT
 
 FitSimData (fw_calculate_sim_data)
-* CompressRawData
-* VariantSimData
+* CompressRawData if COMPRESS_OUTPUT
+* VariantSimData * VARIANTS_TO_RUN
 
 VariantSimData (fw_this_variant_sim_data)
-* CompressFitSimData
-* Simulation/SimulationDaughter
+* CompressFitSimData if COMPRESS_OUTPUT
+* Simulation/SimulationDaughter * N_INIT_SIMS * N_GENS
 
 Simulation/SimulationDaughter (fw_this_variant_this_gen_this_sim)
-* SimulationDaughter
-* AnalysisCohort
-* AnalysisVariant
-* AnalysisMultiGen
-* BuildCausalityNetwork
+* SimulationDaughter (* 2 if not SINGLE_DAUGHTERS) if N_GENS > 1
+* AnalysisSingle if RUN_AGGREGATE_ANALYSIS
+* AnalysisCohort if RUN_AGGREGATE_ANALYSIS and GEN == N_GENS - 1
+* AnalysisVariant if RUN_AGGREGATE_ANALYSIS and GEN == N_GENS - 1
+* AnalysisMultiGen if RUN_AGGREGATE_ANALYSIS and GEN == N_GENS - 1
+* BuildCausalityNetwork if BUILD_CAUSALITY_NETWORK
 
 AnalysisSingle (fw_this_variant_this_gen_this_sim_analysis)
-* CompressVariantSimData
-* CompressValidationData
-* CompressSimulationOutput
+* CompressVariantSimData if COMPRESS_OUTPUT
+* CompressValidationData if COMPRESS_OUTPUT
+* CompressSimulationOutput if COMPRESS_OUTPUT
 
 AnalysisVariant (fw_variant_analysis)
-* CompressVariantSimData
-* CompressValidationData
-* CompressSimulationOutput
+* CompressVariantSimData if COMPRESS_OUTPUT
+* CompressValidationData if COMPRESS_OUTPUT
+* CompressSimulationOutput if COMPRESS_OUTPUT
 
 AnalysisCohort (fw_this_variant_cohort_analysis)
-* CompressVariantSimData
-* CompressValidationData
-* CompressSimulationOutput
+* CompressVariantSimData if COMPRESS_OUTPUT
+* CompressValidationData if COMPRESS_OUTPUT
+* CompressSimulationOutput if COMPRESS_OUTPUT
 
 AnalysisMultiGen (fw_this_variant_this_seed_this_analysis)
-* CompressVariantSimData
-* CompressValidationData
-* CompressSimulationOutput
+* CompressVariantSimData if COMPRESS_OUTPUT
+* CompressValidationData if COMPRESS_OUTPUT
+* CompressSimulationOutput if COMPRESS_OUTPUT
 
 BuildCausalityNetwork (fw_this_variant_this_gen_this_sim_causality_network)
-* CompressVariantSimData
-* CompressSimulationOutput
+* CompressVariantSimData if COMPRESS_OUTPUT
+* CompressSimulationOutput if COMPRESS_OUTPUT
 
 CompressValidationData (fw_validation_data_compression)
-* CompressRawData
-* CompressRawValidationData
 
 CompressRawData (fw_raw_data_compression)
 
