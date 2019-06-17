a9f6ff0b87316e7f7410ea9a349a89a9e0337059
Ryan Spangler <spanglry@stanford.edu>
2019-06-17 16:53:49 -0700

    implemented readColumn in terms of db operations

--- wholecell/io/ingest_tables.py
+++ wholecell/io/ingest_tables.py
@@ -5,15 +5,26 @@ import numpy as np
 from pymongo import MongoClient
 from wholecell.io.tablereader import TableReader
 
+INDEX_COLUMNS = [
+	'key',
+	'column',
+	'subcolumn',
+	'subcolumn_index']
+
 def tolist(a):
 	value = a
 	if isinstance(a, np.ndarray):
 		value = a.tolist()
 	return value
 
+def create_indexes(table):
+	for column in INDEX_COLUMNS:
+		table.create_index(column)
+
 def ingest_tables(key, sim_out, url='localhost:27017', database='wcm'):
 	client = MongoClient(url)
 	db = getattr(client, database)
+	db.attributes.create_index('table')
 
 	table_names = [
 		name
@@ -25,11 +36,16 @@ def ingest_tables(key, sim_out, url='localhost:27017', database='wcm'):
 
 		reader = TableReader(os.path.join(sim_out, table_name))
 		table = getattr(db, table_name)
+		create_indexes(table)
 
 		attribute_names = reader.attributeNames()
 		attributes = {
 			attribute_name: reader.readAttribute(attribute_name)
 			for attribute_name in attribute_names}
+
+		db.attributes.insert_one({
+			"table": table_name,
+			"attributes": attributes})
 
 		column_names = reader.columnNames()
 		subcolumns = attributes.get('subcolumns', {})
@@ -66,6 +82,36 @@ def ingest_tables(key, sim_out, url='localhost:27017', database='wcm'):
 					'data': tolist(column)}
 
 				table.insert_one(document)
+
+class DBTableReader(object):
+	def __init__(self, key, path, url='localhost:27017', database='wcm'):
+		self.key = key
+		self.table_name = os.path.basename(path)
+		self.client = MongoClient(url)
+		self.db = getattr(self.client, database)
+		self.table = getattr(self.db, self.table_name)
+		self.attributes = self.db.attributes.find_one({'table': self.table_name})['attributes']
+
+	def readColumn(self, column_name, indices=None):
+		subcolumn_header = self.attributes.get('subcolumns', {}).get(column_name)
+		column = []
+
+		if subcolumn_header:
+			results = self.table.find({
+				'key': self.key,
+				'column': column_name}).sort('subcolumn_index', 1)
+
+			column = np.transpose(np.array([result['data'] for result in results]))
+
+		else:
+			result = self.table.find_one({
+				'key': self.key,
+				'column': column_name})
+
+			if result:
+				column = np.array(result['data'])
+
+		return column
 
 if __name__ == '__main__':
 	key = sys.argv[1]
