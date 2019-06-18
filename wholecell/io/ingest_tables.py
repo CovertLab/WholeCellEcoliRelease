36b3822e436c504e45a1bd5696063d931e74c42d
Ryan Spangler <spanglry@stanford.edu>
2019-06-17 17:17:31 -0700

    added some comments to the ingest code

--- wholecell/io/ingest_tables.py
+++ wholecell/io/ingest_tables.py
@@ -12,16 +12,30 @@ INDEX_COLUMNS = [
 	'subcolumn_index']
 
 def tolist(a):
+	'''If value is np.ndarray, convert it to a list.'''
+
 	value = a
 	if isinstance(a, np.ndarray):
 		value = a.tolist()
 	return value
 
 def create_indexes(table):
+	'''Create all of the necessary indexes for the given table name.'''
+
 	for column in INDEX_COLUMNS:
 		table.create_index(column)
 
 def ingest_tables(key, sim_out, url='localhost:27017', database='wcm'):
+	'''
+	Ingest all simulation output into a mongo database.
+
+	Args:
+	    key (str): Unique key for this simulation generation.
+	    sim_out (str): Path to the simulation output directory we are ingesting.
+	    url (str): URL to the mongodb instance.
+	    database (str): Name of database to ingest simulation output into.
+	'''
+
 	client = MongoClient(url)
 	db = getattr(client, database)
 	db.attributes.create_index('table')
@@ -84,7 +98,22 @@ def ingest_tables(key, sim_out, url='localhost:27017', database='wcm'):
 				table.insert_one(document)
 
 class DBTableReader(object):
+	'''
+	This class emulates wholecell.io.tablereader.TableReader but provides results
+	from a mongo database instead.
+	'''
+
 	def __init__(self, key, path, url='localhost:27017', database='wcm'):
+		'''
+		Initialize the DBTableReader object with a given unique key and "path" to the
+		table of interest. In TableReader, this path is a filesystem path, but here we
+		use only the `os.path.basename(path)` to identify the mongo collection.
+
+		Args:
+	        key (str): Unique key for this simulation generation.
+		    path (str): Identifer for table.
+		'''
+
 		self.key = key
 		self.table_name = os.path.basename(path)
 		self.client = MongoClient(url)
@@ -93,13 +122,27 @@ class DBTableReader(object):
 		self.attributes = self.db.attributes.find_one({'table': self.table_name})['attributes']
 
 	def readColumn(self, column_name, indices=None):
+		'''
+		Return all data associated with `column_name` for this table.
+
+		Args:
+		    column_name (str): Which column to retrieve.
+		    indices (list(int)): Indexes of subcolumns to retrieve (all if `None`)
+		'''
+
 		subcolumn_header = self.attributes.get('subcolumns', {}).get(column_name)
 		column = []
 
 		if subcolumn_header:
-			results = self.table.find({
-				'key': self.key,
-				'column': column_name}).sort('subcolumn_index', 1)
+			if indices:
+				results = self.table.find({
+					'key': self.key,
+					'column': column_name,
+					'subcolumn_index': {'$in': indices}})
+			else:
+				results = self.table.find({
+					'key': self.key,
+					'column': column_name}).sort('subcolumn_index', 1)
 
 			column = np.transpose(np.array([result['data'] for result in results]))
 
