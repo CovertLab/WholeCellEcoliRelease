a6bcb8cfe1b2a25aa4784908597c9a0f8b7c4660
Jerry Morrison <1fish2@users.noreply.github.com>
2019-07-24 23:05:33 -0700

    launch workers from an image-family
    Launching from an image-family allows creating new sisyphus worker images that supersede the previous ones. And if the new image fails, you can deprecate it to revert to the next older one.

--- runscripts/sisyphus/launch-workers.sh
+++ runscripts/sisyphus/launch-workers.sh
@@ -19,7 +19,7 @@ gcloud compute \
        --maintenance-policy=MIGRATE \
        --service-account=441871726775-compute@developer.gserviceaccount.com \
        --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
-       --image=sisyphus-base \
+       --image-family=sisyphus-worker \
        --image-project=$PROJECT \
        --boot-disk-size=200GB \
        --boot-disk-type=pd-standard \
