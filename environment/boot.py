e46e676af16833ab4882fb6082233dd45882da4a
Eran Agmon <agmon.eran@gmail.com>
2019-09-18 16:58:21 -0700

    make lengthSec long again

--- environment/boot.py
+++ environment/boot.py
@@ -108,7 +108,7 @@ def ecoli_boot_config(agent_config):
 		"variant_type":           variant_type,
 		"variant_index":          variant_index,
 		"outer_id":               outer_id,
-		"lengthSec":              3,
+		"lengthSec":              3*60*60,
 		"outputDir":              output_dir,
 		"initialTime":            start_time,
 		"inheritedStatePath":     inherited_state_path,
