7eb8b115b214bc9c755aef4b4afd72825d1572f1
cs <cs.temporary@icloud.com>
2019-10-17 18:54:29 +0000

    Generalize Behavior Metrics
    Based on review comments from @1fish2, we might want to use the output
    of these metrics for more than just unit testing. For example, we might
    create plots of these metrics over time or run checks only over the
    running averages. To accommodate this use case, this commit removes the
    unit testing framework and instead stores results in a Pandas DataFrame
    for other classes to consume.
    
    This commit also changes the BehaviorMetrics class to take in the
    simulation output directory and configuration file paths as constructor
    arguments for more general usability.
    
    Example Output:
    
    ```
    $ python prototypes/behavior_metrics/behavior_metrics.py
                  metric  expected_min  expected_max     value   pass
    0        proteinMass           1.9           2.1  1.921763   True
    1           rRnaMass           1.9           2.1  1.971384   True
    2            dnaMass           1.9           2.2  2.110126   True
    3  smallMoleculeMass           1.9           2.1  1.949451   True
    4           tRnaMass           1.9           2.1  1.988803   True
    5           mRnaMass           1.9           2.1  1.837701  False
    ```
    
    Addresses some of the review comments from @1fish2 and
    @prismofeverything. Fixes part of #675.

--- prototypes/behavior_metrics/behavior_metrics.py
+++ prototypes/behavior_metrics/behavior_metrics.py
@@ -6,23 +6,16 @@
 from __future__ import absolute_import, division, print_function
 import json
 from os import path
-import unittest
+from typing import Tuple
+
+import numpy as np
+import pandas as pd
+
 
 from wholecell.io.tablereader import TableReader
 
 
-"""Path from repository root to metrics configuration JSON file
-
-File must be structured as follows:
-	{
-		"metric_name": {
-			"table": "A folder in SIM_OUT_DIR"
-			"column": "A file in SIM_OUT_DIR/table/"
-			"mode": "A key from TestBehaviorMetrics.MODE_FUNC_MAP"
-			"range": [min, max]
-		}
-	}
-"""
+"""Path from repository root to metrics configuration JSON file"""
 METRICS_CONF_PATH = "prototypes/behavior_metrics/metrics.json"
 
 """Path to the simulation output directory from repository root"""
@@ -31,55 +24,82 @@ SIM_OUT_DIR = (
 )
 
 
-class TestBehaviorMetrics(unittest.TestCase):
+class BehaviorMetrics:
 	"""Tests for model behavior metrics"""
 
-	longMessage = True
-
-	@classmethod
-	def setUpClass(cls):
+	def __init__(self, metrics_conf_path, sim_out_dir):
+		# type: (BehaviorMetrics, str, str)
 		"""Load metrics configuration and define modes
 
 		Modes are defined by a map from mode string to the function that
 		handles the mode.
+
+		Configuration file ile must be structured as follows (in JSON):
+			{
+				"metric_name": {
+					"table": "A folder in SIM_OUT_DIR"
+					"column": "A file in SIM_OUT_DIR/table/"
+					"mode": "A key from TestBehaviorMetrics.MODE_FUNC_MAP"
+					"range": [min, max]
+				}
+			}
+
+		Arguments:
+			metrics_conf_path: Path to the metrics configuration JSON
+				file
+			sim_out_dir: Path to the simulation output directory
 		"""
-		with open(METRICS_CONF_PATH, "r") as f:
-			cls.metrics_conf = json.load(f)
-		cls.MODE_FUNC_MAP = {
-			"end_start_ratio": cls._check_end_start_ratio,
+		with open(metrics_conf_path, "r") as f:
+			self.metrics_conf = json.load(f)
+		self.MODE_FUNC_MAP = {
+			"end_start_ratio": BehaviorMetrics._calc_end_start_ratio,
 		}
+		self.sim_out_dir = sim_out_dir
 
 	def test_metrics(self):
+		# type: (BehaviorMetrics) -> pd.DataFrame
 		"""Test all behavior metrics
 
 		For each metric defined in the configuration file, calls the
 		appropriate mode function.
 		"""
+		results = []
 		for metric, config in self.metrics_conf.items():
-			reader = TableReader(path.join(SIM_OUT_DIR, config["table"]))
+			reader = TableReader(path.join(self.sim_out_dir, config["table"]))
 			data = reader.readColumn(config["column"])
-			msg = "Metric: {}".format(metric)
-			self.MODE_FUNC_MAP[config["mode"]](
-				self, data, config["range"], msg)
+			metric_val = self.MODE_FUNC_MAP[config["mode"]](data)
+			expected_min, expected_max = config["range"]
+			result = pd.DataFrame(
+				[[metric, expected_min, expected_max, metric_val]],
+				columns=["metric", "expected_min", "expected_max", "value"])
+			results.append(result)
+		results_df = pd.concat(results, ignore_index=True)
+		results_df["pass"] = (
+			(results_df["expected_min"] < results_df["value"])
+			& (results_df["value"] < results_df["expected_max"])
+		)
+		return results_df
 
-	def _check_end_start_ratio(self, data, expected_range, msg):
-		# (TestBehaviorMetrics, np.ndarray, Tuple[float, float]) -> None
+	@staticmethod
+	def _calc_end_start_ratio(data):
+		# type: (np.ndarray) -> float
 		"""Check the ratio of a data's end to its start
-
-		The ratio is computed as: end / start
 
 		Arguments:
 			data: A 1-dimensional array of timeseries data, in
 				chronological order.
-			expected_range: A tuple (min, max) that defines the minimum
-				and maximum allowable values for the ratio.
-			msg: String to display along with any test failures. Should
-				at minimum include the metric being tested.
+
+		Returns:
+			The ratio end / start.
 		"""
-		end_start_ratio = data[-1] / data[0]
-		self.assertLessEqual(expected_range[0], end_start_ratio, msg)
-		self.assertLessEqual(end_start_ratio, expected_range[1], msg)
+		return data[-1] / data[0]
+
+
+def main():
+	metrics = BehaviorMetrics(METRICS_CONF_PATH, SIM_OUT_DIR)
+	results = metrics.test_metrics()
+	print(results)
 
 
 if __name__ == "__main__":
-	unittest.main()
+	main()
