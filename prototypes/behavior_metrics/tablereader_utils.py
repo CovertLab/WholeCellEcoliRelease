d66e334b61996d304326c52e6cdfa8116f83eaea
cs <cs.temporary@icloud.com>
2019-11-11 20:39:29 +0000

    Remove Shebangs or Change to Python 2
    Since the code base is not ready to depend on Python 3, change the
    shebangs to `#!/usr/bin/env python2`. For those files that aren't meant
    to be executed (no `if __name__ == "__main__"`), remove shebangs.
    
    Addresses review comments from @1fish2 on PR #726.

--- prototypes/behavior_metrics/tablereader_utils.py
+++ prototypes/behavior_metrics/tablereader_utils.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-
-
 """Utilities for working with tables"""
 
 
--- prototypes/behavior_metrics/test_tablereader_utils.py
+++ prototypes/behavior_metrics/test_tablereader_utils.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 
 
 """Tests for tablereader_utils utility functions"""
