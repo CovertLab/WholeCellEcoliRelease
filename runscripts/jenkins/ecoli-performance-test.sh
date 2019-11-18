4ac11be4cf6ad248e40f4dd1b1892697933c861e
cs <cs.temporary@icloud.com>
2019-11-18 22:22:27 +0000

    Update Jenkins Scripts for Pytest
    Change pull request script to run pytest. Delete the performance and
    small test scripts because there are few enough tests to always run them
    all.
    
    Fixes part of #45.

--- runscripts/jenkins/ecoli-performance-test.sh
+++
@@ -1,9 +0,0 @@
-module load wcEcoli/sherlock2
-pyenv local wcEcoli2
-
-make clean
-make compile
-
-set -e
-
-PYTHONPATH=$PWD:$PYTHONPATH nosetests -a 'performance' --with-xunit --with-coverage --cover-package=wholecell --cover-xml
