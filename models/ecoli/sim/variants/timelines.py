564b61c3ee3158ceeebcd4d6e6117a43cadad057
Travis Horst <thorst@stanford.edu>
2019-12-05 13:15:23 -0800

    Fix improper initialization for timelines variants not starting in basal (#748)

--- models/ecoli/sim/variants/timelines.py
+++ models/ecoli/sim/variants/timelines.py
@@ -1,25 +1,33 @@
-CONTROL_OUTPUT = dict(
-	shortName = "control",
-	desc = "Control simulation"
-	)
+"""
+Timelines variant for simulations with changing media at certain times
+
+Modifies:
+	sim_data.condition
+	sim_data.external_state.environment.current_timeline_id
+
+Expected variant indices (dependent on sorted order from reconstruction/ecoli/flat/condition/timelines_def.tsv):
+	0-26
+"""
+
+from __future__ import absolute_import, division, print_function
 
 
 def timelines(sim_data, index):
-	'''
-	Run variants of different timelines, saved in condition/media/timelines_def.tsv
-	Args:
-		index: the index in timelines_def.tsv for this variant's timeline
-
-	'''
-
-	n_saved_timelines = len(sim_data.external_state.environment.saved_timelines)
-
-	if index % n_saved_timelines == 0:
-		return CONTROL_OUTPUT, sim_data
-
-	timeline_ids = sorted(sim_data.external_state.environment.saved_timelines)
+	saved_timelines = sim_data.external_state.environment.saved_timelines
+	timeline_ids = sorted(saved_timelines)
 	current_timeline_id = timeline_ids[index]
 	sim_data.external_state.environment.current_timeline_id = current_timeline_id
+
+	# Get possible condition from starting nutrients for proper initialization
+	# Not guaranteed to map to any condition or could map to multiple conditions
+	nutrients = saved_timelines[current_timeline_id][0][1]
+	conditions = [cond for cond in sim_data.conditionActiveTfs
+		if sim_data.conditions[cond]['nutrients'] == nutrients]
+	if len(conditions) == 1:
+		sim_data.condition = conditions[0]
+	else:
+		print('Warning: could not find mapping from nutrients ({}) to condition.'
+			' Using default condition ({}).'.format(nutrients, sim_data.condition))
 
 	return dict(
 		shortName = "{}_env".format(current_timeline_id),
