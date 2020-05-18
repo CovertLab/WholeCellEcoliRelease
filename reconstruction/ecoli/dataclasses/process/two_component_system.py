73cd7a41b8a0bad9571efb1ddf8bf2d26e48a5eb
Travis Horst <thorst@stanford.edu>
2020-05-18 14:04:09 -0700

    Fold change update (#872)
    * Add script to analyze fold change data
    * Update some fold changes with new means in foldChanges.tsv
    * Skip uncertain fold changes that do not match annotations
    * Adjust atol for TCS

--- reconstruction/ecoli/dataclasses/process/two_component_system.py
+++ reconstruction/ecoli/dataclasses/process/two_component_system.py
@@ -393,7 +393,7 @@ class TwoComponentSystem(object):
 
 		sol = scipy.integrate.solve_ivp(
 			self.derivatives, [0, timeStepSec], y_init,
-			method=method, t_eval=[0, timeStepSec], atol=1e-7,
+			method=method, t_eval=[0, timeStepSec], atol=1e-8,
 			jac=self.derivatives_jacobian
 			)
 		y = sol.y.T
