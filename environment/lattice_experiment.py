b92427ca91c3d0e6eaf38b23cc3fd22f7cd52673
cs <cs.temporary@icloud.com>
2020-06-23 14:34:37 +0000

    Make lattice_experiment easier to configure with constants

--- environment/lattice_experiment.py
+++ environment/lattice_experiment.py
@@ -1,19 +1,14 @@
 from __future__ import absolute_import, division, print_function
 
-from subprocess import call
-
 from vivarium.core.composition import (
 	make_agents,
 	make_experiment_from_compartment_dicts,
 	simulate_experiment,
-	save_timeseries,
 	plot_agents_multigen,
 )
 from vivarium.compartments.lattice import Lattice
 from vivarium.plots.multibody_physics import plot_snapshots
 
-from wholecell.utils.make_media import Media
-from wholecell.states.external_state import ExternalState
 from wcecoli_process import wcEcoliAgent
 from wcecoli_compartment import WcEcoliCell
 
@@ -21,11 +16,12 @@ from wcecoli_compartment import WcEcoliCell
 MEDIA_IDS = ["minimal", "minimal_minus_oxygen",
 		"minimal_plus_amino_acids"]
 MEDIA_ID = "minimal"
-N_WCECOLI_AGENTS = 2
+N_WCECOLI_AGENTS = 1  # Works at 50
 BOUNDS = (50, 50)
 N_BINS = (20, 20)
 OUT_DIR = 'out/agent'
-
+SAVE_OUTPUT = True
+SIMULATION_TIME = 60 * 60
 
 
 def main():
@@ -72,32 +68,33 @@ def main():
 		environment_dict, agents_dict, emitter_dict, initial_state)
 	settings = {
 		'timestep': 1.0,
-		'total_time': 20,
+		'total_time': SIMULATION_TIME,
 		'return_raw_data': True,
 	}
 	data = simulate_experiment(experiment, settings)
 
-	# Plot snapshots
-	agents = {
-		time: time_data['agents'] for time, time_data in data.items()}
-	fields = {
-		time: time_data['fields'] for time, time_data in data.items()}
-	snapshots_data = {
-		'agents': agents,
-		'fields': fields,
-		'config': environment_config['multibody'],
-	}
-	plot_config = {
-		'out_dir': OUT_DIR,
-		'filename': 'snapshot',
-	}
-	plot_snapshots(snapshots_data, plot_config)
+	if SAVE_OUTPUT:
+		# Plot snapshots
+		agents = {
+			time: time_data['agents'] for time, time_data in data.items()}
+		fields = {
+			time: time_data['fields'] for time, time_data in data.items()}
+		snapshots_data = {
+			'agents': agents,
+			'fields': fields,
+			'config': environment_config['multibody'],
+		}
+		plot_config = {
+			'out_dir': OUT_DIR,
+			'filename': 'snapshot',
+		}
+		plot_snapshots(snapshots_data, plot_config)
 
-	# Plot Emitted Values
-	plot_settings = {
-		'agents_key': 'agents',
-	}
-	plot_agents_multigen(data, plot_settings, OUT_DIR, 'simulation')
+		# Plot Emitted Values
+		plot_settings = {
+			'agents_key': 'agents',
+		}
+		plot_agents_multigen(data, plot_settings, OUT_DIR, 'simulation')
 
 
 if __name__ == '__main__':
