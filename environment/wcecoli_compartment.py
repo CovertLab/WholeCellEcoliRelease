c4da2f2d61120b876d3a4b2f4490a8d69f33125f
cs <cs.temporary@icloud.com>
2020-07-15 17:15:15 +0000

    Add death from injected antibiotic

--- environment/wcecoli_compartment.py
+++ environment/wcecoli_compartment.py
@@ -3,6 +3,9 @@ from __future__ import absolute_import, division, print_function
 import copy
 
 from vivarium.core.process import Generator
+from vivarium.processes.death import DeathFreezeState
+from vivarium.processes.injector import Injector
+from vivarium.processes.exchange_suppressor import ExchangeSuppressor
 
 from environment.wcecoli_process import wcEcoliAgent
 from environment.wcecoli_meta_division import WcEcoliMetaDivision
@@ -29,10 +32,30 @@ class WcEcoliCell(Generator):
 			'compartment': self,
 		})
 		derive_shape = WcEcoliDeriveShape()
+		death_config = {
+			'detectors': {
+				'antibiotic': {
+					'antibiotic_threshold': 10.0,
+					'antibiotic_key': 'rifampicin',
+				},
+			},
+			'targets': ['wcecoli', 'meta_division', 'death', 'injector'],
+		}
+		death = DeathFreezeState(death_config)
+		injector_config = {
+			'substrate_rate_map': {
+				'rifampicin': 1.0,
+			},
+		}
+		injector = Injector(injector_config)
+		exchange_suppressor = ExchangeSuppressor()
 		return {
 			'wcecoli': wcecoli_process,
 			'meta_division': meta_division,
 			'derive_shape': derive_shape,
+			'death': death,
+			'injector': injector,
+			'exchange_suppressor': exchange_suppressor,
 		}
 
 	def generate_topology(self, config=None):
@@ -59,6 +82,17 @@ class WcEcoliCell(Generator):
 				'cells': self.config['agents_path'],
 			},
 			'derive_shape': {
+				'global': boundary_path,
+			},
+			'death': {
+				'global': boundary_path,
+				'internal': boundary_path + ('cytoplasm',),
+			},
+			'injector': {
+				'internal': boundary_path + ('cytoplasm',),
+			},
+			'exchange_suppressor': {
+				'exchange': boundary_path + ('exchange',),
 				'global': boundary_path,
 			},
 		}
