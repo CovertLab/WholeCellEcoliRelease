6f36238a9f14fa9b4e204a433e8e9df27ff000b0
cs <cs.temporary@icloud.com>
2020-07-28 19:24:36 +0000

    Create analysis script for colony sim

---
+++ environment/analyze.py
@@ -0,0 +1,31 @@
+'''Generate development analysis plots for colony simulations
+
+For usage information, execute:
+
+    python analysis.py -h
+'''
+
+from vivarium.analysis.analyze import Analyzer
+
+
+TIMESERIES_CONFIG = {
+	'remove_zeros': True,
+	'skip_paths': [
+		('boundary', 'wcecoli_fields_null'),
+	],
+}
+SNAPSHOTS_CONFIG = {
+	'include_fields': ['rifampicin'],
+}
+
+
+def main():
+	analyzer = Analyzer(
+		timeseries_config=TIMESERIES_CONFIG,
+		snapshots_config=SNAPSHOTS_CONFIG,
+	)
+	analyzer.run()
+
+
+if __name__ == '__main__':
+    main()
