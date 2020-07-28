a2f9243a428fbc61268db3ec9d79d055a4e204fc
Jerry Morrison <1fish2@users.noreply.github.com>
2020-07-28 14:27:03 -0700

    change back from pyenv wcEcoli3-staging

--- runscripts/jenkins/ecoli-pull-request.sh
+++ runscripts/jenkins/ecoli-pull-request.sh
@@ -11,7 +11,7 @@ module load wcEcoli/python3
 ### Edit this line to make the PR build use another pyenv like wcEcoli3-staging.
 ### Revert it to `wcEcoli3` before merging the PR into master.
 ### -------------------------------------------------------------------
-pyenv local wcEcoli3-staging
+pyenv local wcEcoli3
 
 make clean compile
 
