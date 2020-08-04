1d218233df9fc1415eaf90a9ea65165dad5a7fb3
Jerry Morrison <1fish2@users.noreply.github.com>
2020-08-04 13:29:35 -0700

    refine augment-pycharm-package-list.sh
    There's a new PyCharm release so we need to patch its list of packages where the import name doesn't match the PyPI name.
    
    This change script can now be readily extended to work with more OSs and PyCharm installers, but not snap-installed PyCharm since it's on a readonly filesystem.
    
    It's now careful to be idempotent.

--- runscripts/tools/augment-pycharm-package-list.sh
+++ runscripts/tools/augment-pycharm-package-list.sh
@@ -6,18 +6,47 @@
 # Run this then restart PyCharm each time you install a PyCharm release.
 # See https://youtrack.jetbrains.com/issue/PY-27985
 #
-# This handles PyCharm Pro installed on macOS by JetBrains Toolbox.
-# TODO: Support PyCharm CE (python-ce.jar), other installers, and other OSs.
+# This handles PyCharm Pro (python.jar) and PyCharm CE (python-ce.jar)
+# installed on macOS by JetBrains Toolbox.
+#
+# TODO: Support other PyCharm installers and other OSs. But this approach doesn't
+# work for PyCharm installed by snap since it's on a read-only filesystem.
 
 set -eu
 
-cd ~/Library/Application\ Support/JetBrains/Toolbox/apps/PyCharm-P/ch-0
-cd $(ls -t | head -1)
-cd PyCharm.app/Contents/plugins/python/lib
+FILE_TO_PATCH=tools/packages
 
-unzip python.jar tools/packages
-echo "arrow stochastic-arrow
+patch_packages() {  # Patch the $FILE_TO_PATCH list inside the $1/$2 jar file.
+  if [ -d "$1" ]; then
+    cd "$1"
+
+    if [ -e "$2" ]; then
+      echo Checking PyCharm python*.jar file "$2"...
+
+      unzip "$2" $FILE_TO_PATCH
+
+      if grep --quiet wholecell-vivarium $FILE_TO_PATCH; then
+        echo "$2" // $FILE_TO_PATCH was already patched
+      else
+        echo Patching "$2" // $FILE_TO_PATCH
+        echo "
+arrow stochastic-arrow
 Bio biopython
 borealis borealis-fireworks
-vivarium wholecell-vivarium" >> tools/packages
-zip python.jar tools/packages
+vivarium wholecell-vivarium" >> $FILE_TO_PATCH
+        zip python.jar $FILE_TO_PATCH
+        echo "===Remember to restart PyCharm==="
+      fi
+
+      rm $FILE_TO_PATCH
+    fi
+  fi
+}
+
+MAC_JB_TOOLBOX_PYCHARM="$HOME/Library/Application Support/JetBrains/Toolbox/apps/PyCharm-P/ch-0"
+if [ -d "$MAC_JB_TOOLBOX_PYCHARM" ]; then
+  cd "$MAC_JB_TOOLBOX_PYCHARM"
+  cd "$(ls -t | head -1)"
+  patch_packages PyCharm.app/Contents/plugins/python/lib python.jar
+  patch_packages PyCharm.app/Contents/plugins/python/lib python-ce.jar
+fi
