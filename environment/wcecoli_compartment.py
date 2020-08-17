77c11b81fc2f11b83c2d8417622586642f164b4f
cs <cs.temporary@icloud.com>
2020-08-17 20:20:07 +0000

    Add antibiotic hydrolysis and periplasm volume

--- environment/wcecoli_compartment.py
+++ environment/wcecoli_compartment.py
@@ -9,8 +9,10 @@ from vivarium.processes.diffusion_cell_environment_ficks import (
 )
 from vivarium.processes.derive_concentrations import (
 	DeriveConcentrations)
+from vivarium.processes.derive_globals import DeriveGlobals
 from vivarium.processes.timeline import TimelineProcess
 
+from environment.antibiotic_hydrolysis import AntibioticHydrolysis
 from environment.wcecoli_process import wcEcoliAgent
 from environment.wcecoli_meta_division import WcEcoliMetaDivision
 from environment.wcecoli_derive_shape import WcEcoliDeriveShape
@@ -21,6 +23,9 @@ INITIAL_EXTERNAL_ANTIBIOTIC = 0
 ANTIBIOTIC_KEY = 'nitrocefin'
 PUMP_KEY = 'TRANS-CPLX-201[s]'
 PORIN_KEY = 'porin'
+# Source: (Wülfing & Plückthun, 1994)
+PERIPLASM_FRACTION = 0.3
+BETA_LACTAMASE_KEY = 'EG10040-MONOMER[p]'
 
 
 class WcEcoliCell(Generator):
@@ -40,6 +45,16 @@ class WcEcoliCell(Generator):
 			'pump_km': 4.95e-3,  # Units: mM
 			'pump_key': PUMP_KEY,
 			'antibiotic_key': ANTIBIOTIC_KEY,
+		},
+		'hydrolysis': {
+			'initial_catalyst': 0.0,
+			'catalyst': BETA_LACTAMASE_KEY,
+			'initial_target_internal': INITIAL_INTERNAL_ANTIBIOTIC,
+			'target': ANTIBIOTIC_KEY,
+			# Reported in (Galleni et al., 1988)
+			'kcat': 490 / units.sec,
+			# Reported in (Galleni et al., 1988)
+			'Km': 500 * units.micromolar,
 		},
 		'death': {
 			'detectors': {
@@ -69,6 +84,10 @@ class WcEcoliCell(Generator):
 				'internal': {
 					ANTIBIOTIC_KEY: INITIAL_INTERNAL_ANTIBIOTIC,
 				},
+				'global': {
+					'periplasm_volume': (
+						1.2 * units.fL * PERIPLASM_FRACTION),
+				},
 			},
 			'molecules_to_diffuse': [ANTIBIOTIC_KEY],
 			# (Nagano & Nikaido, 2009) reports that their mutant strain,
@@ -78,11 +97,15 @@ class WcEcoliCell(Generator):
 			'permeability': 0.2e-6 * units.cm / units.sec,
 			# From (Nagano & Nikaido, 2009)
 			'surface_area_mass_ratio': 132 * units.cm**2 / units.mg,
+			'volume_variable': 'periplasm_volume',
 		},
 		'derive_concentrations': {
-			'concentration_keys': [PUMP_KEY],
+			'concentration_keys': [PUMP_KEY, BETA_LACTAMASE_KEY],
 		},
 		'timeline': {},
+		'derive_globals': {
+			'periplasm_volume_fraction': PERIPLASM_FRACTION,
+		},
 	}
 
 	def generate_processes(self, config):
@@ -95,18 +118,23 @@ class WcEcoliCell(Generator):
 		death = DeathFreezeState(config['death'])
 		antibiotic_transport = AntibioticTransport(
 			config['antibiotic_transport'])
+		hydrolysis = AntibioticHydrolysis(
+			config['hydrolysis'])
 		cell_environment_diffusion = CellEnvironmentDiffusionFicks(
 			config['cell_environment_diffusion'])
 		derive_concentrations = DeriveConcentrations(
 			config['derive_concentrations'])
 		timeline = TimelineProcess(config['timeline'])
+		derive_globals = DeriveGlobals(config['derive_globals'])
 		return {
+			'derive_globals': derive_globals,
 			'derive_concentrations': derive_concentrations,
 			'wcecoli': wcecoli_process,
 			'meta_division': meta_division,
 			'derive_shape': derive_shape,
 			'death': death,
 			'antibiotic_transport': antibiotic_transport,
+			'hydrolysis': hydrolysis,
 			'cell_environment_diffusion': cell_environment_diffusion,
 			'timeline': timeline,
 		}
@@ -114,6 +142,9 @@ class WcEcoliCell(Generator):
 	def generate_topology(self, config=None):
 		boundary_path = config['boundary_path']
 		topology = {
+			'derive_globals': {
+				'global': boundary_path,
+			},
 			'derive_concentrations': {
 				'global': boundary_path,
 				'counts': boundary_path + ('bulk_molecules_report',),
@@ -153,6 +184,16 @@ class WcEcoliCell(Generator):
 				'internal': boundary_path + ('cytoplasm',),
 				'external': boundary_path + ('external',),
 				'pump_port': (
+					boundary_path + ('bulk_molecule_concentrations',)
+				),
+				'fields': config['fields_path'],
+				'fluxes': boundary_path + ('fluxes',),
+				'global': boundary_path,
+				'dimensions': config['dimensions_path'],
+			},
+			'hydrolysis': {
+				'internal': boundary_path + ('cytoplasm',),
+				'catalyst_port': (
 					boundary_path + ('bulk_molecule_concentrations',)
 				),
 				'fields': config['fields_path'],
