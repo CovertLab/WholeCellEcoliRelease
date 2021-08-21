14af419c282aae5bbcec1fcdc67d974f7d844fcb
Travis Ahn-Horst <thorst@stanford.edu>
2021-08-21 19:33:08 -0400

    Updates to charging debug script (#1157)
    * Allow grid search to be run in parallel
    * Use scattered time steps through the simulation to run the grid search
    * Save the standard deviation for the grid search results
    * Accept output file paths and # of CPUs from the command line

--- runscripts/debug/charging.py
+++ runscripts/debug/charging.py
@@ -26,7 +26,7 @@ from models.ecoli.processes.metabolism import CONC_UNITS as METABOLISM_CONC_UNIT
 from models.ecoli.processes.polypeptide_elongation import (calculate_trna_charging,
 	CONC_UNITS, get_charging_params, get_ppgpp_params, ppgpp_metabolite_changes)
 from wholecell.io.tablereader import TableReader
-from wholecell.utils import constants, scriptBase
+from wholecell.utils import constants, scriptBase, parallelization
 
 
 # Set of charging/ppGpp parameters that should not be modified by a slider
@@ -39,6 +39,23 @@ GRAPH_ID = 'graph-id'
 LOW_TIMESTEP = 'low-timestep'
 HIGH_TIMESTEP = 'high-timestep'
 
+# Grid search columns
+MEAN_COL = 'elongation rate mean (AA/s)'
+STD_COL = 'elongation rate std'
+
+
+def run_grid_search(charging, levels, index, search_params, timesteps):
+	n_levels = len(levels)
+	params = {}
+	for i, (param, value) in enumerate(search_params.items()):
+		params[param] = levels[index // n_levels**i % n_levels]
+
+	v_ribs = []
+	for timestep in timesteps:
+		_, _, v, _, _ = charging.solve_timestep(timestep, param_adjustments=params)
+		v_ribs.append(v)
+
+	return v_ribs, params
 
 class ChargingDebug(scriptBase.ScriptBase):
 	def define_parameters(self, parser):
@@ -63,6 +80,10 @@ class ChargingDebug(scriptBase.ScriptBase):
 			help='set if sims were run with variable_elongation_translation')
 
 		# Debug options
+		parser.add_argument('-c', '--cpus', type=int, default=1,
+			help='Number of CPUs to use to run in parallel.')
+		parser.add_argument('-o', '--output', default='charging-debug',
+			help='Base name of output file to save.')
 		parser.add_argument('--validation', type=int, default=1,
 			help='Number of time steps to run for validation. If < 0, will run all.')
 		parser.add_argument('--grid-search', action='store_true',
@@ -279,7 +300,7 @@ class ChargingDebug(scriptBase.ScriptBase):
 				raise ValueError(f'Charging fraction does not match for time step {timestep}')
 		print('All {} timesteps match the results from the whole-cell model.'.format(n_steps))
 
-	def grid_search(self, filename='grid.tsv', t_start=200, t_end=205, n_levels=9, low_level=-1, high_level=1):
+	def grid_search(self, filename, n_steps=20, n_levels=9, low_level=-1, high_level=1, cpus=1):
 		"""
 		Perform a grid search on the charging parameters to determine the effect
 		on elongation rate.  Results are saved to a tsv file that can be used
@@ -289,41 +310,46 @@ class ChargingDebug(scriptBase.ScriptBase):
 			accept args from the command line
 		"""
 
+		filename = f'{filename}.tsv'
+
 		levels = np.logspace(low_level, high_level, n_levels)
 		search_params = {k: v for k, v in self.adjustable_charging_params.items() if k != 'max_elong_rate'}
 		n_params = len(search_params)
-		timesteps = list(range(t_start, t_end))
+		timesteps = np.arange(self.n_time_steps)[::int(np.ceil(self.n_time_steps / n_steps))]
 		n_samples = n_levels**n_params
-		v_rib_key = 'elongation rate (AA/s)'
 
 		with open(filename, 'w') as f:
-			writer = csv.DictWriter(f, fieldnames=list(search_params.keys()) + [v_rib_key], delimiter='\t')
+			writer = csv.DictWriter(f, fieldnames=list(search_params.keys()) + [MEAN_COL, STD_COL], delimiter='\t')
 			writer.writeheader()
 
-			for i in range(n_samples):
-				if i % 100 == 0:
-					print(i)
-				params = {}
-				for j, (param, value) in enumerate(search_params.items()):
-					params[param] = levels[i // n_levels**j % n_levels]
+			def callback(result):
+				v_ribs, params = result
+				writer.writerow({MEAN_COL: np.mean(v_ribs), STD_COL: np.std(v_ribs), **params})
 
-				v_ribs = []
-				for timestep in timesteps:
-					_, _, v, _, _ = self.solve_timestep(timestep, param_adjustments=params)
-					v_ribs.append(v)
+			# Run timesteps in parallel
+			pool = parallelization.pool(num_processes=cpus)
+			results = [
+				pool.apply_async(run_grid_search, (self, levels, index, search_params, timesteps), callback=callback)
+				for index in range(n_samples)
+				]
+			pool.close()
+			pool.join()
 
-				writer.writerow({v_rib_key: np.mean(v_ribs), **params})
+			# Check for errors
+			for result in results:
+				if not result.successful():
+					result.get()
 
-	def plot_grid_results(self, path1: str, path2: str, output: str = 'grid.html'):
+	def plot_grid_results(self, filename: str, path1: str, path2: str):
 		"""
 		Compares results from two different grid searches in an interactive
 		plot.  Useful for checking sets of parameters that give desired results
 		in different simulations.
 
 		Args:
+			filename: path to the html file with the comparison plot
 			path1: path to the tsv file results from one grid search
 			path2: path to the tsv file results from another grid search
-			output: path to the html file with the comparison plot
 		"""
 
 		def load_data(path):
@@ -331,20 +357,37 @@ class ChargingDebug(scriptBase.ScriptBase):
 			with open(path) as f:
 				reader = csv.reader(f, delimiter='\t')
 				headers = next(reader)
+				mean_col = headers.index(MEAN_COL)
+				std_col = headers.index(STD_COL)
 				for line in reader:
-					data[tuple(zip(headers, line[:-1]))] = float(line[-1])
+					data[tuple(zip(headers, line[:mean_col]))] = {
+						'mean': float(line[mean_col]),
+						'std': float(line[std_col]),
+						}
 
 			return data
+
+		filename = f'{filename}.html'
 
 		data1 = load_data(path1)
 		data2 = load_data(path2)
 
 		labels = list(data1.keys() | data2.keys())
-		x = np.array([data1.get(label, 0) for label in labels])
-		y = np.array([data2.get(label, 0) for label in labels])
+		x = np.array([data1.get(label, {}).get('mean', 0) for label in labels])
+		y = np.array([data2.get(label, {}).get('mean', 0) for label in labels])
+		error_x = dict(
+			type='data',
+			array=[data1.get(label, {}).get('std', 0) for label in labels],
+			visible=False,  # Need to find better way of displaying before making visible
+			)
+		error_y = dict(
+			type='data',
+			array=[data2.get(label, {}).get('std', 0) for label in labels],
+			visible=False,  # Need to find better way of displaying before making visible
+			)
 
-		fig = go.Figure(data=go.Scatter(x=x, y=y, mode='markers', text=labels))
-		fig.write_html(output)
+		fig = go.Figure(data=go.Scatter(x=x, y=y, error_x=error_x, error_y=error_y, mode='markers', text=labels))
+		fig.write_html(filename)
 
 	def interactive_debug(self, port: int):
 		"""
@@ -557,9 +600,9 @@ class ChargingDebug(scriptBase.ScriptBase):
 		self.load_data(args.sim_data_file, args.sim_out_dir)
 		self.validation(args.validation)
 		if args.grid_search:
-			self.grid_search()
+			self.grid_search(args.output, cpus=args.cpus)
 		if args.grid_compare:
-			self.plot_grid_results(*args.grid_compare)
+			self.plot_grid_results(args.output, *args.grid_compare)
 		if args.interactive:
 			self.interactive_debug(args.port)
 
