e1f755e7f6acae06277d3dd78dceb1db2af418b9
Gwanggyu Sun <32276711+ggsun@users.noreply.github.com>
2021-10-01 11:43:15 -0700

    Update mRNA cistron expression plot to exclude genes with expression adjustments (#1190)
    * Update mRNA cistron expression plot to exclude genes with manual adjustments
    
    * Exclude cistrons that belong to the same TUs as the cistrons being adjusted

--- models/ecoli/analysis/parca/mRNA_cistron_expression.py
+++ models/ecoli/analysis/parca/mRNA_cistron_expression.py
@@ -1,15 +1,7 @@
 """
 Plots the differences between the relative expression levels of each mRNA
 cistron that is expected from RNAseq data and the actual expression levels of
-each mRNA cistron after fitting RNA expression. These two values may be
-different for one or more of these three reasons:
-
-1) Adjustment of expression levels for genes encoding for RNAP/ribosome subunits
-and the following normalization of expression levels
-2) Manual adjustment of expression levels for specific genes listed in
-reconstruction/flat/adjustments/rna_expression_adjustments.tsv.
-3) Transcription unit structure forcing stoichiometric transcription of certain
-cistrons.
+each mRNA cistron after accounting for operon structure.
 """
 
 import pickle
@@ -32,29 +24,51 @@ class Plot(parcaAnalysisPlot.ParcaAnalysisPlot):
 			sim_data = pickle.load(f)
 
 		# Load data from sim_data
-		cistron_ids = sim_data.process.transcription.cistron_data['id']
+		all_cistron_ids = sim_data.process.transcription.cistron_data['id']
+		all_rna_ids = sim_data.process.transcription.rna_data['id']
 		cistron_tu_mapping_matrix = sim_data.process.transcription.cistron_tu_mapping_matrix
 		cistron_is_mRNA = sim_data.process.transcription.cistron_data['is_mRNA']
-		mRNA_cistron_ids = cistron_ids[cistron_is_mRNA]
-		mRNA_cistron_is_ribosomal_protein = sim_data.process.transcription.cistron_data['is_ribosomal_protein'][cistron_is_mRNA]
-		mRNA_cistron_is_rnap = sim_data.process.transcription.cistron_data['is_RNAP'][cistron_is_mRNA]
+		is_ribosomal_protein = sim_data.process.transcription.cistron_data['is_ribosomal_protein']
+		is_rnap = sim_data.process.transcription.cistron_data['is_RNAP']
 
-		# Get boolean array of all cistrons that belong to at least one
+		# Get mask for genes that are
+		# i) mRNAs
+		# ii) Does not encode for ribosomal proteins or RNAPs
+		# iii) not manually overexpressed
+		# to isolate the effects of operons on expression levels.
+		is_adjusted = np.zeros_like(cistron_is_mRNA, dtype=bool)
+		for adjusted_cistron_id in sim_data.adjustments.rna_expression_adjustments.keys():
+			# Include cistrons whose expression is adjusted because they belong
+			# to the same TU as the cistron that is bumped up
+			adjusted_rna_indexes = sim_data.process.transcription.cistron_id_to_rna_indexes(
+				adjusted_cistron_id)
+			adjusted_cistron_indexes = []
+			for adjusted_rna_index in adjusted_rna_indexes:
+				adjusted_cistron_indexes.extend(
+					sim_data.process.transcription.rna_id_to_cistron_indexes(
+						all_rna_ids[adjusted_rna_index]))
+
+			is_adjusted[adjusted_cistron_indexes] = True
+
+		mask = cistron_is_mRNA & ~is_ribosomal_protein & ~is_rnap & ~is_adjusted
+		plotted_cistron_ids = all_cistron_ids[mask]
+
+		# Get boolean array of relevant cistrons that belong to at least one
 		# polycistronic transcript
 		polycistronic_cistron_indexes = []
 		for rna_id in sim_data.process.transcription.rna_data['id']:
 			cistron_indexes = sim_data.process.transcription.rna_id_to_cistron_indexes(rna_id)
 			if len(cistron_indexes) > 1:
 				polycistronic_cistron_indexes.extend(cistron_indexes)
-		is_polycistronic = np.zeros(len(cistron_ids), bool)
+		is_polycistronic = np.zeros(len(all_cistron_ids), bool)
 		if len(polycistronic_cistron_indexes) > 0:
 			is_polycistronic[np.array(list(set(polycistronic_cistron_indexes)))] = True
-		mRNA_cistron_is_polycistronic = is_polycistronic[cistron_is_mRNA]
+		is_polycistronic = is_polycistronic[mask]
 
 		# Get expression
-		expected_mRNA_cistron_exp = sim_data.process.transcription.cistron_expression[CONDITION][cistron_is_mRNA]
+		expected_mRNA_cistron_exp = sim_data.process.transcription.cistron_expression[CONDITION][mask]
 		rna_exp = sim_data.process.transcription.rna_expression[CONDITION]
-		actual_mRNA_cistron_exp = cistron_tu_mapping_matrix.dot(rna_exp)[cistron_is_mRNA]
+		actual_mRNA_cistron_exp = cistron_tu_mapping_matrix.dot(rna_exp)[mask]
 
 		# Normalize expression
 		expected_mRNA_cistron_exp /= expected_mRNA_cistron_exp.sum()
@@ -72,34 +86,24 @@ class Plot(parcaAnalysisPlot.ParcaAnalysisPlot):
 
 		plt.scatter(
 			expected_mRNA_cistron_exp, actual_mRNA_cistron_exp,
-			c='#dddddd', s=2)
+			c='#dddddd', s=2, label='monocistronic')
 
 		# Highlight genes that are polycistronic
 		plt.scatter(
-			expected_mRNA_cistron_exp[mRNA_cistron_is_polycistronic],
-			actual_mRNA_cistron_exp[mRNA_cistron_is_polycistronic],
+			expected_mRNA_cistron_exp[is_polycistronic],
+			actual_mRNA_cistron_exp[is_polycistronic],
 			c='#333333', s=4, label='polycistronic')
-
-		# Highlight cistrons that encode for RNAPs and ribosomal proteins
-		plt.scatter(
-			expected_mRNA_cistron_exp[mRNA_cistron_is_ribosomal_protein],
-			actual_mRNA_cistron_exp[mRNA_cistron_is_ribosomal_protein],
-			c='r', s=4, label='ribosomal proteins')
-		plt.scatter(
-			expected_mRNA_cistron_exp[mRNA_cistron_is_rnap],
-			actual_mRNA_cistron_exp[mRNA_cistron_is_rnap],
-			c='b', s=4, label='RNAP subunits')
 
 		# Label cistrons that have more than a 10-fold difference
 		for index in largest_diff_indexes:
 			plt.text(
 				expected_mRNA_cistron_exp[index],
 				actual_mRNA_cistron_exp[index] * 1.05,
-				mRNA_cistron_ids[index],
+				plotted_cistron_ids[index],
 				ha='center', va='bottom', fontsize=5)
 
-		plt.xlabel('Original mRNA cistron expression')
-		plt.ylabel('Actual mRNA cistron expression')
+		plt.xlabel('Original expression expected from RNAseq')
+		plt.ylabel('Actual expression after applying operon structure')
 		plt.xlim([1e-8, 1e-1])
 		plt.ylim([1e-8, 1e-1])
 		plt.xscale('log')
