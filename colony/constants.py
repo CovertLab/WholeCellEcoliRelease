c5e2873521cfa9a6992cb9b726c275c78ad5775c
Jerry Morrison <1fish2@users.noreply.github.com>
2021-10-14 22:10:36 -0700

    remove colony/ and its libraries
    The Colony project is moving to `vivarium-ecoli`, expanding its model, and will be released there.
    
    Therefore the next wcEcoli release should not include this older Colony code and does not need the old versions of pips `confluent-kafka`, `vivarium-core`, and `vivarium-cell`. Every pip is another opportunity for dependency hell and for GitHub's `dependabot` to flag security flaws in pips that we're not updating in a published release snapshot.
    
    After fetching these git changes and running `make clean`, the `colony/` directory should contain just empty directories.
    
    To update your existing `wcEcoli3` pyenv:
    
    ```
    pip install --upgrade pip setuptools virtualenv virtualenvwrapper virtualenv-clone wheel
    pip uninstall confluent-kafka vivarium-core vivarium-cell
    ```
    
    (Neither step is strictly needed to continue using your pyenv. The first step is our usual "update the installers.")

--- colony/constants.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-WCECOLI_ROOT = os.path.abspath(
-	os.path.join(os.path.dirname(__file__), '..'))
-OUT_DIR = os.path.abspath(
-	os.path.join(os.path.dirname(__file__), 'out'))
-REFERENCE_DATA_PATH = os.path.abspath(
-	os.path.join(os.path.dirname(__file__), 'reference_data'))
