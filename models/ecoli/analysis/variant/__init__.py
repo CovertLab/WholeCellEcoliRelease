0c246b20c8b1b7d5f07b5099f636dca286d569f7
Travis Ahn-Horst <thorst@stanford.edu>
2022-04-18 00:59:29 -0400

    Amino acid concentration period analysis with aa_period variant plot (#1279)

--- models/ecoli/analysis/variant/__init__.py
+++ models/ecoli/analysis/variant/__init__.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function
 # Active analysis modules to run, in this order.
 # Tip: Edit this during development to run ones you're working on first.
 ACTIVE = [
+	"aa_period.py",
 	"aa_synthesis_sensitivity.py",
 	"aa_uptake_sensitivity.py",
 	"adder_sizer.py",
@@ -83,6 +84,7 @@ TAGS = {
 		'ppgpp_conc',
 		],
 	'REMOVE_AA_INHIBITION': [
+		'aa_period.py',
 		'remove_aa_inhibition.py',
 		],
 	'REMOVE_AAS_SHIFT': [
