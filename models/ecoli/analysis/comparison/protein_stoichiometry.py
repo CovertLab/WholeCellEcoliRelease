5b1c8f52e36b4045e65c76441e02fe2bd910211f
Gwanggyu Sun <ggsun@stanford.edu>
2022-07-11 13:32:25 -0700

    Add plot for protein stoichiometry maintainence

---
+++ models/ecoli/analysis/comparison/protein_stoichiometry.py
@@ -0,0 +1,171 @@
+"""
+Comparison plot to compare how far the stoichiometry of proteins that are
+encoded from the same operon deviates from their mean values for sims
+with/without polycistronic operons.
+"""
+import os
+from typing import Tuple
+
+from matplotlib import pyplot as plt
+import matplotlib.gridspec as gridspec
+# noinspection PyUnresolvedReferences
+import numpy as np
+from scipy import stats
+
+from models.ecoli.analysis import comparisonAnalysisPlot
+from models.ecoli.analysis.AnalysisPaths import AnalysisPaths
+from reconstruction.ecoli.simulation_data import SimulationDataEcoli
+from validation.ecoli.validation_data import ValidationDataEcoli
+from wholecell.analysis.analysis_tools import (exportFigure, read_stacked_columns)
+# noinspection PyUnresolvedReferences
+from wholecell.io.tablereader import TableReader
+
+
+
+class Plot(comparisonAnalysisPlot.ComparisonAnalysisPlot):
+	def do_plot(self, reference_sim_dir, plotOutDir, plotOutFileName, input_sim_dir, unused, metadata):
+		# From fw_queue, reference_sim_dir has operons="off"; input_sim_dir has
+		# operons="on".
+		# manual/analysisComparison.py can compare any two sim dirs.
+		# sim_data1.operons_on and sim_data2.operons_on indicate operons on/off.
+		ap1, sim_data1, _ = self.setup(reference_sim_dir)
+		ap2, sim_data2, _ = self.setup(input_sim_dir)
+
+		# Load from sim_data
+		cistron_ids = sim_data2.process.transcription.cistron_data['id']
+		cistron_id_to_monomer_id = {
+			monomer['cistron_id']: monomer['id'] for monomer
+			in sim_data2.process.translation.monomer_data
+			}
+		operons = sim_data2.process.transcription.operons
+
+		# Load monomer ID attribute
+		monomer_counts_reader = TableReader(
+			os.path.join(ap1.get_cells()[0], 'simOut', 'MonomerCounts'))
+		monomer_ids = monomer_counts_reader.readAttribute('monomerIds')
+		monomer_id_to_index = {
+			monomer_id: i for (i, monomer_id) in enumerate(monomer_ids)}
+
+		# Load list of tuples of monomer indexes that are in the same
+		# polycistronic operon
+		operon_monomer_indexes = []
+
+		for operon in operons:
+			cistron_indexes = operon[0]
+			if len(cistron_indexes) < 2:
+				continue
+			operon_monomer_indexes.append(
+				np.array([monomer_id_to_index[cistron_id_to_monomer_id[cistron_ids[i]]] for i in cistron_indexes])
+				)
+
+		def read_sims(ap):
+			# Ignore data from first two gens
+			cell_paths = ap.get_cells(generation=np.arange(2, ap.n_generation))
+
+			all_monomer_counts = read_stacked_columns(
+				cell_paths, 'MonomerCounts', 'monomerCounts',
+				ignore_exception=True)
+
+			# Take averages across all sims and timepoints
+			all_monomer_counts_mean = all_monomer_counts.mean(axis=0)
+
+			# Initialize array of maximum coefficients of variation
+			is_constitutive = np.zeros(len(operon_monomer_indexes), dtype=np.bool)
+			max_cov = np.zeros(len(operon_monomer_indexes))
+
+			# Loop through each operon
+			for i, monomer_indexes in enumerate(operon_monomer_indexes):
+				# Get counts of each monomer subunit
+				monomer_counts = all_monomer_counts[:, monomer_indexes]
+				avg_monomer_counts = all_monomer_counts_mean[monomer_indexes]
+
+				if np.any(monomer_counts == 0):
+					continue
+
+				# Rescale counts with average counts
+				rescaled_monomer_counts = monomer_counts / avg_monomer_counts
+
+				# Calculate coefficients of variation at every timestep
+				cov = rescaled_monomer_counts.std(axis=1)/rescaled_monomer_counts.mean(axis=1)
+
+				is_constitutive[i] = True
+				max_cov[i] = cov.max()
+
+			return max_cov, is_constitutive
+
+		max_cov1, is_constitutive1 = read_sims(ap1)
+		max_cov2, is_constitutive2 = read_sims(ap2)
+
+		# Plot comparison of coefficient distributions
+		fig = plt.figure(figsize=(4.5, 4.4))
+		gs = gridspec.GridSpec(
+			2, 2, width_ratios=(4, 1), height_ratios=(1, 4))
+
+		def draw_scatter_plot(p1, p2, grid_i, grid_j, y_max):
+			scatter_ax = fig.add_subplot(gs[grid_i, grid_j])
+			scatter_ax.plot([0, 1.5], [0, 1.5], ls='--', lw=1, c='k', alpha=0.1)
+			scatter_ax.scatter(
+				p1, p2,
+				alpha=0.4, s=10, c='#555555', clip_on=False, edgecolors='none')
+
+			scatter_ax.set_xlim([0, 1.5])
+			scatter_ax.set_ylim([0, 1.5])
+			scatter_ax.set_xticks([0, 1.5])
+			scatter_ax.set_yticks([0, 1.5])
+			scatter_ax.set_xlabel('Reference')
+			scatter_ax.set_ylabel('Input')
+
+			scatter_ax.spines["top"].set_visible(False)
+			scatter_ax.spines["right"].set_visible(False)
+			scatter_ax.spines["bottom"].set_position(("outward", 20))
+			scatter_ax.spines["left"].set_position(("outward", 20))
+
+			x = np.linspace(0, 1.5, 1000)
+			kde1 = stats.gaussian_kde(p1)
+			kde2 = stats.gaussian_kde(p2)
+
+			hist1_ax = fig.add_subplot(gs[grid_i - 1, grid_j], sharex=scatter_ax)
+			hist1_ax.fill_between(x, kde1(x), alpha=0.5)
+			hist1_ax.axvline(p1.mean(), lw=2, ls='--', c='#555555')
+			hist1_ax.set_xlim([0, 1.5])
+			hist1_ax.set_ylim([0, y_max])
+			hist1_ax.set_yticks([])
+			hist1_ax.spines["top"].set_visible(False)
+			hist1_ax.spines["right"].set_visible(False)
+			hist1_ax.spines["left"].set_visible(False)
+			hist1_ax.spines["bottom"].set_visible(False)
+			plt.setp(hist1_ax.get_xaxis(), visible=False)
+
+			hist2_ax = fig.add_subplot(gs[grid_i, grid_j + 1], sharey=scatter_ax)
+			hist2_ax.fill_betweenx(x, kde2(x), fc='C1', alpha=0.5)
+			hist2_ax.axhline(p2.mean(), lw=2, ls='--', c='#555555')
+			hist2_ax.set_ylim([0, 1.5])
+			hist2_ax.set_xlim([0, y_max])
+			hist2_ax.set_xticks([])
+			hist2_ax.spines["top"].set_visible(False)
+			hist2_ax.spines["right"].set_visible(False)
+			hist2_ax.spines["left"].set_visible(False)
+			hist2_ax.spines["bottom"].set_visible(False)
+			plt.setp(hist2_ax.get_yaxis(), visible=False)
+
+		plotted_operons = np.logical_and(is_constitutive1, is_constitutive2)
+		draw_scatter_plot(
+			max_cov1[plotted_operons], max_cov2[plotted_operons],
+			1, 0, 3)
+
+		plt.tight_layout()
+		exportFigure(plt, plotOutDir, plotOutFileName, metadata)
+		plt.close('all')
+
+
+	def setup(self, inputDir: str) -> Tuple[
+			AnalysisPaths, SimulationDataEcoli, ValidationDataEcoli]:
+		"""Return objects used for analyzing multiple sims."""
+		ap = AnalysisPaths(inputDir, variant_plot=True)
+		sim_data = self.read_sim_data_file(inputDir)
+		validation_data = self.read_validation_data_file(inputDir)
+		return ap, sim_data, validation_data
+
+
+if __name__ == "__main__":
+	Plot().cli()
